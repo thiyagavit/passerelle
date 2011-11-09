f892a840e482e5e04f34df7d4b3ffb6965a63a25
kheuninck@gmail.com <kheuninck@gmail.com@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2011-11-09 12:43:15 +0000

    STARTED - task 8: Merge work done in DAWB's temporarily forked Passerelle into our trunk. http://code.google.com/a/eclipselabs.org/p/passerelle/issues/detail?id=8

--- trunk/passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.ui/src/main/java/com/isencia/passerelle/workbench/model/ui/PasserellePerspective.java
+++ trunk/passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.ui/src/main/java/com/isencia/passerelle/workbench/model/ui/PasserellePerspective.java
@@ -16,10 +16,11 @@ public class PasserellePerspective implements IPerspectiveFactory {
 		String editorArea = layout.getEditorArea();
 		layout.setEditorAreaVisible(true);
 				
-		IFolderLayout navigatorFolder = layout.createFolder("navigator-folder", IPageLayout.LEFT, 0.3f, editorArea);
+		IFolderLayout navigatorFolder = layout.createFolder("navigator-folder", IPageLayout.LEFT, 0.35f, editorArea);
 		navigatorFolder.addView("org.eclipse.ui.navigator.ProjectExplorer");
 		navigatorFolder.addView("com.isencia.passerelle.workbench.model.editor.ui.views.ActorTreeView");
-        IFolderLayout outlineFolder = layout.createFolder("outline-folder",IPageLayout.BOTTOM,0.6f,"navigator-folder");
+
+		IFolderLayout outlineFolder = layout.createFolder("outline-folder",IPageLayout.BOTTOM,0.6f,"navigator-folder");
         outlineFolder.addView(IPageLayout.ID_OUTLINE);
         
         IFolderLayout viewFolder = layout.createFolder("view-folder",IPageLayout.BOTTOM,0.6f,editorArea);
