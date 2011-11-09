83f0a18553beaf4959e1b97082f756d935179ba5
kheuninck@gmail.com <kheuninck@gmail.com@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2011-11-09 15:21:57 +0000

    STARTED - task 8: Merge work done in DAWB's temporarily forked Passerelle into our trunk. http://code.google.com/a/eclipselabs.org/p/passerelle/issues/detail?id=8

--- trunk/passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/palette/SubModelPaletteItemDefinition.java
+++ trunk/passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/palette/SubModelPaletteItemDefinition.java
@@ -3,11 +3,13 @@ package com.isencia.passerelle.workbench.model.editor.ui.palette;
 import java.io.File;
 
 import org.eclipse.core.internal.resources.Workspace;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jface.resource.ImageDescriptor;
 
 import com.isencia.passerelle.model.Flow;
 import com.isencia.passerelle.workbench.model.editor.ui.Activator;
+import com.isencia.passerelle.workbench.model.utils.ModelUtils;
 
 public class SubModelPaletteItemDefinition extends PaletteItemDefinition {
 	public static ImageDescriptor IMAGE_SUBMODEL = Activator
@@ -41,16 +43,14 @@ public class SubModelPaletteItemDefinition extends PaletteItemDefinition {
 		this.flow = flow;
 	}
 
-	public SubModelPaletteItemDefinition(Flow flow, PaletteGroup group,
-			String id, String name) {
+	public SubModelPaletteItemDefinition(Flow flow, PaletteGroup group, String id, String name) throws Exception {
 		super(group, id, name, Flow.class);
 		setIcon(IMAGE_SUBMODEL);
 
 		setFlow(flow);
-		final String workspacePath = ResourcesPlugin.getWorkspace().getRoot()
-				.getLocation().toOSString();
-		path = workspacePath + File.separator + ".metadata"
-				+ File.separator + name + ".moml";
+		
+		final IProject pass = ModelUtils.getPasserelleProject();
+		path = pass.getLocation().toOSString() + "/"+name + ".moml";
 		if (ResourcesPlugin.getWorkspace() instanceof Workspace)
 			workSpace = (Workspace)ResourcesPlugin.getWorkspace();
 
