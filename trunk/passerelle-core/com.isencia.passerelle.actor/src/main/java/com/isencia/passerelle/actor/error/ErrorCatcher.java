53a5eda6cce294ccba813f5c506cc1e5f35ef79c
erwindl0@gmail.com <erwindl0@gmail.com@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2012-03-16 23:26:11 +0000

    Add handling of exit code for launched cmd in CommandLineExecutor to launch flows, and in CommandExecutor actor.
    Extend CommandExecutor with output ports to handle stdout and stderr outputs from running cmd.
    
    Enable ErrorCatcher to generate output with the error description.

--- trunk/passerelle-core/com.isencia.passerelle.actor/src/main/java/com/isencia/passerelle/actor/error/ErrorCatcher.java
+++ trunk/passerelle-core/com.isencia.passerelle.actor/src/main/java/com/isencia/passerelle/actor/error/ErrorCatcher.java
@@ -20,24 +20,29 @@ import com.isencia.passerelle.actor.v5.ProcessResponse;
 import com.isencia.passerelle.core.Port;
 import com.isencia.passerelle.core.PortFactory;
 import com.isencia.passerelle.message.ManagedMessage;
+import com.isencia.passerelle.message.MessageException;
 import com.isencia.passerelle.message.internal.ErrorMessageContainer;
 
 /**
  * An actor that extracts the ManagedMessage from ErrorMessages received
- * on its input port, and sends it out. 
+ * on its input port, and sends it out.
+ * It can also send out the description of the contained PasserelleException
+ * via the <code>errorDescrOutput</code> port.
+ *  
  * The error can optionally be logged.
- * 
+ * <p>
  * This is useful to link to an error-output of one or more actors,
  * to implement an "ignore error" behaviour, combined with continued
  * sequence operations "as-if-everything-was-OK".
  * 
  * In this respect it differs from the plain DevNullActor, which
  * discards the received msg and thus effectively interrupts the sequence.
- * 
+ * </p>
+ * <p>
  * If the received message is not an ErrorMessage, or it does not
  * contain a ManagedMessage as the error's context, it is just forwarded
  * to the output as is.
- * 
+ * </p>
  * @author delerw
  *
  */
@@ -49,6 +54,7 @@ public class ErrorCatcher extends Actor {
 
 	public Port input;
 	public Port output;
+	public Port errorDescrOutput;
 	public Parameter logReceivedMessages;
 
 	/**
@@ -62,6 +68,7 @@ public class ErrorCatcher extends Actor {
 		super(container, name);
 		input = PortFactory.getInstance().createInputPort(this, null);
 		output = PortFactory.getInstance().createOutputPort(this);
+		errorDescrOutput = PortFactory.getInstance().createOutputPort(this, "errorDescrOutput");
 		
 		logReceivedMessages = new Parameter(this,"Log received messages", new BooleanToken(false));
 		logReceivedMessages.setTypeEquals(BaseType.BOOLEAN);
@@ -96,6 +103,18 @@ public class ErrorCatcher extends Actor {
 				Object context = errorMsg.getContext();
 				if(context!=null && context instanceof ManagedMessage) {
 					outputMsg = (ManagedMessage) context;
+				}
+				if(errorMsg.getRootException()!=null) {
+  				String errDescr = errorMsg.getRootException().getMessage();
+  				if(errDescr!=null) {
+  				  try {
+              ManagedMessage errDescrOutputMsg = createMessage();
+              errDescrOutputMsg.setBodyContentPlainText(errDescr);
+              response.addOutputMessage(errorDescrOutput, errDescrOutputMsg);
+            } catch (MessageException e) {
+              getLogger().error("Error sending error description",e);
+            }
+  				}
 				}
 				if(((BooleanToken)logReceivedMessages.getToken()).booleanValue()) {
 				  getLogger().info("Discarding error for context "+errorMsg.getContext(),errorMsg.getRootException());
