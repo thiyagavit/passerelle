c2b47c16de9ce3783970569a6d125e4e429e50c1
vgramer <vgramer@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2012-07-30 14:54:03 +0000

    add test

---
+++ contributions-actors/fr.soleil.passerelle.extensions/src/test/java/fr/soleil/passerelle/actor/flow/MessageAssemblerTest.java
@@ -0,0 +1,38 @@
+package fr.soleil.passerelle.actor.flow;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.isencia.passerelle.core.PasserelleException;
+
+import fr.soleil.passerelle.testUtils.MomlRule;
+
+public class MessageAssemblerTest {
+    private static final String ACTOR_NAME = "MessageAssembler1";
+    @Rule
+    public MomlRule moml = new MomlRule("/sequences/MessageAssembler.moml");
+
+    @Test
+    public void should_assemble_messag() throws PasserelleException {
+        final Map<String, String> props = new HashMap<String, String>();
+        props.put("ForLoopMemorize1.Start Value", "0");
+        props.put("ForLoopMemorize1.End Value", "2");
+        props.put("ValuesGenerator.Values List (sep by commas)", "a,b,c");
+
+        final ArrayBlockingQueue<String> outputReceiver = new ArrayBlockingQueue<String>(3);
+        moml.addMessageReceiver(ACTOR_NAME, "output", outputReceiver);
+
+        moml.executeBlockingErrorLocally(props);
+
+        assertThat(outputReceiver.poll()).isEqualTo("a");
+        assertThat(outputReceiver.poll()).isEqualTo("a,b");
+        assertThat(outputReceiver.poll()).isEqualTo("a,b,c");
+
+    }
+}
