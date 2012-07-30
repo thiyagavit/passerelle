136c064b39410e3836fb2b049fd50421e6cb0125
vgramer <vgramer@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2012-07-30 15:41:19 +0000

    throw exception if message name is empty

--- trunk/contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/actor/flow/MessageMemoriser.java
+++ trunk/contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/actor/flow/MessageMemoriser.java
@@ -10,10 +10,12 @@ import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.Attribute;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
+
 import com.isencia.passerelle.actor.ProcessingException;
 import com.isencia.passerelle.actor.Transformer;
 import com.isencia.passerelle.message.ManagedMessage;
 import com.isencia.passerelle.util.ExecutionTracerService;
+
 import fr.soleil.passerelle.util.PasserelleUtil;
 
 @SuppressWarnings("serial")
@@ -27,39 +29,39 @@ public class MessageMemoriser extends Transformer {
     private boolean doMemorization = true;
 
     public MessageMemoriser(final CompositeEntity container, final String name)
-	    throws NameDuplicationException, IllegalActionException {
-	super(container, name);
-	messageNameParam = new StringParameter(this, "Message Name");
-	messageNameParam.setExpression(messageName);
+            throws NameDuplicationException, IllegalActionException {
+        super(container, name);
+        messageNameParam = new StringParameter(this, "Message Name");
+        messageNameParam.setExpression(messageName);
 
-	doMemorizationParam = new StringParameter(this, "Memorization");
-	doMemorizationParam.addChoice("do memorization");
-	doMemorizationParam.addChoice("output value");
-	doMemorizationParam.setExpression("do memorization");
+        doMemorizationParam = new StringParameter(this, "Memorization");
+        doMemorizationParam.addChoice("do memorization");
+        doMemorizationParam.addChoice("output value");
+        doMemorizationParam.setExpression("do memorization");
     }
 
     @Override
     protected void doFire(final ManagedMessage message) throws ProcessingException {
-	if (doMemorization) {
-	    ExecutionTracerService.trace(this, "Memorizing value in " + messageName);
-	    memorizedMessages.put(messageName, message);
-	    // System.out.println(memorizedMessages);
-	    // just output a copy of the input
-	    sendOutputMsg(output, PasserelleUtil.createCopyMessage(this, message));
-	} else { // output previoulsy saved message
-	    ExecutionTracerService.trace(this, "Outputting value " + messageName);
-	    final ManagedMessage memorizedMessage = memorizedMessages.get(messageName);
-	    if (memorizedMessage == null) { // message not found
-		throw new ProcessingException("memorized message not found", messageName, null);
-	    }
-	    sendOutputMsg(output, PasserelleUtil.createCopyMessage(this, memorizedMessage));
-	}
+        if (doMemorization) {
+            ExecutionTracerService.trace(this, "Memorizing value in " + messageName);
+            memorizedMessages.put(messageName, message);
+            // System.out.println(memorizedMessages);
+            // just output a copy of the input
+            sendOutputMsg(output, PasserelleUtil.createCopyMessage(this, message));
+        } else { // output previoulsy saved message
+            ExecutionTracerService.trace(this, "Outputting value " + messageName);
+            final ManagedMessage memorizedMessage = memorizedMessages.get(messageName);
+            if (memorizedMessage == null) { // message not found
+                throw new ProcessingException("memorized message not found", messageName, null);
+            }
+            sendOutputMsg(output, PasserelleUtil.createCopyMessage(this, memorizedMessage));
+        }
     }
 
     @Override
     protected String getExtendedInfo() {
-	// TODO Auto-generated method stub
-	return null;
+        // TODO Auto-generated method stub
+        return null;
     }
 
     /*
@@ -71,18 +73,21 @@ public class MessageMemoriser extends Transformer {
 
     @Override
     public void attributeChanged(final Attribute attribute) throws IllegalActionException {
-	if (attribute == messageNameParam) {
-	    messageName = ((StringToken) messageNameParam.getToken()).stringValue();
-	} else if (attribute == doMemorizationParam) {
-	    final String doMemorizationS = ((StringToken) doMemorizationParam.getToken())
-		    .stringValue();
-	    if (doMemorizationS.equals("do memorization")) {
-		doMemorization = true;
-	    } else {
-		doMemorization = false;
-	    }
-	}
-	super.attributeChanged(attribute);
+        if (attribute == messageNameParam) {
+            messageName = ((StringToken) messageNameParam.getToken()).stringValue();
+            if (messageName.isEmpty()) {
+                throw new IllegalActionException("Message Name can not be empty");
+            }
+        } else if (attribute == doMemorizationParam) {
+            final String doMemorizationS = ((StringToken) doMemorizationParam.getToken())
+                    .stringValue();
+            if (doMemorizationS.equals("do memorization")) {
+                doMemorization = true;
+            } else {
+                doMemorization = false;
+            }
+        }
+        super.attributeChanged(attribute);
     }
 
 }
