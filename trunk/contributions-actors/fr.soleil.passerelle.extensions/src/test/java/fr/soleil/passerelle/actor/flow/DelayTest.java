2d3ba1275b47f12f4f2aa84061a0598d1f8b06a1
vgramer <vgramer@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2012-07-30 09:45:49 +0000

    add test

---
+++ trunk/contributions-actors/fr.soleil.passerelle.extensions/src/test/java/fr/soleil/passerelle/actor/flow/DelayTest.java
@@ -0,0 +1,93 @@
+package fr.soleil.passerelle.actor.flow;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.fail;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import ptolemy.kernel.util.IllegalActionException;
+
+import com.isencia.passerelle.core.PasserelleException;
+
+import fr.soleil.passerelle.testUtils.MomlRule;
+
+public class DelayTest {
+
+    private static final String ACTOR_NAME = "Delay1";
+    @Rule
+    public MomlRule moml = new MomlRule("/sequences/Delay.moml");
+
+    @Test(expected = NumberFormatException.class)
+    public void should_throw_expection_if_time_is_a_string() throws IllegalActionException {
+
+        final Delay actor = (Delay) moml.getEntity(ACTOR_NAME);
+        actor.timeParameter.setToken("yop");
+        actor.attributeChanged(actor.timeParameter);
+
+    }
+
+    @Test(timeout = 5000)
+    public void parameterSleepTest() throws PasserelleException {
+        final Map<String, String> props = new HashMap<String, String>();
+        props.put("DirBasic.Mock Mode", "false");
+        props.put("Constant.value", "azerty");
+        props.put(ACTOR_NAME + ".time(s)", "3");
+
+        // add receiver to collect the messages which pass through the output
+        // ports
+        final ArrayBlockingQueue<String> outputReceiver = new ArrayBlockingQueue<String>(1);
+        moml.addMessageReceiver(ACTOR_NAME, "output", outputReceiver);
+
+        final long start = System.currentTimeMillis();
+        moml.executeBlockingErrorLocally(props);
+        final long end = System.currentTimeMillis();
+
+        assertThat(end - start).isGreaterThanOrEqualTo(3000).isLessThan(4000);
+        assertThat(MomlRule.extractBodyContent(outputReceiver.poll())).isEqualTo("azerty");
+
+    }
+
+    @Test(timeout = 5000)
+    public void portSleepTest() throws PasserelleException {
+        final Map<String, String> props = new HashMap<String, String>();
+        props.put("DirBasic.Mock Mode", "false");
+        props.put("Constant.value", "2.0");
+        props.put(ACTOR_NAME + ".time(s)", "4.0");
+        props.put(ACTOR_NAME + ".take delay from port", "true");
+
+        // add receiver to collect the messages which pass through the output
+        // ports
+        final ArrayBlockingQueue<String> outputReceiver = new ArrayBlockingQueue<String>(1);
+        moml.addMessageReceiver(ACTOR_NAME, "output", outputReceiver);
+
+        final long start = System.currentTimeMillis();
+        moml.executeBlockingErrorLocally(props);
+        final long end = System.currentTimeMillis();
+
+        assertThat(end - start).isGreaterThanOrEqualTo(2000).isLessThan(3000);
+        assertThat(MomlRule.extractBodyContent(outputReceiver.poll())).isEqualTo("2.0");
+    }
+
+    @Test
+    public void should_throw_expection_if_input_is_string_and_porDelay_is_true() {
+        final Map<String, String> props = new HashMap<String, String>();
+        props.put("DirBasic.Mock Mode", "false");
+        props.put("Constant.value", "azerty");
+        props.put(ACTOR_NAME + ".time(s)", "4.0");
+        props.put(ACTOR_NAME + ".take delay from port", "true");
+
+        try {
+            moml.executeBlockingErrorLocally(props);
+            fail("an excpetion should be thrown if take delay from port is true and the value send on port is not a string");
+
+        } catch (final PasserelleException e) {
+            assertThat(e).hasMessageContaining("azerty");
+        }
+
+    }
+}
