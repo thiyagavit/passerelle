c0bc6deae1b3170e8edddd7e22e80c65b26c7d75
kheuninck <kheuninck@gmail.com>
2012-08-01 08:16:22 +0000

    add historical data lookup

--- passerelle-core/com.isencia.passerelle.process.model/src/main/java/com/isencia/passerelle/process/model/service/ServiceRegistry.java
+++ passerelle-core/com.isencia.passerelle.process.model/src/main/java/com/isencia/passerelle/process/model/service/ServiceRegistry.java
@@ -5,49 +5,60 @@ package com.isencia.passerelle.process.model.service;
 
 import com.isencia.passerelle.process.model.factory.EntityFactory;
 import com.isencia.passerelle.process.model.factory.EntityManager;
+import com.isencia.passerelle.process.model.factory.HistoricalDataProvider;
 
 /**
  * @author "puidir"
- *
+ * 
  */
 public class ServiceRegistry {
 
-  private EntityFactory entityFactory;
-  private EntityManager entityManager;
-  
-  private static ServiceRegistry _instance = new ServiceRegistry();
-  
-  public static ServiceRegistry getInstance() {
-    return _instance;
-  }
+	private EntityFactory entityFactory;
+	private EntityManager entityManager;
+	private HistoricalDataProvider historicalDataProvider;
 
-  /**
-   * @return the entityFactory
-   */
-  public EntityFactory getEntityFactory() {
-    return entityFactory;
-  }
+	public HistoricalDataProvider getHistoricalDataProvider() {
+		return historicalDataProvider;
+	}
 
-  /**
-   * @param entityFactory the entityFactory to set
-   */
-  public void setEntityFactory(EntityFactory entityFactory) {
-    this.entityFactory = entityFactory;
-  }
+	public void setHistoricalDataProvider(HistoricalDataProvider historicalDataProvider) {
+		this.historicalDataProvider = historicalDataProvider;
+	}
 
-  /**
-   * @return the entityManager
-   */
-  public EntityManager getEntityManager() {
-    return entityManager;
-  }
+	private static ServiceRegistry _instance = new ServiceRegistry();
 
-  /**
-   * @param entityManager the entityManager to set
-   */
-  public void setEntityManager(EntityManager entityManager) {
-    this.entityManager = entityManager;
-  }
-  
-  
+	public static ServiceRegistry getInstance() {
+		return _instance;
+	}
+
+	/**
+	 * @return the entityFactory
+	 */
+	public EntityFactory getEntityFactory() {
+		return entityFactory;
+	}
+
+	/**
+	 * @param entityFactory
+	 *            the entityFactory to set
+	 */
+	public void setEntityFactory(EntityFactory entityFactory) {
+		this.entityFactory = entityFactory;
+	}
+
+	/**
+	 * @return the entityManager
+	 */
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	/**
+	 * @param entityManager
+	 *            the entityManager to set
+	 */
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
 }
