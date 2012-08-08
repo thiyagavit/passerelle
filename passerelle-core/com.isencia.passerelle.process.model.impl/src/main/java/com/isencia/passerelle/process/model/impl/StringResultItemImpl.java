40bcd3eeb1e9412a237180f0e294ebbdf603c0bb
kheuninck <kheuninck@gmail.com>
2012-08-08 14:32:28 +0000

    add mutable resultitem

--- passerelle-core/com.isencia.passerelle.process.model.impl/src/main/java/com/isencia/passerelle/process/model/impl/StringResultItemImpl.java
+++ passerelle-core/com.isencia.passerelle.process.model.impl/src/main/java/com/isencia/passerelle/process/model/impl/StringResultItemImpl.java
@@ -12,6 +12,7 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
+import com.isencia.passerelle.process.model.Mutable;
 import com.isencia.passerelle.process.model.ResultBlock;
 
 /**
@@ -20,7 +21,7 @@ import com.isencia.passerelle.process.model.ResultBlock;
  */
 @Entity
 @DiscriminatorValue("STRING_RESULT")
-public class StringResultItemImpl extends ResultItemImpl<String> {
+public class StringResultItemImpl extends ResultItemImpl<String> implements Mutable {
 
 	private static final long serialVersionUID = 1L;
 	private static final int MAX_CHAR_SIZE = 4000;
@@ -31,18 +32,19 @@ public class StringResultItemImpl extends ResultItemImpl<String> {
 
 	public StringResultItemImpl() {
 	}
-	
+
 	public StringResultItemImpl(ResultBlock resultBlock, String name, String value, String unit, Date creationTS) {
-		this(resultBlock, name, value,unit, creationTS,null);
- }
-	
-	public StringResultItemImpl(ResultBlock resultBlock, String name, String value, String unit, Date creationTS,Integer level) {
-		super(resultBlock, name, unit, creationTS,level);
+		this(resultBlock, name, value, unit, creationTS, null);
+	}
+
+	public StringResultItemImpl(ResultBlock resultBlock, String name, String value, String unit, Date creationTS,
+			Integer level) {
+		super(resultBlock, name, unit, creationTS, level);
 		setValue(value);
-  }
-		  
+	}
+
 	public StringResultItemImpl(ResultBlock resultBlock, String name, String value, Date creationTS) {
-		this(resultBlock, name, value, null, creationTS,null);
+		this(resultBlock, name, value, null, creationTS, null);
 	}
 
 	public StringResultItemImpl(ResultBlock resultBlock, String name, String value) {
@@ -62,15 +64,15 @@ public class StringResultItemImpl extends ResultItemImpl<String> {
 		return value;
 	}
 
-  public void setValue(String value) {
-    if (value != null && value.length() > MAX_CHAR_SIZE) {
-      this.clobItem = new ClobItem(value);
-    } else {
-      this.value = value;
-    }
-  }
+	public void setValue(String value) {
+		if (value != null && value.length() > MAX_CHAR_SIZE) {
+			this.clobItem = new ClobItem(value);
+		} else {
+			this.value = value;
+		}
+	}
 
-  /*
+	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see com.isencia.passerelle.process.model.ResultItem#getDataType()
