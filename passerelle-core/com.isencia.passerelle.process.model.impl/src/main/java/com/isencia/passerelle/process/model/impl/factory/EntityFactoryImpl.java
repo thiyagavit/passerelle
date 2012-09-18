017edd95d6b5311e57877e814510558546a15789
erwindl0 <erwindl0@gmail.com>
2012-09-18 11:22:51 +0000

    Incomplete - task : Finish integration/implementation of ErrorItem handling & ContextErrorEvent

--- passerelle-core/com.isencia.passerelle.process.model.impl/src/main/java/com/isencia/passerelle/process/model/impl/factory/EntityFactoryImpl.java
+++ passerelle-core/com.isencia.passerelle.process.model.impl/src/main/java/com/isencia/passerelle/process/model/impl/factory/EntityFactoryImpl.java
@@ -11,6 +11,7 @@ import com.isencia.passerelle.process.model.Case;
 import com.isencia.passerelle.process.model.Context;
 import com.isencia.passerelle.process.model.ContextErrorEvent;
 import com.isencia.passerelle.process.model.ContextEvent;
+import com.isencia.passerelle.process.model.ErrorItem;
 import com.isencia.passerelle.process.model.ErrorItem.Category;
 import com.isencia.passerelle.process.model.ErrorItem.Severity;
 import com.isencia.passerelle.process.model.Request;
@@ -34,127 +35,59 @@ import com.isencia.passerelle.process.model.impl.TaskImpl;
  */
 public class EntityFactoryImpl implements EntityFactory {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.isencia.passerelle.process.model.factory.EntityFactory#createCase
-	 * (java.lang.String)
-	 */
-	public Case createCase(String externalReference) {
-		return new CaseImpl();
-	}
+  public Case createCase(String externalReference) {
+    return new CaseImpl();
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.isencia.passerelle.process.model.factory.EntityFactory#createRequest
-	 * (com.isencia.passerelle.process.model.Case, java.lang.String,
-	 * java.lang.String)
-	 */
-	public Request createRequest(Case requestCase, String initiator, String category, String type, String correlationId) {
-		return new MainRequestImpl(requestCase, initiator, type, correlationId, category);
-	}
+  public Request createRequest(Case requestCase, String initiator, String category, String type, String correlationId) {
+    return new MainRequestImpl(requestCase, initiator, type, correlationId, category);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.isencia.passerelle.process.model.factory.EntityFactory#createTask
-	 * (com.isencia.passerelle.process.model.Context, java.lang.String,
-	 * java.lang.String)
-	 */
-	public Task createTask(Context parentContext, String initiator, String type) {
-		return new TaskImpl(parentContext, initiator, type);
-	}
+  public Task createTask(Context parentContext, String initiator, String type) {
+    return new TaskImpl(parentContext, initiator, type);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.isencia.passerelle.process.model.factory.EntityFactory#createResultBlock
-	 * (com.isencia.passerelle.process.model.Task, java.lang.String)
-	 */
-	public ResultBlock createResultBlock(Task task, String type) {
-		return new ResultBlockImpl(task, type);
-	}
+  public ResultBlock createResultBlock(Task task, String type) {
+    return new ResultBlockImpl(task, type);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.isencia.passerelle.process.model.factory.EntityFactory#createAttribute
-	 * (com.isencia.passerelle.process.model.AttributeHolder, java.lang.String,
-	 * java.lang.String)
-	 */
-	public Attribute createAttribute(AttributeHolder holder, String name, String value) {
-		if (holder == null) {
-			throw new IllegalArgumentException("AttributeHolder can not be null");
-		}
-		if (holder instanceof Request) {
-			return new RequestAttributeImpl((Request) holder, name, value);
-		} else if (holder instanceof ResultBlock) {
-			return new ResultBlockAttributeImpl((ResultBlock) holder, name, value);
-		} else if (holder instanceof ResultItem) {
+  public Attribute createAttribute(AttributeHolder holder, String name, String value) {
+    if (holder == null) {
+      throw new IllegalArgumentException("AttributeHolder can not be null");
+    }
+    if (holder instanceof Request) {
+      return new RequestAttributeImpl((Request) holder, name, value);
+    } else if (holder instanceof ResultBlock) {
+      return new ResultBlockAttributeImpl((ResultBlock) holder, name, value);
+    } else if (holder instanceof ResultItem) {
+      return new ResultItemAttributeImpl((ResultItem<?>) holder, name, value);
+    } else {
+      throw new IllegalArgumentException("Unknown AttributeHolder type " + holder.getClass());
+    }
+  }
 
-			return new ResultItemAttributeImpl((ResultItem<?>) holder, name, value);
-		} else {
-			throw new IllegalArgumentException("Unknown AttributeHolder type " + holder.getClass());
-		}
-	}
+  public ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit, Date date) {
+    return createResultItem(resultBlock, name, value, unit, null, date);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.isencia.passerelle.process.model.factory.EntityFactory#createResultItem
-	 * (com.isencia.passerelle.process.model.ResultBlock, java.lang.String,
-	 * java.lang.String, java.lang.String, java.util.Date)
-	 */
-	public ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit,
-			Date date) {
-		return createResultItem(resultBlock, name, value, unit, null, date);
+  public ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit) {
+    return createResultItem(resultBlock, name, value, unit, null, null);
+  }
 
-	}
+  public ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit, Integer level, Date date) {
+    return new StringResultItemImpl(resultBlock, name, value, unit, date, level);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.isencia.passerelle.process.model.factory.EntityFactory#createResultItem
-	 * (com.isencia.passerelle.process.model.ResultBlock, java.lang.String,
-	 * java.lang.String, java.lang.String)
-	 */
-	public ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit) {
-		return createResultItem(resultBlock, name, value, unit, null, null);
-	}
+  public ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit, Integer level) {
+    return createResultItem(resultBlock, name, value, unit, level, null);
+  }
 
-	public ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit,
-			Integer level, Date date) {
-
-		return new StringResultItemImpl(resultBlock, name, value, unit, date, level);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.isencia.passerelle.process.model.factory.EntityFactory#createResultItem
-	 * (com.isencia.passerelle.process.model.ResultBlock, java.lang.String,
-	 * java.lang.String, java.lang.String)
-	 */
-	public ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit,
-			Integer level) {
-		return createResultItem(resultBlock, name, value, unit, level, null);
-
-	}
-
-  /* (non-Javadoc)
-   * @see com.isencia.passerelle.process.model.factory.EntityFactory#createContextEvent(com.isencia.passerelle.process.model.Context, java.lang.String, java.lang.String)
-   */
   public ContextEvent createContextEvent(Context context, String topic, String message) {
     return new ContextEventImpl(context, topic, message);
+  }
+
+  public ContextErrorEvent createContextErrorEvent(Context context, ErrorItem errorItem) {
+    throw new UnsupportedOperationException();
   }
 
   public ContextErrorEvent createContextErrorEvent(Context context, Severity severity, Category category, String code, String shortDescription, String description) {
@@ -165,4 +98,11 @@ public class EntityFactoryImpl implements EntityFactory {
     throw new UnsupportedOperationException();
   }
 
+  public ErrorItem createErrorItem(Severity severity, Category category, String code, String shortDescription, Throwable cause) {
+    throw new UnsupportedOperationException();
+  }
+
+  public ErrorItem createErrorItem(Severity severity, Category category, String code, String shortDescription, String description) {
+    throw new UnsupportedOperationException();
+  }
 }
