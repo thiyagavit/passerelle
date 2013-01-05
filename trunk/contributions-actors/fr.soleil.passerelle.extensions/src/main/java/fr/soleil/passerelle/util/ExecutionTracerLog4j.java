fee56b02156580bbb3d86c513ed32fef187d86e2
erwindl0 <erwindl0@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-01-05 15:43:38 +0000

    Adapt to change in ExecutionTracer interface

--- trunk/contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/util/ExecutionTracerLog4j.java
+++ trunk/contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/util/ExecutionTracerLog4j.java
@@ -2,14 +2,12 @@ package fr.soleil.passerelle.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import ptolemy.actor.Actor;
 import ptolemy.actor.Director;
 import ptolemy.kernel.util.NamedObj;
-
 import com.isencia.passerelle.ext.ExecutionTracer;
+import com.isencia.passerelle.util.Level;
 
-@SuppressWarnings("serial")
 public class ExecutionTracerLog4j implements ExecutionTracer {
 
 	private final static Logger traceLogger = LoggerFactory.getLogger("trace");
@@ -22,4 +20,14 @@ public class ExecutionTracerLog4j implements ExecutionTracer {
 	public void trace(final Director source, final String message) {
 		traceLogger.info(source.getName() + " - " + message);
 	}
+
+  @Override
+  public void trace(Actor source, String message, Level level) {
+    trace(source, message);
+  }
+
+  @Override
+  public void trace(Director source, String message, Level level) {
+    trace(source, message);
+  }
 }
