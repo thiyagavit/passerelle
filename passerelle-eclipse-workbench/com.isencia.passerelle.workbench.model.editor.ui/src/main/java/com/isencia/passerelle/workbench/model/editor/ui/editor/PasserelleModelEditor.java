0dab0fde7b1e45a6f5281124e5047f87c343c685
kheuninck <kheuninck@gmail.com>
2013-01-11 16:44:58 +0000

    cleanup + add commit and update

--- passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/editor/PasserelleModelEditor.java
+++ passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/editor/PasserelleModelEditor.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.EventObject;
 import java.util.List;
 import java.util.Map;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResourceChangeEvent;
@@ -14,6 +15,7 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.draw2d.Figure;
 import org.eclipse.draw2d.FigureCanvas;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.LineBorder;
@@ -60,6 +62,7 @@ import org.eclipse.gef.ui.rulers.RulerComposite;
 import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.SWT;
@@ -67,11 +70,13 @@ import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DropTarget;
 import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
@@ -86,12 +91,15 @@ import org.eclipse.ui.views.properties.tabbed.ITabbedPropertySheetPageContributo
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import ptolemy.actor.CompositeActor;
 import ptolemy.kernel.ComponentEntity;
+
 import com.isencia.passerelle.workbench.model.editor.ui.WorkbenchUtility;
 import com.isencia.passerelle.workbench.model.editor.ui.dnd.FileTransferDropTargetListener;
 import com.isencia.passerelle.workbench.model.editor.ui.dnd.PasserelleTemplateTransferDropTargetListener;
 import com.isencia.passerelle.workbench.model.editor.ui.editor.actions.CloseEditorAction;
+import com.isencia.passerelle.workbench.model.editor.ui.editor.actions.CommitFlowAction;
 import com.isencia.passerelle.workbench.model.editor.ui.editor.actions.CopyNodeAction;
 import com.isencia.passerelle.workbench.model.editor.ui.editor.actions.CreateSubModelAction;
 import com.isencia.passerelle.workbench.model.editor.ui.editor.actions.CutNodeAction;
@@ -102,7 +110,9 @@ import com.isencia.passerelle.workbench.model.editor.ui.editor.actions.PasteNode
 import com.isencia.passerelle.workbench.model.editor.ui.editor.actions.RenameAction;
 import com.isencia.passerelle.workbench.model.editor.ui.editor.actions.RouterFactory;
 import com.isencia.passerelle.workbench.model.editor.ui.editor.actions.ScreenshotAction;
+import com.isencia.passerelle.workbench.model.editor.ui.editor.actions.UpdateFlowAction;
 import com.isencia.passerelle.workbench.model.editor.ui.editor.actions.ViewAttributesAction;
+import com.isencia.passerelle.workbench.model.editor.ui.editpart.AbstractBaseEditPart;
 import com.isencia.passerelle.workbench.model.editor.ui.editpart.EditPartFactory;
 import com.isencia.passerelle.workbench.model.editor.ui.palette.PaletteBuilder;
 import com.isencia.passerelle.workbench.model.editor.ui.views.DropFavouriteListener;
@@ -507,6 +517,15 @@ public class PasserelleModelEditor extends    GraphicalEditorWithFlyoutPalette
 		registry.registerAction(subModelAction);
 		getSelectionActions().add(subModelAction.getId());
 		
+//		CommitFlowAction commitFlow = new CommitFlowAction(this,getParent());
+//		commitFlow.setText("Commit flow");
+//		registry.registerAction(commitFlow);
+//		getSelectionActions().add(commitFlow.getId());
+//		UpdateFlowAction updateFlow = new UpdateFlowAction(this,getParent());
+//		updateFlow.setText("Update flow");
+//		registry.registerAction(updateFlow);
+//		getSelectionActions().add(updateFlow.getId());
+//		
 		final IToolBarManager man = getEditorSite().getActionBars().getToolBarManager();
 		final Separator sep = new Separator(getClass().getName() + ".openActions");
 		if (man.find(sep.getId()) == null) man.add(sep);
@@ -594,11 +613,9 @@ public class PasserelleModelEditor extends    GraphicalEditorWithFlyoutPalette
 			final ScopedPreferenceStore store = new ScopedPreferenceStore(new InstanceScope(),"org.dawb.workbench.ui");
 			final boolean isSel = store.getBoolean("org.dawb.actor.highlight.choice");
 			final ActionContributionItem action = (ActionContributionItem)getEditorSite().getActionBars().getToolBarManager().find("org.edna.workbench.editors.highlightWorkflowAction");
-			if(action!=null) {
-			  action.getAction().setChecked(isSel);
-			}
+			action.getAction().setChecked(isSel);
 		} catch (Throwable ne) {
-			logger.error("Cannot read dawb-specific preferences in generic Passerelle Workbench!");
+			logger.error("Cannot read dawb preferences because this is not dawb!");
 		}
 	}
 
