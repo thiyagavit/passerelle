a593685a7c160a5b28ac4fa9b1a253b14fd26388
kheuninck <kheuninck@gmail.com>
2013-01-11 16:43:28 +0000

    charge submodels from project api

--- passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/editor/actions/CreateSubModelAction.java
+++ passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/editor/actions/CreateSubModelAction.java
@@ -1,10 +1,12 @@
 package com.isencia.passerelle.workbench.model.editor.ui.editor.actions;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.gef.ui.actions.SelectionAction;
@@ -35,204 +37,172 @@ import com.isencia.passerelle.workbench.model.ui.utils.FileUtils;
 import com.isencia.passerelle.workbench.model.ui.wizards.NameChecker;
 import com.isencia.passerelle.workbench.model.ui.wizards.NameWizard;
 import com.isencia.passerelle.workbench.model.utils.ModelUtils;
-import com.isencia.passerelle.workbench.model.utils.SubModelUtils;
 
 public class CreateSubModelAction extends SelectionAction implements NameChecker {
-	
-	private static final Logger logger = LoggerFactory.getLogger(CreateSubModelAction.class);
-	
-	private PasserelleModelMultiPageEditor parent;
-	private final String icon = "icons/flow.png";
-	public static String CREATE_SUBMODEL = "createSubModel";
 
-	/**
-	 * Creates an empty model
-	 * 
-	 * @param part
-	 */
-	public CreateSubModelAction() {
-		this(null, null);
-		setId(ActionFactory.NEW.getId());
-	}
+  private static final Logger logger = LoggerFactory.getLogger(CreateSubModelAction.class);
 
-	/**
-	 * Creates an empty model
-	 * 
-	 * @param part
-	 */
-	public CreateSubModelAction(final IEditorPart part) {
-		this(part, null);
-		setId(ActionFactory.NEW.getId());
-	}
+  private PasserelleModelMultiPageEditor parent;
+  private final String icon = "icons/flow.png";
+  public static String CREATE_SUBMODEL = "createSubModel";
 
-	/**
-	 * Creates the model from the contents of the part
-	 * 
-	 * @param part
-	 * @param parent
-	 */
-	public CreateSubModelAction(final IEditorPart part, final PasserelleModelMultiPageEditor parent) {
-		super(part);
-		this.parent = parent;
-		setLazyEnablementCalculation(true);
-		if (parent != null)
-			setId(ActionFactory.EXPORT.getId());
-	}
+  /**
+   * Creates an empty model
+   * 
+   * @param part
+   */
+  public CreateSubModelAction() {
+    this(null, null);
+    setId(ActionFactory.NEW.getId());
+  }
 
-	@Override
-	protected void init() {
-		super.init();
-		Activator.getImageDescriptor(icon);
-		setHoverImageDescriptor(Activator.getImageDescriptor(icon));
-		setImageDescriptor(Activator.getImageDescriptor(icon));
-		setDisabledImageDescriptor(Activator.getImageDescriptor(icon));
-		setEnabled(false);
+  /**
+   * Creates an empty model
+   * 
+   * @param part
+   */
+  public CreateSubModelAction(final IEditorPart part) {
+    this(part, null);
+    setId(ActionFactory.NEW.getId());
+  }
 
-	}
+  /**
+   * Creates the model from the contents of the part
+   * 
+   * @param part
+   * @param parent
+   */
+  public CreateSubModelAction(final IEditorPart part, final PasserelleModelMultiPageEditor parent) {
+    super(part);
+    this.parent = parent;
+    setLazyEnablementCalculation(true);
+    if (parent != null)
+      setId(ActionFactory.EXPORT.getId());
+  }
 
-	@Override
-	public void run() {
-		try {
-			if (parent != null) {
-				final Entity entity = parent.getSelectedContainer();
-				final String name = getName(entity.getName());
-				if (name != null) {
-					entity.setName(name);
-					exportEntityToClassFile(entity);
-					if (parent != null && parent.getActorTreeViewPage() != null)
-						parent.getActorTreeViewPage().refresh();
-				}
-			} else {
-				final String name = getName("emptyComposite");
-				if (name != null) {
-					final IProject pass = ModelUtils.getPasserelleProject();
-					final File file = new File(pass.getLocation().toOSString() + "/" + name + ".moml");
-					final InputStream stream = ModelUtils.getEmptyCompositeStream(file.getAbsolutePath());
-					FileUtils.write(stream, new FileOutputStream(file));
+  @Override
+  protected void init() {
+    super.init();
+    Activator.getImageDescriptor(icon);
+    setHoverImageDescriptor(Activator.getImageDescriptor(icon));
+    setImageDescriptor(Activator.getImageDescriptor(icon));
+    setDisabledImageDescriptor(Activator.getImageDescriptor(icon));
+    setEnabled(false);
 
-					PaletteItemFactory factory = PaletteItemFactory.getInstance();
-					Flow flow = FlowManager.readMoml(new FileReader(file));
-					flow.setName(name);
-					factory.addSubModel(flow);
-					SubModelUtils.addSubModel(flow);
+  }
 
-					final IViewPart part = EclipseUtils.getPage().findView(ActorTreeView.ID);
-					if (part != null && part instanceof ActorTreeView) {
-						((ActorTreeView) part).refresh();
-					}
-				}
-			}
+  @Override
+  public void run() {
+    try {
+      if (parent != null) {
+        final Entity entity = parent.getSelectedContainer();
+        final String name = getName(entity.getName());
+        if (name != null) {
+          entity.setName(name);
+          exportEntityToClassFile(entity);
+          if (parent != null && parent.getActorTreeViewPage() != null)
+            parent.getActorTreeViewPage().refresh();
+        }
+      } else {
+        final String name = getName("emptyComposite");
+        if (name != null) {
+          final InputStream stream = ModelUtils.getEmptyCompositeStream(name);
+          Reader reader = new InputStreamReader(stream);
+          try {
+            Flow flow = FlowManager.readMoml(reader);
+            Attribute ctrlFact = flow.getAttribute("_controllerFactory");
+            if (ctrlFact == null) {
+              new PasserelleActorControllerFactory(flow, "_controllerFactory");
+            }
+            Attribute editorFact = flow.getAttribute("_editorFactory");
+            if (editorFact == null) {
+              new PasserelleEditorFactory(flow, "_editorFactory");
+            }
+            Attribute editorPaneFact = flow.getAttribute("_editorPaneFactory");
+            if (editorPaneFact == null) {
+              new PasserelleEditorPaneFactory(flow, "_editorPaneFactory");
+            }
+            PaletteItemFactory factory = PaletteItemFactory.getInstance();
+            factory.addSubModel(flow);
+            Activator.getDefault().getRepositoryService().createSubmodel(flow);
+          } catch (Exception e) {
 
-		} catch (Exception e) {
-			logger.error("Cannot export sub-model", e);
-		}
-	}
+          }
+          final IViewPart part = EclipseUtils.getPage().findView(ActorTreeView.ID);
+          if (part != null && part instanceof ActorTreeView) {
+            ((ActorTreeView) part).refresh();
+          }
+        }
+      }
 
-	private String getName(final String name) {
+    } catch (Exception e) {
+      logger.error("Cannot export sub-model", e);
+    }
+  }
 
-		NameWizard wizard = new NameWizard(name, this);
-		WizardDialog dialog = new WizardDialog(Display.getCurrent().getActiveShell(), wizard);
-		dialog.create();
-		dialog.getShell().setSize(400, 300);
-		dialog.setTitle("Name of Composite");
-		dialog.setMessage("Please choose a unique name for your exported composite.");
-		if (dialog.open() == WizardDialog.OK) {
-			return wizard.getRenameValue();
-		}
-		return null;
-	}
+  private String getName(final String name) {
 
-	public Entity exportEntityToClassFile(Entity entity) throws Exception {
+    NameWizard wizard = new NameWizard(name, this);
+    WizardDialog dialog = new WizardDialog(Display.getCurrent().getActiveShell(), wizard);
+    dialog.create();
+    dialog.getShell().setSize(400, 300);
+    dialog.setTitle("Name of Composite");
+    dialog.setMessage("Please choose a unique name for your exported composite.");
+    if (dialog.open() == WizardDialog.OK) {
+      return wizard.getRenameValue();
+    }
+    return null;
+  }
 
-		Entity entityAsClass = (Entity) entity.clone(entity.workspace());
-		entityAsClass.setClassDefinition(true);
+  public Entity exportEntityToClassFile(Entity entity) throws Exception {
+    CompositeActor entityAsClass = (CompositeActor) entity.clone();
+    entityAsClass.setClassDefinition(true);
 
-		if (entityAsClass instanceof CompositeActor) {
-			CompositeActor compActor = ((CompositeActor) entityAsClass);
-			Director d = compActor.getDirector();
-			if (d != null) {
-				// remove the director from the class definition
-				d.setContainer(null);
-			}
+    Director d = entityAsClass.getDirector();
+    if (d != null) {
+      // remove the director from the class definition
+      d.setContainer(null);
+    }
 
-			Attribute ctrlFact = compActor.getAttribute("_controllerFactory");
-			if (ctrlFact == null) {
-				new PasserelleActorControllerFactory(compActor,
-						"_controllerFactory");
-			} else if (!(ctrlFact instanceof PasserelleActorControllerFactory)) {
-				ctrlFact.setContainer(null);
-				new PasserelleActorControllerFactory(compActor,
-						"_controllerFactory");
-			}
-			Attribute editorFact = compActor.getAttribute("_editorFactory");
-			if (editorFact == null) {
-				new PasserelleEditorFactory(compActor, "_editorFactory");
-			} else if (!(editorFact instanceof PasserelleEditorFactory)) {
-				editorFact.setContainer(null);
-				new PasserelleEditorFactory(compActor, "_editorFactory");
-			}
-			Attribute editorPaneFact = compActor
-					.getAttribute("_editorPaneFactory");
-			if (editorPaneFact == null) {
-				new PasserelleEditorPaneFactory(compActor, "_editorPaneFactory");
-			} else if (!(editorPaneFact instanceof PasserelleEditorPaneFactory)) {
-				editorPaneFact.setContainer(null);
-				new PasserelleEditorPaneFactory(compActor, "_editorPaneFactory");
-			}
-		}
+    Attribute ctrlFact = entityAsClass.getAttribute("_controllerFactory");
+    if (ctrlFact == null) {
+      new PasserelleActorControllerFactory(entityAsClass, "_controllerFactory");
+    }
+    Attribute editorFact = entityAsClass.getAttribute("_editorFactory");
+    if (editorFact == null) {
+      new PasserelleEditorFactory(entityAsClass, "_editorFactory");
+    }
+    Attribute editorPaneFact = entityAsClass.getAttribute("_editorPaneFactory");
+    if (editorPaneFact == null) {
+      new PasserelleEditorPaneFactory(entityAsClass, "_editorPaneFactory");
+    }
 
-		final IProject pass = ModelUtils.getPasserelleProject();
-		final File file = new File(pass.getLocation().toOSString() + "/" + entityAsClass.getName() + ".moml");
-		String name = entityAsClass.getName();
-		String filename = file.getName();
-		int period = filename.indexOf(".");
-		if (period > 0) {
-			name = filename.substring(0, period);
-		} else {
-			name = filename;
-		}
+    PaletteItemFactory factory = PaletteItemFactory.getInstance();
 
-		FileWriter fileWriter = new FileWriter(file);
-		try {
-			if (entityAsClass.getContainer() != null) {
-				// in this case the exportMoML below does not add the xml
-				// header itself
-				// if the entity is a top-level one,without container the
-				// exportMoML does add it
-				fileWriter.write("<?xml version=\"1.0\" standalone=\"no\"?>\n"
-						+ "<!DOCTYPE " + entityAsClass.getElementName()
-						+ " PUBLIC " + "\"-//UC Berkeley//DTD MoML 1//EN\"\n"
-						+ "    \"http://ptolemy.eecs.berkeley.edu"
-						+ "/xml/dtd/MoML_1.dtd\">\n");
-			}
-			entityAsClass.exportMoML(fileWriter, 0, name);
-		} finally {
-			fileWriter.close();
-		}
-		PaletteItemFactory factory = PaletteItemFactory.getInstance();
-		Flow flow = FlowManager.readMoml(new FileReader(file));
-		factory.addSubModel(flow);
-		SubModelUtils.addSubModel(flow);
+    Activator.getDefault().getRepositoryService().createSubmodel(entityAsClass);
+    Flow flow = Activator.getDefault().getRepositoryService().getSubmodel(entityAsClass.getName());
+    factory.addSubModel(flow);
+    return entityAsClass;
+  }
 
-		return entityAsClass;
-	}
+  @Override
+  protected boolean calculateEnabled() {
+    // TODO Auto-generated method stub
+    return true;
+  }
 
-	@Override
-	protected boolean calculateEnabled() {
-		// TODO Auto-generated method stub
-		return true;
-	}
+  public boolean isSubModel(String name) {
+    return Activator.getDefault().getRepositoryService().getSubmodel(name) != null;
+  }
 
+  public boolean isNameValid(final String name) {
+    return !isSubModel(name);
+  }
 
-	public boolean isNameValid(final String name) {
-		return !SubModelUtils.isSubModel(name);
-	}
-
-
-	public String getErrorMessage(String name) {
-		if (SubModelUtils.isSubModel(name)) {
-			return "'" + name + "' is already existing as a composite.";
-		}
-		return null;
-	}
+  public String getErrorMessage(String name) {
+    if (isSubModel(name)) {
+      return "'" + name + "' is already existing as a composite.";
+    }
+    return null;
+  }
 }
