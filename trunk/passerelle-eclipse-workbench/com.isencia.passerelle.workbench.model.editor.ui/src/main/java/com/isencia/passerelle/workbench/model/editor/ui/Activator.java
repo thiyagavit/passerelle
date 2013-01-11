0fbad6a528084c116cdf7929fb657dc7b769db47
kheuninck@gmail.com <kheuninck@gmail.com@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-01-11 16:47:11 +0000

    charge submodels from project api

--- trunk/passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/Activator.java
+++ trunk/passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/Activator.java
@@ -3,62 +3,86 @@ package com.isencia.passerelle.workbench.model.editor.ui;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.util.tracker.ServiceTracker;
+
+import com.isencia.passerelle.ext.ActorOrientedClassProvider;
+import com.isencia.passerelle.project.repository.api.RepositoryService;
 
 /**
  * The activator class controls the plug-in life cycle
  */
 public class Activator extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "com.isencia.passerelle.workbench.model.editor.ui";
+  // The plug-in ID
+  public static final String PLUGIN_ID = "com.isencia.passerelle.workbench.model.editor.ui";
 
-	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
+  // The shared instance
+  private static Activator plugin;
+  private ServiceTracker repoSvcTracker;
+  private ServiceRegistration submodelSvcReg;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
+  /**
+   * The constructor
+   */
+  public Activator() {
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext )
+   */
+  public void start(BundleContext context) throws Exception {
+    super.start(context);
+    plugin = this;
+    repoSvcTracker = new ServiceTracker(context, RepositoryService.class.getName(), null);
+    repoSvcTracker.open();
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
+    submodelSvcReg = context.registerService(ActorOrientedClassProvider.class.getName(), new SubmodelProvider(), null);
 
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return getImageDescriptor(PLUGIN_ID, path);
-	}
-	public static ImageDescriptor getImageDescriptor(String plugin,String path) {
-		return imageDescriptorFromPlugin(plugin, path);
-	}
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext )
+   */
+  public void stop(BundleContext context) throws Exception {
+    plugin = null;
+    super.stop(context);
+    repoSvcTracker.close();
+  }
+
+  /**
+   * Returns the shared instance
+   * 
+   * @return the shared instance
+   */
+  public static Activator getDefault() {
+    return plugin;
+  }
+
+  /**
+   * Returns an image descriptor for the image file at the given plug-in relative path
+   * 
+   * @param path
+   *          the path
+   * @return the image descriptor
+   */
+  public static ImageDescriptor getImageDescriptor(String path) {
+    return getImageDescriptor(PLUGIN_ID, path);
+  }
+
+  public static ImageDescriptor getImageDescriptor(String plugin, String path) {
+    return imageDescriptorFromPlugin(plugin, path);
+  }
+
+  public RepositoryService getRepositoryService() {
+    try {
+      return (RepositoryService) (repoSvcTracker != null ? repoSvcTracker.waitForService(3000) : null);
+    } catch (InterruptedException e) {
+      return null;
+    }
+  }
 }
