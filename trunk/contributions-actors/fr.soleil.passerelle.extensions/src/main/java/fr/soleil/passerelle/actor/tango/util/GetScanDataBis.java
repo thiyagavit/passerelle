ce6ffadaee34c61d8286e8abf0d51bf9df51b7db
sandra.pierrejoseph@synchrotron-soleil.fr <sandra.pierrejoseph@synchrotron-soleil.fr@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-02-08 08:05:27 +0000

    Bug 17602 : récupérer la trajectoire

--- trunk/contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/actor/tango/util/GetScanDataBis.java
+++ trunk/contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/actor/tango/util/GetScanDataBis.java
@@ -1,281 +1,190 @@
 package fr.soleil.passerelle.actor.tango.util;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ptolemy.data.expr.Parameter;
-import ptolemy.data.expr.StringParameter;
 import ptolemy.kernel.CompositeEntity;
-import ptolemy.kernel.util.Attribute;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
 
 import com.isencia.passerelle.actor.InitializationException;
 import com.isencia.passerelle.actor.ProcessingException;
-import com.isencia.passerelle.actor.v3.ActorContext;
-import com.isencia.passerelle.actor.v3.ProcessRequest;
-import com.isencia.passerelle.actor.v3.ProcessResponse;
 import com.isencia.passerelle.core.Port;
-import com.isencia.passerelle.core.PortFactory;
 import com.isencia.passerelle.util.ExecutionTracerService;
 
-import fr.esrf.Tango.AttrDataFormat;
 import fr.esrf.Tango.DevFailed;
 import fr.soleil.passerelle.actor.PortUtilities;
-import fr.soleil.passerelle.actor.TransformerV3;
-import fr.soleil.passerelle.actor.tango.acquisition.scan.ScanUtil;
-import fr.soleil.passerelle.util.DevFailedProcessingException;
 import fr.soleil.passerelle.util.PasserelleUtil;
 import fr.soleil.passerelle.util.ProcessingExceptionWithLog;
 import fr.soleil.salsa.entity.IActuator;
 import fr.soleil.salsa.entity.IScanResult;
 import fr.soleil.salsa.entity.ISensor;
-import fr.soleil.salsa.entity.scan2D.IScanResult2D;
-import fr.soleil.salsa.exception.SalsaDeviceException;
-import fr.soleil.salsa.exception.SalsaScanConfigurationException;
 import fr.soleil.tango.clientapi.TangoAttribute;
 
 /**
  * Actuators and Sensors extraction thank's to their number
- *
+ * 
  * @author GRAMER
  */
 @SuppressWarnings("serial")
-public class GetScanDataBis extends TransformerV3 {
+public class GetScanDataBis extends AbstractGetScanData {
 
-	private final static Logger logger = LoggerFactory
-			.getLogger(GetScanDataBis.class);
+    private final static Logger logger = LoggerFactory.getLogger(GetScanDataBis.class);
 
-	private static final String SENSOR = "sensor";
-	private static final String ACTUATOR = "actuator";
+    private int actuatorsNb = 0;
 
-	public Parameter actuatorsParam;
-	private int actuatorsNb = 0;
+    private int sensorsNb = 0;
 
-	public Parameter sensorsParam;
-	private int sensorsNb = 0;
+    private final List<String> actuatorsSources = new ArrayList<String>();
+    private final List<String> sensorsSources = new ArrayList<String>();
 
-	private final List<Port> actuatorsPorts = Collections
-			.synchronizedList(new ArrayList<Port>());
-	private final List<Port> sensorsPorts = Collections
-			.synchronizedList(new ArrayList<Port>());
-	private final List<String> actuatorsSources = new ArrayList<String>();
-	private final List<String> sensorsSources = new ArrayList<String>();
+    public GetScanDataBis(CompositeEntity container, String name) throws NameDuplicationException,
+            IllegalActionException {
+        super(container, name);
+    }
 
-	public GetScanDataBis(final CompositeEntity container, final String name)
-			throws NameDuplicationException, IllegalActionException {
-		super(container, name);
+    @Override
+    protected void doInitialize() throws InitializationException {
+        super.doInitialize();
+        actuatorsSources.clear();
+        sensorsSources.clear();
+    }
 
-		actuatorsParam = new StringParameter(this, "Actuators Nr");
-		actuatorsParam.setExpression("0");
+    @Override
+    protected void realProcessImpl(final IScanResult res, final List<IActuator> actuatorList,
+            final List<ISensor> sensorList) throws ProcessingException, DevFailed {
 
-		sensorsParam = new StringParameter(this, "Sensors Nr");
-		sensorsParam.setExpression("0");
+        if (actuatorList.size() != actuatorsNb) {
+            throw new ProcessingExceptionWithLog(this,
+                    "Actuator number port is incorect. Correct number is " + actuatorList.size(),
+                    this, null);
+        }
+        if (sensorList.size() != sensorsNb) {
+            throw new ProcessingExceptionWithLog(this,
+                    "Sensor number port is incorect. Correct number is " + sensorList.size(), this,
+                    null);
+        }
 
-		output.setName("timestamps");
-	}
+        // output sensor and actuators
+        final List<Port> orderedActuatorPorts = PortUtilities.getOrderedOutputPorts(this, ACTUATOR,
+                0);
+        final Iterator<IActuator> iteratorAct = actuatorList.iterator();
+        String realScanDataName;
+        String scanDataName;
+        int i = 0;
+        IActuator iact;
+        if (isTrajectoryValue) {
 
-	@Override
-	protected void doInitialize() throws InitializationException {
-		super.doInitialize();
-		actuatorsSources.clear();
-		sensorsSources.clear();
-//		if (!isMockMode()) {
-//			ScanUtil.getCurrentSalsaApi();
-//		}
-	}
+            Map<String, double[]> realTrajectoryValues = super.getRealTrajectory(res);            
+            while (iteratorAct.hasNext()) {
+                iact = iteratorAct.next();
+                realScanDataName = iact.getScanServerAttributeName();
+                scanDataName = iact.getName();
+                double[] trajectory = realTrajectoryValues.get(scanDataName);
 
-	@Override
-	protected void process(ActorContext ctxt, ProcessRequest request,
-			ProcessResponse response) throws ProcessingException {
-		// TODO Auto-generated method stub
+                if (trajectory != null) {
+                    System.out.println("trajectory " + Arrays.toString(trajectory));
 
-		if (isMockMode()) {
-			final List<Port> outputPortList = outputPortList();
-			for (final Port port : outputPortList) {
-				if (port.getName().startsWith(SENSOR)
-						|| port.getName().startsWith(ACTUATOR)) {
-					ExecutionTracerService.trace(this,
-							"MOCK - reading data for " + port.getName());
-				}
-				sendOutputMsg(port, PasserelleUtil.createContentMessage(this,
-						"1,2,3"));
-			}
-		} else {
-			try {
+                    logger.debug("source act name " + realScanDataName + " for attribute " + i + " : " + scanDataName);
+                
+                    // read attribute is done by the TangoAttribute constructor
+                    ExecutionTracerService.trace(this, "reading trajectory for actuator " + i + " : " + scanDataName);
+                    sendOutputMsg(orderedActuatorPorts.get(i),
+                            PasserelleUtil.createContentMessage(this, trajectory));
+                }
+                else {
+                    sendOutputMsg(orderedActuatorPorts.get(i),
+                            PasserelleUtil.createContentMessage(this, new double[0]));
+                    ExecutionTracerService.trace(this, "No reading trajectory for actuator "
+                            + scanDataName);
+                }
+                i++;
 
-				final IScanResult res = ScanUtil.getCurrentSalsaApi().readScanResult();
-				final List<IActuator> actuatorList = res.getActuatorsXList();
-				final List<ISensor> sensorList = res.getSensorsList();
+            }
+        }
+        else {
+            while (iteratorAct.hasNext()) {               
+                iact = iteratorAct.next();
+                realScanDataName = iact.getScanServerAttributeName();
+                scanDataName = iact.getName();
+                logger.debug("source act name " + realScanDataName);
+                final TangoAttribute attr = new TangoAttribute(realScanDataName);
+                // read attribute is done by the TangoAttribute constructor
+                ExecutionTracerService.trace(this, "reading data for actuator " + i + " : " + scanDataName);
+                sendOutputMsg(orderedActuatorPorts.get(i),
+                        PasserelleUtil.createContentMessage(this, attr));
+                i++;
 
-				if (actuatorList.size() != actuatorsNb) {
-					throw new ProcessingExceptionWithLog(this,
-							"Actuator number port is incorect. Correct number is "
-									+ actuatorList.size(), this, null);
-				}
-				if (sensorList.size() != sensorsNb) {
-					throw new ProcessingExceptionWithLog(this,
-							"Sensor number port is incorect. Correct number is "
-									+ sensorList.size(), this, null);
-				}
+            }
+        }
 
-				// if scan is a 2D scan we add Y actuator at the end of actuator
-				// list.
-				if (res.getResultType() == IScanResult.ResultType.RESULT_2D) {
-					final IScanResult2D res2D = (IScanResult2D) res;
-					actuatorList.addAll(res2D.getActuatorsYList());
-				}
+        final List<Port> orderedSensorPorts = PortUtilities.getOrderedOutputPorts(this, SENSOR, 0);
+        final Iterator<ISensor> iteratorSen = sensorList.iterator();
+        i = 0;
+        ISensor sens;
+        while (iteratorSen.hasNext()) {
+            sens = iteratorSen.next();
+            realScanDataName = sens.getScanServerAttributeName();
+            scanDataName = sens.getName();
+            logger.debug("source sensor name " + realScanDataName);
 
-				// output timestamps
-				final TangoAttribute timestamps = new TangoAttribute(res.getSensorsTimeStampsCompleteName());
-						//ScanUtil.getCurrentContext().getScanServerName()+ "/sensorsTimestamps");
-				// read attribute is done by the TangoAttribute constructor
-				ExecutionTracerService.trace(this,
-						"reading data for sensorTimestamps ");
-				response.addOutputMessage(0, output, PasserelleUtil
-						.createContentMessage(this, timestamps));
+            final TangoAttribute sensor = new TangoAttribute(realScanDataName);
+            // read attribute is done by the TangoAttribute
+            // constructor
+            //System.err.println(sensor.getDataType() == AttrDataFormat._IMAGE);
+            ExecutionTracerService.trace(this, "reading data for sensor " + i + " : " + scanDataName);
+            sendOutputMsg(orderedSensorPorts.get(i),
+                    PasserelleUtil.createContentMessage(this, sensor));
+            i++;
+        }
 
-				// output sensor and actuators
-				final List<Port> orderedActuatorPorts = PortUtilities
-						.getOrderedOutputPorts(this, ACTUATOR, 0);
-				final List<Port> orderedSensorPorts = PortUtilities
-						.getOrderedOutputPorts(this, SENSOR, 0);
-				final Iterator<IActuator> iteratorAct = actuatorList.iterator();
-				int i = 0;
-				while (iteratorAct.hasNext()) {
-					final String scanDataName = iteratorAct.next()
-							.getScanServerAttributeName();
-					logger.debug("source act name " + scanDataName);
-					final TangoAttribute attr = new TangoAttribute(scanDataName);
-					// read attribute is done by the TangoAttribute constructor
-					ExecutionTracerService.trace(this,
-							"reading data for actuator " + i);
-					response.addOutputMessage(i, orderedActuatorPorts.get(i),
-							PasserelleUtil.createContentMessage(this, attr));
-					i++;
-				}
+        super.sendTimestampsOnOutputPort(res.getActuatorsTimeStampsCompleteName());
 
-				i = 0;
-				final Iterator<ISensor> iteratorSen = sensorList.iterator();
-				while (iteratorSen.hasNext()) {
-					final String scanDataName = iteratorSen.next()
-							.getScanServerAttributeName();
-					logger.debug("source sensor name " + scanDataName);
+    }
 
-					final TangoAttribute sensor = new TangoAttribute(
-							scanDataName);
-					// read attribute is done by the TangoAttribute
-					// constructor
-					System.err
-							.println(sensor.getDataType() == AttrDataFormat._IMAGE);
-					ExecutionTracerService.trace(this,
-							"reading data for sensor " + i);
-					response.addOutputMessage(i, orderedSensorPorts.get(i),
-							PasserelleUtil.createContentMessage(this, sensor));
-					i++;
-				}
+    @Override
+    protected String getActuatorParamName() {
+        return "Actuators Nr";
+    }
 
-			} catch (final SalsaDeviceException e) {
-				throw new ProcessingException(e.getMessage(), this, e);
-			} catch (final DevFailed e) {
-				throw new DevFailedProcessingException(e, this);
-			} catch (final SalsaScanConfigurationException e) {
-				throw new ProcessingException(e.getMessage(), this, e);
-			}
-		}
-	}
+    @Override
+    protected String getSensorParamName() {
+        return "Sensors Nr";
+    }
 
-	@Override
-	public void attributeChanged(final Attribute attribute)
-			throws IllegalActionException {
-		if (attribute == actuatorsParam) {
-			actuatorsNb = PasserelleUtil.getParameterIntValue(actuatorsParam);
-			final int nrPorts = actuatorsPorts.size();
-			// System.out.println(actuatorsString);
-			final int newPortCount = actuatorsNb;
-			// System.out.println("actuactors port nr futur:"+newPortCount);
-			// System.out.println("actuactors port nr actual:"+nrPorts);
-			// remove no more needed ports
-			if (newPortCount < nrPorts) {
-				for (int i = nrPorts - 1; i >= newPortCount; i--) {
-					try {
-						System.out.println("remove port :"
-								+ actuatorsPorts.get(i).getName());
-						actuatorsPorts.get(i).setContainer(null);
-						actuatorsPorts.remove(i);
-					} catch (final NameDuplicationException e) {
-						throw new IllegalActionException(this, e,
-								"Error for index " + i);
-					}
-				}
+    @Override
+    protected String getInitActuatorParamValue() {
+        return "0";
+    }
 
-			}// add missing ports
-			else if (newPortCount > nrPorts) {
-				for (int i = nrPorts; i < newPortCount; i++) {
-					try {
-						// System.out.println("checking for port :"+i);
-						final String portName = ACTUATOR + i;
-						Port extraOutputPort = (Port) getPort(portName);
-						if (extraOutputPort == null) {
-							extraOutputPort = PortFactory.getInstance()
-									.createOutputPort(this, portName);
-						}
-						System.out.println("adding port :"
-								+ extraOutputPort.getName());
-						actuatorsPorts.add(extraOutputPort);
+    @Override
+    protected String getInitSensorParamValue() {
+        return "0";
+    }
 
-					} catch (final NameDuplicationException e) {
-						throw new IllegalActionException(this, e,
-								"Error for index " + i);
-					}
-				}
-			}
-		} else if (attribute == sensorsParam) {
-			sensorsNb = PasserelleUtil.getParameterIntValue(sensorsParam);
-			final int nrPorts = sensorsPorts.size();
-			final int newPortCount = sensorsNb;
-			// remove no more needed ports
-			if (newPortCount < nrPorts) {
-				for (int i = nrPorts - 1; i >= newPortCount; i--) {
-					try {
-						sensorsPorts.get(i).setContainer(null);
-						sensorsPorts.remove(i);
-					} catch (final NameDuplicationException e) {
-						throw new IllegalActionException(this, e,
-								"Error for index " + i);
-					}
-				}
+    @Override
+    protected int getNbRequieredActuator() {
+        return actuatorsNb;
+    }
 
-			}// add missing ports
-			else if (newPortCount > nrPorts) {
-				for (int i = nrPorts; i < newPortCount; i++) {
-					try {
-						final String intputName = SENSOR + i;// sensors[i];
-						Port extraOutputPort = (Port) getPort(intputName);
-						if (extraOutputPort == null) {
-							extraOutputPort = PortFactory.getInstance()
-									.createOutputPort(this, intputName);
-						}
-						// sensorsPorts.add(extraOutputPort);
-					} catch (final NameDuplicationException e) {
-						throw new IllegalActionException(this, e,
-								"Error for index " + i);
-					}
-				}
-			}
-		}
+    @Override
+    protected int getNbRequieredSensor() {
+        return sensorsNb;
+    }
 
-		super.attributeChanged(attribute);
-	}
+    @Override
+    protected void readActuatorValue() throws IllegalActionException {
+        actuatorsNb = PasserelleUtil.getParameterIntValue(actuatorsParam);
+    }
 
-	@Override
-	protected String getExtendedInfo() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    protected void readSensorValue() throws IllegalActionException {
+        sensorsNb = PasserelleUtil.getParameterIntValue(sensorsParam);
+    }
 }
