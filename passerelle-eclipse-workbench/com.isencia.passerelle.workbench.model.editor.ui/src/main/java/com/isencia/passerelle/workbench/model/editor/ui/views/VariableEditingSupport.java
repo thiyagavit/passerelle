e99a28263e0fcb4e60b3e57ea952de3e1324b19d
erwindl0 <erwindl0@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-02-27 00:44:45 +0000

    add basic support in actor attribute view to show&edit multi-line StringParameters, similar to a Text Area in the plain ptolemy & Passerelle swing UI, when a TextStyle has been set on the StringParameter

--- passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/views/VariableEditingSupport.java
+++ passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/views/VariableEditingSupport.java
@@ -1,15 +1,15 @@
 package com.isencia.passerelle.workbench.model.editor.ui.views;
 
+import java.util.List;
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.ColumnViewer;
 import org.eclipse.jface.viewers.EditingSupport;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.views.properties.PropertyDescriptor;
-import org.eclipse.ui.views.properties.TextPropertyDescriptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import ptolemy.actor.gui.ColorAttribute;
+import ptolemy.actor.gui.style.TextStyle;
 import ptolemy.data.BooleanToken;
 import ptolemy.data.expr.FileParameter;
 import ptolemy.data.expr.Parameter;
@@ -19,7 +19,6 @@ import ptolemy.data.type.Type;
 import ptolemy.kernel.util.Attribute;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.StringAttribute;
-
 import com.isencia.passerelle.actor.Actor;
 import com.isencia.passerelle.util.ptolemy.RegularExpressionParameter;
 import com.isencia.passerelle.util.ptolemy.ResourceParameter;
@@ -36,6 +35,8 @@ import com.isencia.passerelle.workbench.model.editor.ui.descriptor.RegularExpres
 import com.isencia.passerelle.workbench.model.editor.ui.descriptor.ResourcePropertyDescriptor;
 import com.isencia.passerelle.workbench.model.editor.ui.descriptor.StringChoicePropertyDescriptor;
 import com.isencia.passerelle.workbench.model.editor.ui.descriptor.StringMapPropertyDescriptor;
+import com.isencia.passerelle.workbench.model.editor.ui.descriptor.TextAreaPropertyDescriptor;
+import com.isencia.passerelle.workbench.model.editor.ui.descriptor.TextPropertyDescriptor;
 import com.isencia.passerelle.workbench.model.editor.ui.properties.CellEditorAttribute;
 import com.isencia.passerelle.workbench.model.ui.GeneralAttribute;
 
@@ -43,7 +44,6 @@ import com.isencia.passerelle.workbench.model.ui.GeneralAttribute;
  * Editing support for parameter column.
  * 
  * @author gerring
- * 
  */
 public class VariableEditingSupport extends EditingSupport {
 
@@ -68,6 +68,9 @@ public class VariableEditingSupport extends EditingSupport {
     } else if (element instanceof GeneralAttribute) {
 
       if (((GeneralAttribute) element).getType().equals(GeneralAttribute.ATTRIBUTE_TYPE.NAME)) {
+        //When combining TextAreaPD with plain TextPD, a height set for an area remains operational
+        // for all following plain TextPDs used.
+        //This slightly adapted TextPD does an explicit reset of the height.
         desc = new TextPropertyDescriptor(VariableEditingSupport.class.getName() + ".nameText", "Name");
       }
 
@@ -102,8 +105,8 @@ public class VariableEditingSupport extends EditingSupport {
   }
 
   /**
-   * NOTE: the attribute may now implement 'CellEditorParameter' in which case this method is not asked for the cell
-   * editor. This allows exotic editors to be created without changing the core passerelle code.
+   * NOTE: the attribute may now implement 'CellEditorParameter' in which case this method is not asked for the cell editor. This allows exotic editors to be
+   * created without changing the core passerelle code.
    * 
    * @param parameter
    * @param type
@@ -113,22 +116,17 @@ public class VariableEditingSupport extends EditingSupport {
 
     if (parameter instanceof ColorAttribute) {
       return new ColorPropertyDescriptor(parameter.getName(), parameter.getDisplayName());
-
     } else if (parameter instanceof ResourceParameter) {
-
       ResourcePropertyDescriptor des = new ResourcePropertyDescriptor((ResourceParameter) parameter);
       return des;
-
       // If we use this parameter, we can sent the file extensions to the
       // editor
     } else if (parameter instanceof FileParameter) {
-
       FilePickerPropertyDescriptor des = new FilePickerPropertyDescriptor(parameter.getName(), parameter.getDisplayName());
       final FileParameter fp = (FileParameter) parameter;
       if (parameter instanceof com.isencia.passerelle.util.ptolemy.FileParameter) {
         des.setFilter(((com.isencia.passerelle.util.ptolemy.FileParameter) fp).getFilterExtensions());
       }
-
       try {
         if (fp.asFile() != null)
           des.setCurrentPath(fp.asFile().getParent());
@@ -136,41 +134,36 @@ public class VariableEditingSupport extends EditingSupport {
         logger.error("Cannot get file path!", e);
       }
       return des;
-
     } else if (parameter instanceof RegularExpressionParameter) {
       return new RegularExpressionDescriptor((RegularExpressionParameter) parameter);
-
     } else if (parameter instanceof FileParameter) {
       return new FilePickerPropertyDescriptor(parameter.getName(), parameter.getDisplayName());
-
     } else if (parameter instanceof StringMapParameter) {
       final StringMapPropertyDescriptor des = new StringMapPropertyDescriptor((StringMapParameter) parameter);
       return des;
-
     } else if (parameter instanceof StringChoiceParameter) {
       final StringChoicePropertyDescriptor des = new StringChoicePropertyDescriptor((StringChoiceParameter) parameter);
       return des;
-
     } else if (hasOptions(parameter)) {
       final String[] choices = ((Parameter) parameter).getChoices();
       final CComboBoxPropertyDescriptor des = new CComboBoxPropertyDescriptor(parameter.getName(), parameter.getDisplayName(), choices);
       return des;
-
     } else if (BaseType.INT.equals(type)) {
-
       return new IntegerPropertyDescriptor(parameter.getName(), parameter.getDisplayName());
-
     } else if (BaseType.FLOAT.equals(type)) {
-
       return new FloatPropertyDescriptor(parameter.getName(), parameter.getDisplayName());
     } else if (BaseType.BOOLEAN.equals(type)) {
-
       return new CheckboxPropertyDescriptor(parameter.getName(), parameter.getDisplayName());
     } else {
-
-      return new TextPropertyDescriptor(parameter.getName(), parameter.getDisplayName());
+      PropertyDescriptor propertyDescriptor = null;
+      List txtStyleAttrs = parameter.attributeList(TextStyle.class);
+      if (txtStyleAttrs != null && !txtStyleAttrs.isEmpty()) {
+        propertyDescriptor = new TextAreaPropertyDescriptor(parameter.getName(), parameter.getDisplayName(), 50);
+      } else {
+        propertyDescriptor = new TextPropertyDescriptor(parameter.getName(), parameter.getDisplayName());
+      }
+      return propertyDescriptor;
     }
-
   }
 
   private boolean hasOptions(Attribute parameter) {
@@ -181,34 +174,31 @@ public class VariableEditingSupport extends EditingSupport {
   }
 
   private void initializeOptionsFactory(Actor actor) {
-//    try {
-//      Attribute attribute = actor.getAttribute(Actor.OPTIONS_FACTORY_CFG_NAME, OptionsFactory.class);
-//      if (attribute != null) {
-//        actor.initialize();
-//        if (actor.getOptionsFactory() != null) {
-//          List parameters = actor.attributeList(Parameter.class);
-//          for (Iterator iter = parameters.iterator(); iter.hasNext();) {
-//            Parameter p = (Parameter) iter.next();
-//            actor.getOptionsFactory().setOptionsForParameter(p);
-//          }
-//        }
-//      }
-//    } catch (IllegalActionException e) {
-//    }
+    // try {
+    // Attribute attribute = actor.getAttribute(Actor.OPTIONS_FACTORY_CFG_NAME, OptionsFactory.class);
+    // if (attribute != null) {
+    // actor.initialize();
+    // if (actor.getOptionsFactory() != null) {
+    // List parameters = actor.attributeList(Parameter.class);
+    // for (Iterator iter = parameters.iterator(); iter.hasNext();) {
+    // Parameter p = (Parameter) iter.next();
+    // actor.getOptionsFactory().setOptionsForParameter(p);
+    // }
+    // }
+    // }
+    // } catch (IllegalActionException e) {
+    // }
   }
 
   @Override
   protected boolean canEdit(Object element) {
     if (element instanceof GeneralAttribute)
-
       return ((GeneralAttribute) element).getType().equals(GeneralAttribute.ATTRIBUTE_TYPE.NAME);
-
     return true;
   }
 
   @Override
   protected Object getValue(Object element) {
-
     if (element instanceof GeneralAttribute)
       return ((GeneralAttribute) element).getValue();
     if (element instanceof StringAttribute)
@@ -225,7 +215,6 @@ public class VariableEditingSupport extends EditingSupport {
   }
 
   public String showHelpSelectedParameter(Variable param) {
-
     Attribute attr = (Attribute) param;
     if (param.getContainer() != null) {
       String helpBundle = Constants.HELP_BUNDLE_ID;
@@ -237,17 +226,14 @@ public class VariableEditingSupport extends EditingSupport {
 
   @Override
   protected void setValue(Object element, Object value) {
-
     try {
       if (element instanceof GeneralAttribute) {
         actorAttributesView.setActorName((GeneralAttribute) element, (String) value);
       } else {
         actorAttributesView.setAttributeValue(element, value);
       }
-
     } catch (Exception ne) {
       logger.error("Cannot set variable value " + value, ne);
     }
   }
-
 }
