192fbbd032c6109461f94e484fc4d538ea2206d1
kheuninck <kheuninck@gmail.com>
2013-03-08 16:06:20 +0000

    add RepositoryService

--- passerelle-core/com.isencia.passerelle.process.actor/src/main/java/com/isencia/passerelle/process/actor/activator/Activator.java
+++ passerelle-core/com.isencia.passerelle.process.actor/src/main/java/com/isencia/passerelle/process/actor/activator/Activator.java
@@ -3,10 +3,12 @@ package com.isencia.passerelle.process.actor.activator;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.util.tracker.ServiceTracker;
 
 import ptolemy.kernel.util.NamedObj;
 
 import com.isencia.passerelle.ext.ModelElementClassProvider;
+import com.isencia.passerelle.project.repository.api.RepositoryService;
 import com.isencia.passerelle.validation.version.VersionSpecification;
 
 public class Activator implements BundleActivator {
@@ -14,13 +16,19 @@ public class Activator implements BundleActivator {
 
   private ServiceRegistration apSvcReg;
   private BundleActivator testFragmentActivator;
-  
+
+  private ServiceTracker repoSvcTracker;
+
   public void start(BundleContext context) throws Exception {
     apSvcReg = context.registerService(ModelElementClassProvider.class.getName(), new ModelElementClassProvider() {
       public Class<? extends NamedObj> getClass(String className, VersionSpecification versionSpec) throws ClassNotFoundException {
         return (Class<? extends NamedObj>) this.getClass().getClassLoader().loadClass(className);
       }
     }, null);
+
+    repoSvcTracker = new ServiceTracker(context, RepositoryService.class.getName(), null);
+    repoSvcTracker.open();
+
     defaultInstance = this;
     try {
       Class<? extends BundleActivator> svcTester = (Class<? extends BundleActivator>) Class.forName("com.isencia.passerelle.process.actor.activator.TestFragmentActivator");
@@ -33,7 +41,7 @@ public class Activator implements BundleActivator {
   }
 
   public void stop(BundleContext context) throws Exception {
-    if(testFragmentActivator!=null) {
+    if (testFragmentActivator != null) {
       testFragmentActivator.stop(context);
     }
     apSvcReg.unregister();
@@ -42,5 +50,17 @@ public class Activator implements BundleActivator {
 
   public static Activator getDefault() {
     return defaultInstance;
+  }
+
+  public RepositoryService getRepositoryService() {
+    try {
+      if (repoSvcTracker == null) {
+        return null;
+      }
+
+      return (RepositoryService) repoSvcTracker.waitForService(3000);
+    } catch (InterruptedException ex) {
+      return null;
+    }
   }
 }
