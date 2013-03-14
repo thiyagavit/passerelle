cd5b0c8ef6f5357ee1ae3068886551c7eb5f1325
erwindl0 <erwindl0@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-03-14 12:38:15 +0000

    Rename ErrorCategories to simple FUNCTIONAL&TECHNICAL

--- passerelle-core/com.isencia.passerelle.engine/src/main/java/com/isencia/passerelle/core/ErrorCategory.java
+++ passerelle-core/com.isencia.passerelle.engine/src/main/java/com/isencia/passerelle/core/ErrorCategory.java
@@ -22,8 +22,8 @@ public class ErrorCategory extends Enumerated<ErrorCode> {
   private static final char PREFIX_SEPARATOR = '_';
   
   public static final ErrorCategory PASS_ROOTCATEGORY = new ErrorCategory("PASS_ROOTCATEGORY", null, "PASS");
-  public static final ErrorCategory PASS_FUNCTIONAL = new ErrorCategory("PASS_FUNCTIONAL", PASS_ROOTCATEGORY, "FUNC");
-  public static final ErrorCategory PASS_TECHNICAL = new ErrorCategory("PASS_TECHNICAL", PASS_ROOTCATEGORY, "TECH");
+  public static final ErrorCategory FUNCTIONAL = new ErrorCategory("FUNCTIONAL", PASS_ROOTCATEGORY, "FUNC");
+  public static final ErrorCategory TECHNICAL = new ErrorCategory("TECHNICAL", PASS_ROOTCATEGORY, "TECH");
   
   /**
    * the (optional) parent category
