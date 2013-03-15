e1bfab598bd37768197ae85360b8d4079798010d
kheuninck@gmail.com <kheuninck@gmail.com@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-03-15 12:41:07 +0000

    use new DefaultModelClassProvider

--- branches/edm_v1.0/com.isencia.passerelle.process.actor/src/main/java/com/isencia/passerelle/process/actor/activator/Activator.java
+++ branches/edm_v1.0/com.isencia.passerelle.process.actor/src/main/java/com/isencia/passerelle/process/actor/activator/Activator.java
@@ -4,23 +4,25 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
-import ptolemy.kernel.util.NamedObj;
-
 import com.isencia.passerelle.ext.ModelElementClassProvider;
-import com.isencia.passerelle.validation.version.VersionSpecification;
+import com.isencia.passerelle.ext.impl.DefaultModelElementClassProvider;
+import com.isencia.passerelle.process.actor.flow.BufferedTrigger;
+import com.isencia.passerelle.process.actor.flow.ContextEntryModifier;
+import com.isencia.passerelle.process.actor.flow.Fork;
+import com.isencia.passerelle.process.actor.flow.Join;
+import com.isencia.passerelle.process.actor.flow.StartActor;
+import com.isencia.passerelle.process.actor.trial.ContextTracerConsole;
 
 public class Activator implements BundleActivator {
   private static Activator defaultInstance;
 
   private ServiceRegistration apSvcReg;
   private BundleActivator testFragmentActivator;
-  
+
   public void start(BundleContext context) throws Exception {
-    apSvcReg = context.registerService(ModelElementClassProvider.class.getName(), new ModelElementClassProvider() {
-      public Class<? extends NamedObj> getClass(String className, VersionSpecification versionSpec) throws ClassNotFoundException {
-        return (Class<? extends NamedObj>) this.getClass().getClassLoader().loadClass(className);
-      }
-    }, null);
+    apSvcReg = context.registerService(ModelElementClassProvider.class.getName(), new DefaultModelElementClassProvider(BufferedTrigger.class, Fork.class, Join.class,
+        StartActor.class, ContextTracerConsole.class,ContextEntryModifier.class), null);
+
     defaultInstance = this;
     try {
       Class<? extends BundleActivator> svcTester = (Class<? extends BundleActivator>) Class.forName("com.isencia.passerelle.process.actor.activator.TestFragmentActivator");
@@ -33,7 +35,7 @@ public class Activator implements BundleActivator {
   }
 
   public void stop(BundleContext context) throws Exception {
-    if(testFragmentActivator!=null) {
+    if (testFragmentActivator != null) {
       testFragmentActivator.stop(context);
     }
     apSvcReg.unregister();
