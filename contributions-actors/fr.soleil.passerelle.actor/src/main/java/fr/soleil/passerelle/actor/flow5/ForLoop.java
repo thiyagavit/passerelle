6f14a83aeb02e57815da7dd2c0ece261091f0c9a
erwindl0 <erwindl0@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-03-18 22:06:06 +0000

    Incomplete - task : Small changes during Soleil workshop
    -- implement a ValueListSplitter actor, and base it on a common base class with ForLoop actors

--- contributions-actors/fr.soleil.passerelle.actor/src/main/java/fr/soleil/passerelle/actor/flow5/ForLoop.java
+++ contributions-actors/fr.soleil.passerelle.actor/src/main/java/fr/soleil/passerelle/actor/flow5/ForLoop.java
@@ -4,34 +4,17 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ptolemy.data.StringToken;
+import ptolemy.data.IntToken;
 import ptolemy.data.expr.Parameter;
-import ptolemy.data.expr.StringParameter;
 import ptolemy.kernel.CompositeEntity;
-import ptolemy.kernel.util.Attribute;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
 import ptolemy.kernel.util.StringAttribute;
 import com.isencia.passerelle.actor.InitializationException;
-import com.isencia.passerelle.actor.ProcessingException;
-import com.isencia.passerelle.actor.v5.Actor;
-import com.isencia.passerelle.actor.v5.ActorContext;
-import com.isencia.passerelle.actor.v5.ProcessRequest;
 import com.isencia.passerelle.actor.v5.ProcessResponse;
 import com.isencia.passerelle.core.ErrorCode;
-import com.isencia.passerelle.core.PasserelleException;
-import com.isencia.passerelle.core.Port;
-import com.isencia.passerelle.core.PortFactory;
-import com.isencia.passerelle.core.PortMode;
-import com.isencia.passerelle.message.ManagedMessage;
-import com.isencia.passerelle.message.MessageException;
-import com.isencia.passerelle.message.MessageFactory;
-import com.isencia.passerelle.message.MessageInputContext;
-import com.isencia.passerelle.util.ExecutionTracerService;
 
 /**
  * A loop actor implementation based on the Passerelle v5 Actor API.
@@ -47,7 +30,7 @@ import com.isencia.passerelle.util.ExecutionTracerService;
  * 
  * @author erwin
  */
-public class ForLoop extends Actor {
+public class ForLoop extends AbstractSequenceStepper {
 
   private static final long serialVersionUID = 1L;
   private final static Logger LOGGER = LoggerFactory.getLogger(ForLoop.class);
@@ -55,15 +38,7 @@ public class ForLoop extends Actor {
   public static final String STEP_WIDTH_PARAM_NAME = "Step Width";
   public static final String END_VALUE_PARAM_NAME = "End Value";
   public static final String START_VALUE_PARAM_NAME = "Start Value";
-  public static final String END_LOOP_PORT_NAME = "end loop trigger";
-  public static final String OUTPUT_PORT_NAME = "output value";
 
-  // input ports
-  public Port triggerPort;
-  public Port handledPort;
-  // output ports
-  public Port outputPort;
-  public Port endLoopPort;
   // Parameters
   public Parameter startValueParam;
   private double startValue;
@@ -76,8 +51,6 @@ public class ForLoop extends Actor {
   // number of steps in the loop
   private long stepNumber;
 
-  private BlockingQueue<Step> loopStepQueue = new LinkedBlockingQueue<Step>();
-
   /**
    * @param container
    * @param name
@@ -87,24 +60,14 @@ public class ForLoop extends Actor {
   public ForLoop(final CompositeEntity container, final String name) throws IllegalActionException, NameDuplicationException {
     super(container, name);
 
-    triggerPort = PortFactory.getInstance().createInputPort(this, "trigger (start loop)", null);
-    triggerPort.setMultiport(false);
-    handledPort = PortFactory.getInstance().createInputPort(this, "handled", PortMode.PUSH, null);
-    handledPort.setMultiport(false);
-    endLoopPort = PortFactory.getInstance().createOutputPort(this, END_LOOP_PORT_NAME);
-    outputPort = PortFactory.getInstance().createOutputPort(this, OUTPUT_PORT_NAME);
-
-    startValueParam = new StringParameter(this, START_VALUE_PARAM_NAME);
-    startValueParam.setExpression("0");
-    endValueParam = new StringParameter(this, END_VALUE_PARAM_NAME);
-    endValueParam.setExpression("3");
-    stepWidthParam = new StringParameter(this, STEP_WIDTH_PARAM_NAME);
-    stepWidthParam.setExpression("1");
+    startValueParam = new Parameter(this, START_VALUE_PARAM_NAME, new IntToken(0));
+    endValueParam = new Parameter(this, END_VALUE_PARAM_NAME, new IntToken(3));
+    stepWidthParam = new Parameter(this, STEP_WIDTH_PARAM_NAME, new IntToken(0));
 
     final StringAttribute outputPortCardinal = new StringAttribute(outputPort, "_cardinal");
     outputPortCardinal.setExpression("SOUTH");
 
-    final StringAttribute handledPortCardinal = new StringAttribute(handledPort, "_cardinal");
+    final StringAttribute handledPortCardinal = new StringAttribute(nextPort, "_cardinal");
     handledPortCardinal.setExpression("SOUTH");
 
     _attachText("_iconDescription", "<svg>\n" + "<rect x=\"-25\" y=\"-25\" width=\"50\" " + "height=\"50\" style=\"fill:pink;stroke:pink\"/>\n"
@@ -126,8 +89,9 @@ public class ForLoop extends Actor {
   protected void doInitialize() throws InitializationException {
     super.doInitialize();
     try {
-      startValue = Double.valueOf(((StringToken) startValueParam.getToken()).stringValue());
-      endValue = Double.valueOf(((StringToken) endValueParam.getToken()).stringValue());
+      stepWidth = Math.abs(((IntToken) stepWidthParam.getToken()).doubleValue());
+      startValue = ((IntToken) startValueParam.getToken()).doubleValue();
+      endValue = ((IntToken) endValueParam.getToken()).doubleValue();
     } catch (Exception e) {
       throw new InitializationException(ErrorCode.ACTOR_INITIALISATION_ERROR, "Error reading loop start/end values", this, e);
     }
@@ -138,61 +102,6 @@ public class ForLoop extends Actor {
     final BigDecimal div = totalWidth.divide(new BigDecimal(stepWidth), MathContext.DECIMAL32);
     stepNumber = (long) Math.floor(div.doubleValue()) + 1;
     getLogger().debug("stepNumber {}", stepNumber);
-    loopStepQueue.clear();
-  }
-
-  @Override
-  public void attributeChanged(final Attribute arg0) throws IllegalActionException {
-    // check the stepWidth param on each change, so we can complain immediately if it has an invalid value
-    if (arg0 == stepWidthParam) {
-      stepWidth = Double.valueOf(((StringToken) stepWidthParam.getToken()).stringValue());
-      if (stepWidth <= 0) {
-        throw new IllegalActionException(stepWidthParam, "Step Width must be positive");
-      }
-    } else {
-      super.attributeChanged(arg0);
-    }
-  }
-
-  /**
-   * Intercept each "handled" message while it's being pushed via the handledPort, so we can use this as indication that a next loop msg can be sent out.
-   */
-  @Override
-  public void offer(MessageInputContext ctxt) throws PasserelleException {
-    if (handledPort.getName().equals(ctxt.getPortName())) {
-      sendLoopData();
-    }
-    super.offer(ctxt);
-  }
-
-  /**
-   * React on each received trigger message, by generating a complete loop step sequence again, and store the steps in a queue.
-   */
-  @Override
-  protected void process(ActorContext ctxt, ProcessRequest request, ProcessResponse response) throws ProcessingException {
-    ManagedMessage inputMsg = request.getMessage(triggerPort);
-    if (inputMsg != null) {
-      getLogger().debug("{} - received trigger msg {}", getFullName(), inputMsg);
-      boolean wasIdle = loopStepQueue.isEmpty();
-      generateSteps(response);
-      if (wasIdle) {
-        // send out first msg of the loop
-        sendLoopData();
-      }
-    }
-  }
-
-  /**
-   * As the loop actor manages a complete loop execution "in the background" for each received trigger msg, i.e. we do not block the process method during the
-   * complete loop execution, this must be marked as "asynchronous" processing.
-   */
-  @Override
-  protected ProcessingMode getProcessingMode(ActorContext ctxt, ProcessRequest request) {
-    if (request.getMessage(triggerPort) != null) {
-      return ProcessingMode.ASYNCHRONOUS;
-    } else {
-      return super.getProcessingMode(ctxt, request);
-    }
   }
 
   /**
@@ -200,7 +109,7 @@ public class ForLoop extends Actor {
    * 
    * @param response
    */
-  private void generateSteps(ProcessResponse response) {
+  protected void generateSteps(ProcessResponse response) {
     Double currentValue = startValue;
     for (long i = 0; i < stepNumber; ++i) {
       // use formatter to remove the "problem" of precision
@@ -211,88 +120,13 @@ public class ForLoop extends Actor {
       df.setGroupingUsed(false);
       df.setDecimalFormatSymbols(dfs);
       currentValue = Double.parseDouble(df.format(currentValue));
-      loopStepQueue.offer(new Step(i, currentValue, response));
+      addStep(new Step(i, currentValue, response));
       if (up) {
         currentValue += stepWidth;
       } else {
         currentValue -= stepWidth;
       }
     }
-    loopStepQueue.offer(Step.buildEndMarker(response));
-  }
-
-  /**
-   * @param inputMsg
-   * @throws ProcessingException
-   */
-  private void sendLoopData() throws ProcessingException {
-    Step step = loopStepQueue.poll();
-    if (step != null) {
-      if (step.isEndMarker()) {
-        // output end loop signal
-        ProcessResponse response = step.response;
-        processFinished(response.getContext(), response.getRequest(), response);
-        ExecutionTracerService.trace(this, "All steps done");
-        sendOutputMsg(endLoopPort, MessageFactory.getInstance().createTriggerMessage());
-        if (loopStepQueue.isEmpty()) {
-          if (!isFinishRequested() && triggerPort.getActiveSources().isEmpty()) {
-            requestFinish();
-          }
-        } else {
-          // continue with next loop that seems to be on the queue
-          sendLoopData();
-        }
-      } else if (isFinishRequested()) {
-        ExecutionTracerService.trace(this, "Loop has been interrupted at step number " + step.stepCtr);
-        if (getDirectorAdapter().isActorBusy(this)) {
-          while (!loopStepQueue.isEmpty()) {
-            // clear all busy work
-            ProcessResponse response = step.response;
-            if (response != null)
-              processFinished(response.getContext(), response.getRequest(), response);
-          }
-        }
-      } else {
-        final ManagedMessage resultMsg = createMessage();
-        getLogger().trace("{} - sendLoopData() - iteration {}", getFullName(), step.stepCtr);
-        try {
-          resultMsg.setBodyContent(step.stepValue, ManagedMessage.objectContentType);
-          sendOutputMsg(outputPort, resultMsg);
-          ExecutionTracerService.trace(this, "Loop with value: " + step.stepValue);
-        } catch (final MessageException e) {
-          throw new ProcessingException(ErrorCode.MSG_CONSTRUCTION_ERROR, "Cannot send message out", this, e);
-        }
-      }
-    }
-  }
-
-  /**
-   * Simple container structure to maintain all relevant info for each loop step that must be executed.
-   */
-  static class Step {
-    private boolean endMarker;
-    long stepCtr;
-    double stepValue;
-    ProcessResponse response;
-
-    public Step(long stepCtr, double stepValue, ProcessResponse response) {
-      this.stepCtr = stepCtr;
-      this.stepValue = stepValue;
-      this.response = response;
-    }
-
-    private Step() {
-    }
-
-    public boolean isEndMarker() {
-      return endMarker;
-    }
-
-    public static Step buildEndMarker(ProcessResponse response) {
-      Step s = new Step();
-      s.endMarker = true;
-      s.response = response;
-      return s;
-    }
+    addEndMarkerStep(response);
   }
 }
--- contributions-actors/fr.soleil.passerelle.actor/src/main/java/fr/soleil/passerelle/actor/flow5/ForLoopWithPortCfg.java
+++ contributions-actors/fr.soleil.passerelle.actor/src/main/java/fr/soleil/passerelle/actor/flow5/ForLoopWithPortCfg.java
@@ -4,11 +4,11 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ptolemy.data.StringToken;
+import ptolemy.data.IntToken;
+import ptolemy.data.ScalarToken;
+import ptolemy.data.Token;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.Attribute;
 import ptolemy.kernel.util.IllegalActionException;
@@ -16,20 +16,12 @@ import ptolemy.kernel.util.NameDuplicationException;
 import ptolemy.kernel.util.StringAttribute;
 import com.isencia.passerelle.actor.InitializationException;
 import com.isencia.passerelle.actor.ProcessingException;
-import com.isencia.passerelle.actor.v5.Actor;
 import com.isencia.passerelle.actor.v5.ActorContext;
 import com.isencia.passerelle.actor.v5.ProcessRequest;
 import com.isencia.passerelle.actor.v5.ProcessResponse;
 import com.isencia.passerelle.core.ErrorCode;
-import com.isencia.passerelle.core.PasserelleException;
-import com.isencia.passerelle.core.Port;
+import com.isencia.passerelle.core.PasserelleToken;
 import com.isencia.passerelle.core.PortFactory;
-import com.isencia.passerelle.core.PortMode;
-import com.isencia.passerelle.message.ManagedMessage;
-import com.isencia.passerelle.message.MessageException;
-import com.isencia.passerelle.message.MessageFactory;
-import com.isencia.passerelle.message.MessageInputContext;
-import com.isencia.passerelle.util.ExecutionTracerService;
 import com.isencia.passerelle.util.ptolemy.PortParameter;
 
 /**
@@ -49,7 +41,7 @@ import com.isencia.passerelle.util.ptolemy.PortParameter;
  * 
  * @author erwin
  */
-public class ForLoopWithPortCfg extends Actor {
+public class ForLoopWithPortCfg extends AbstractSequenceStepper {
 
   private static final long serialVersionUID = 1L;
   private final static Logger LOGGER = LoggerFactory.getLogger(ForLoopWithPortCfg.class);
@@ -57,15 +49,7 @@ public class ForLoopWithPortCfg extends Actor {
   public static final String STEP_WIDTH_PARAM_NAME = "Step Width";
   public static final String END_VALUE_PARAM_NAME = "End Value";
   public static final String START_VALUE_PARAM_NAME = "Start Value";
-  public static final String END_LOOP_PORT_NAME = "end loop trigger";
-  public static final String OUTPUT_PORT_NAME = "output value";
 
-  // input ports
-  public Port triggerPort;
-  public Port handledPort;
-  // output ports
-  public Port outputPort;
-  public Port endLoopPort;
   // Parameters
   public PortParameter startValueParam;
   private double startValue;
@@ -80,8 +64,6 @@ public class ForLoopWithPortCfg extends Actor {
   
   // marker for first iteration
   private volatile boolean firstIteration = true;
-  
-  private BlockingQueue<Step> loopStepQueue = new LinkedBlockingQueue<Step>();
 
   /**
    * @param container
@@ -92,25 +74,17 @@ public class ForLoopWithPortCfg extends Actor {
   public ForLoopWithPortCfg(final CompositeEntity container, final String name) throws IllegalActionException, NameDuplicationException {
     super(container, name);
 
-    handledPort = PortFactory.getInstance().createInputPort(this, "handled", PortMode.PUSH, null);
-    handledPort.setMultiport(false);
-    endLoopPort = PortFactory.getInstance().createOutputPort(this, END_LOOP_PORT_NAME);
-    outputPort = PortFactory.getInstance().createOutputPort(this, OUTPUT_PORT_NAME);
+    startValueParam = PortFactory.getInstance().createPortParameter(this, START_VALUE_PARAM_NAME, Integer.class);
+    startValueParam.setToken(new IntToken(0));
+    endValueParam = PortFactory.getInstance().createPortParameter(this, END_VALUE_PARAM_NAME, Integer.class);
+    endValueParam.setToken(new IntToken(3));
+    stepWidthParam = PortFactory.getInstance().createPortParameter(this, STEP_WIDTH_PARAM_NAME, Integer.class);
+    stepWidthParam.setToken(new IntToken(1));
 
-    startValueParam = PortFactory.getInstance().createPortParameter(this, START_VALUE_PARAM_NAME, null);
-    startValueParam.setToken(new StringToken("0"));
-    endValueParam = PortFactory.getInstance().createPortParameter(this, END_VALUE_PARAM_NAME, null);
-    endValueParam.setToken(new StringToken("3"));
-    stepWidthParam = PortFactory.getInstance().createPortParameter(this, STEP_WIDTH_PARAM_NAME, null);
-    stepWidthParam.setToken(new StringToken("1"));
-
-    triggerPort = PortFactory.getInstance().createInputPort(this, "trigger (start loop)", null);
-    triggerPort.setMultiport(false);
-    
     final StringAttribute outputPortCardinal = new StringAttribute(outputPort, "_cardinal");
     outputPortCardinal.setExpression("SOUTH");
 
-    final StringAttribute handledPortCardinal = new StringAttribute(handledPort, "_cardinal");
+    final StringAttribute handledPortCardinal = new StringAttribute(nextPort, "_cardinal");
     handledPortCardinal.setExpression("SOUTH");
 
     _attachText("_iconDescription", "<svg>\n" + "<rect x=\"-25\" y=\"-25\" width=\"50\" " + "height=\"50\" style=\"fill:pink;stroke:pink\"/>\n"
@@ -132,7 +106,7 @@ public class ForLoopWithPortCfg extends Actor {
   public void attributeChanged(final Attribute arg0) throws IllegalActionException {
     // check the stepWidth param on each change, so we can complain immediately if it has an invalid value
     if (arg0 == stepWidthParam) {
-      stepWidth = Double.valueOf(((StringToken) stepWidthParam.getToken()).stringValue());
+      stepWidth =  getValueFromPortParameter(stepWidthParam, 1L);
       if (stepWidth <= 0) {
         throw new IllegalActionException(stepWidthParam, "Step Width must be positive");
       }
@@ -144,7 +118,6 @@ public class ForLoopWithPortCfg extends Actor {
   @Override
   protected void doInitialize() throws InitializationException {
     super.doInitialize();
-    
     firstIteration = true;
   }
 
@@ -162,9 +135,9 @@ public class ForLoopWithPortCfg extends Actor {
       endValueParam.setOnce();
       stepWidthParam.setOnce();
       
-      startValue = Double.valueOf(((StringToken) startValueParam.getToken()).stringValue());
-      endValue = Double.valueOf(((StringToken) endValueParam.getToken()).stringValue());
-      stepWidth = Double.valueOf(((StringToken) stepWidthParam.getToken()).stringValue());
+      startValue = getValueFromPortParameter(startValueParam, 0L);
+      endValue = getValueFromPortParameter(endValueParam, 3L);
+      stepWidth = getValueFromPortParameter(stepWidthParam, 1L);
       if (stepWidth <= 0) {
         throw new IllegalActionException(stepWidthParam, "Step Width must be positive");
       }
@@ -178,18 +151,22 @@ public class ForLoopWithPortCfg extends Actor {
     final BigDecimal div = totalWidth.divide(new BigDecimal(stepWidth), MathContext.DECIMAL32);
     stepNumber = (long) Math.floor(div.doubleValue()) + 1;
     getLogger().debug("stepNumber {}", stepNumber);
-    loopStepQueue.clear();
+    clearStepQueue();
   }
 
-  /**
-   * Intercept each "handled" message while it's being pushed via the handledPort, so we can use this as indication that a next loop msg can be sent out.
-   */
-  @Override
-  public void offer(MessageInputContext ctxt) throws PasserelleException {
-    if (handledPort.getName().equals(ctxt.getPortName())) {
-      sendLoopData();
+  protected double getValueFromPortParameter(PortParameter portParam, double defValue) throws IllegalActionException {
+    Token t = portParam.getToken();
+    if(t instanceof PasserelleToken) {
+      try {
+        Object res = ((PasserelleToken)t).getMessage().getBodyContent();
+        return Double.parseDouble(res.toString());
+      } catch (Exception e) {
+        throw new IllegalActionException(this, e, "error reading msg content");
+      }
+    } else if (t instanceof ScalarToken) {
+      return ((ScalarToken) portParam.getToken()).doubleValue();
     }
-    super.offer(ctxt);
+    return defValue;
   }
 
   /**
@@ -201,29 +178,7 @@ public class ForLoopWithPortCfg extends Actor {
       setLoopConfiguration();
       firstIteration = false;
     }
-    ManagedMessage inputMsg = request.getMessage(triggerPort);
-    if (inputMsg != null) {
-      getLogger().debug("{} - received trigger msg {}", getFullName(), inputMsg);
-      boolean wasIdle = loopStepQueue.isEmpty();
-      generateSteps(response);
-      if (wasIdle) {
-        // send out first msg of the loop
-        sendLoopData();
-      }
-    }
-  }
-
-  /**
-   * As the loop actor manages a complete loop execution "in the background" for each received trigger msg, i.e. we do not block the process method during the
-   * complete loop execution, this must be marked as "asynchronous" processing.
-   */
-  @Override
-  protected ProcessingMode getProcessingMode(ActorContext ctxt, ProcessRequest request) {
-    if (request.getMessage(triggerPort) != null) {
-      return ProcessingMode.ASYNCHRONOUS;
-    } else {
-      return super.getProcessingMode(ctxt, request);
-    }
+    super.process(ctxt, request, response);
   }
 
   /**
@@ -231,7 +186,7 @@ public class ForLoopWithPortCfg extends Actor {
    * 
    * @param response
    */
-  private void generateSteps(ProcessResponse response) {
+  protected void generateSteps(ProcessResponse response) {
     Double currentValue = startValue;
     for (long i = 0; i < stepNumber; ++i) {
       // use formatter to remove the "problem" of precision
@@ -242,88 +197,13 @@ public class ForLoopWithPortCfg extends Actor {
       df.setGroupingUsed(false);
       df.setDecimalFormatSymbols(dfs);
       currentValue = Double.parseDouble(df.format(currentValue));
-      loopStepQueue.offer(new Step(i, currentValue, response));
+      addStep(new Step(i, currentValue, response));
       if (up) {
         currentValue += stepWidth;
       } else {
         currentValue -= stepWidth;
       }
     }
-    loopStepQueue.offer(Step.buildEndMarker(response));
-  }
-
-  /**
-   * @param inputMsg
-   * @throws ProcessingException
-   */
-  private void sendLoopData() throws ProcessingException {
-    Step step = loopStepQueue.poll();
-    if (step != null) {
-      if (step.isEndMarker()) {
-        // output end loop signal
-        ProcessResponse response = step.response;
-        processFinished(response.getContext(), response.getRequest(), response);
-        ExecutionTracerService.trace(this, "All steps done");
-        sendOutputMsg(endLoopPort, MessageFactory.getInstance().createTriggerMessage());
-        if (loopStepQueue.isEmpty()) {
-          if (!isFinishRequested() && triggerPort.getActiveSources().isEmpty()) {
-            requestFinish();
-          }
-        } else {
-          // continue with next loop that seems to be on the queue
-          sendLoopData();
-        }
-      } else if (isFinishRequested()) {
-        ExecutionTracerService.trace(this, "Loop has been interrupted at step number " + step.stepCtr);
-        if (getDirectorAdapter().isActorBusy(this)) {
-          while (!loopStepQueue.isEmpty()) {
-            // clear all busy work
-            ProcessResponse response = step.response;
-            if (response != null)
-              processFinished(response.getContext(), response.getRequest(), response);
-          }
-        }
-      } else {
-        final ManagedMessage resultMsg = createMessage();
-        getLogger().trace("{} - sendLoopData() - iteration {}", getFullName(), step.stepCtr);
-        try {
-          resultMsg.setBodyContent(step.stepValue, ManagedMessage.objectContentType);
-          sendOutputMsg(outputPort, resultMsg);
-          ExecutionTracerService.trace(this, "Loop with value: " + step.stepValue);
-        } catch (final MessageException e) {
-          throw new ProcessingException(ErrorCode.MSG_CONSTRUCTION_ERROR, "Cannot send message out", this, e);
-        }
-      }
-    }
-  }
-
-  /**
-   * Simple container structure to maintain all relevant info for each loop step that must be executed.
-   */
-  static class Step {
-    private boolean endMarker;
-    long stepCtr;
-    double stepValue;
-    ProcessResponse response;
-
-    public Step(long stepCtr, double stepValue, ProcessResponse response) {
-      this.stepCtr = stepCtr;
-      this.stepValue = stepValue;
-      this.response = response;
-    }
-
-    private Step() {
-    }
-
-    public boolean isEndMarker() {
-      return endMarker;
-    }
-
-    public static Step buildEndMarker(ProcessResponse response) {
-      Step s = new Step();
-      s.endMarker = true;
-      s.response = response;
-      return s;
-    }
+    addEndMarkerStep(response);
   }
 }
