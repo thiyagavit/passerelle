d14c1916118bf0f11d52c086ada365408906bc78
kheuninck <kheuninck@gmail.com>
2013-03-22 15:24:34 +0000

    fix classloading issue

--- passerelle-core/com.isencia.passerelle.process.actor/src/main/java/com/isencia/passerelle/process/actor/activator/Activator.java
+++ passerelle-core/com.isencia.passerelle.process.actor/src/main/java/com/isencia/passerelle/process/actor/activator/Activator.java
@@ -5,11 +5,15 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.util.tracker.ServiceTracker;
 
-import ptolemy.kernel.util.NamedObj;
-
 import com.isencia.passerelle.ext.ModelElementClassProvider;
+import com.isencia.passerelle.ext.impl.DefaultModelElementClassProvider;
+import com.isencia.passerelle.process.actor.flow.BufferedTrigger;
+import com.isencia.passerelle.process.actor.flow.ContextEntryModifier;
+import com.isencia.passerelle.process.actor.flow.Fork;
+import com.isencia.passerelle.process.actor.flow.Join;
+import com.isencia.passerelle.process.actor.flow.StartActor;
+import com.isencia.passerelle.process.actor.trial.ContextTracerConsole;
 import com.isencia.passerelle.project.repository.api.RepositoryService;
-import com.isencia.passerelle.validation.version.VersionSpecification;
 
 public class Activator implements BundleActivator {
   private static Activator defaultInstance;
@@ -20,11 +24,8 @@ public class Activator implements BundleActivator {
   private ServiceTracker repoSvcTracker;
 
   public void start(BundleContext context) throws Exception {
-    apSvcReg = context.registerService(ModelElementClassProvider.class.getName(), new ModelElementClassProvider() {
-      public Class<? extends NamedObj> getClass(String className, VersionSpecification versionSpec) throws ClassNotFoundException {
-        return (Class<? extends NamedObj>) this.getClass().getClassLoader().loadClass(className);
-      }
-    }, null);
+    apSvcReg = context.registerService(ModelElementClassProvider.class.getName(), new DefaultModelElementClassProvider(BufferedTrigger.class, Fork.class, Join.class,
+        StartActor.class, ContextTracerConsole.class, ContextEntryModifier.class), null);
 
     repoSvcTracker = new ServiceTracker(context, RepositoryService.class.getName(), null);
     repoSvcTracker.open();
