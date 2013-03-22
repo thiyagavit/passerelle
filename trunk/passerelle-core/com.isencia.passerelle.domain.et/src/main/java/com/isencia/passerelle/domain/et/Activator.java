d9461bb3c93ffd89b1e2c477401029de78096063
kheuninck@gmail.com <kheuninck@gmail.com@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-03-22 15:23:20 +0000

    fix classloading issue

--- trunk/passerelle-core/com.isencia.passerelle.domain.et/src/main/java/com/isencia/passerelle/domain/et/Activator.java
+++ trunk/passerelle-core/com.isencia.passerelle.domain.et/src/main/java/com/isencia/passerelle/domain/et/Activator.java
@@ -18,21 +18,16 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
-import ptolemy.kernel.util.NamedObj;
-
 import com.isencia.passerelle.ext.ModelElementClassProvider;
-import com.isencia.passerelle.validation.version.VersionSpecification;
+import com.isencia.passerelle.ext.impl.DefaultModelElementClassProvider;
 
 public class Activator implements BundleActivator {
 
   private ServiceRegistration apSvcReg;
   
   public void start(BundleContext context) throws Exception {
-    apSvcReg = context.registerService(ModelElementClassProvider.class.getName(), new ModelElementClassProvider() {
-      public Class<? extends NamedObj> getClass(String className, VersionSpecification versionSpec) throws ClassNotFoundException {
-        return (Class<? extends NamedObj>) this.getClass().getClassLoader().loadClass(className);
-      }
-    }, null);
+    apSvcReg = context.registerService(ModelElementClassProvider.class.getName(), new DefaultModelElementClassProvider(ETDirector.class), null);
+
   }
 
   public void stop(BundleContext context) throws Exception {
