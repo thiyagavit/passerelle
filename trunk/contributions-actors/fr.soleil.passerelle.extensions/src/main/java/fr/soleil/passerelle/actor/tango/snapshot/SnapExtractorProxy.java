4dd80b0b7b9e1246b08db270071369239d0e0ef0
vgramer <vgramer@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-03-22 17:02:29 +0000

    indent code

--- trunk/contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/actor/tango/snapshot/SnapExtractorProxy.java
+++ trunk/contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/actor/tango/snapshot/SnapExtractorProxy.java
@@ -9,10 +9,12 @@ public class SnapExtractorProxy {
 
     private final TangoCommand getSnapValues;
     private final TangoCommand getSnapID;
+    private final TangoCommand getSnap;
 
-    SnapExtractorProxy(final String snapExtractorName) throws DevFailed {
-	getSnapValues = new TangoCommand(snapExtractorName, "GetSnapValues");
-	getSnapID = new TangoCommand(snapExtractorName, "GetSnapID");
+    public SnapExtractorProxy(final String snapExtractorName) throws DevFailed {
+        getSnapValues = new TangoCommand(snapExtractorName, "GetSnapValues");
+        getSnapID = new TangoCommand(snapExtractorName, "GetSnapID");
+        getSnap = new TangoCommand(snapExtractorName, "GetSnap");
     }
 
     /**
@@ -20,12 +22,15 @@ public class SnapExtractorProxy {
      * 
      * @param snapID
      * @param attributeNames
+     * 
      * @return
+     * 
      * @throws DevFailed
      */
-    public String[] getReadValues(final String snapID, final String... attributeNames) throws DevFailed {
-	final Object[] array = ArrayUtils.addAll(new String[] { snapID, "true" }, attributeNames);
-	return getSnapValues.execute(String[].class, array);
+    public String[] getReadValues(final String snapID, final String... attributeNames)
+            throws DevFailed {
+        final Object[] array = ArrayUtils.addAll(new String[] { snapID, "true" }, attributeNames);
+        return getSnapValues.execute(String[].class, array);
     }
 
     /**
@@ -33,38 +38,44 @@ public class SnapExtractorProxy {
      * 
      * @param snapID
      * @param attributeNames
+     * 
      * @return
+     * 
      * @throws DevFailed
      */
-    public String[] getWriteValues(final String snapID, final String... attributeNames) throws DevFailed {
-	final Object[] array = ArrayUtils.addAll(new String[] { snapID, "false" }, attributeNames);
-	return getSnapValues.execute(String[].class, array);
+    public String[] getWriteValues(final String snapID, final String... attributeNames)
+            throws DevFailed {
+        final Object[] array = ArrayUtils.addAll(new String[] { snapID, "false" }, attributeNames);
+        return getSnapValues.execute(String[].class, array);
     }
 
     /**
      * Get last snap for a context
      * 
      * @param contextID
+     * 
      * @return
+     * 
      * @throws DevFailed
      */
     public String getLastSnapID(final String contextID) throws DevFailed {
-	return getSnapID.execute(String[].class, contextID, "last")[0];
+        return getSnapID.execute(String[].class, contextID, "last")[0];
     }
 
     /**
      * Get snap ID with a filter
      * 
      * @param contextID
-     * @param searchFilter
-     *            ctx_id, "id_snap > | < | = | <= | >= nbr",
-     *            "time < | > | >= | <=  yyyy-mm-dd hh:mm:ss | dd-mm-yyyy hh:mm:ss"
-     *            , "comment starts | ends | contains string", first | last
+     * @param searchFilter ctx_id, "id_snap > | < | = | <= | >= nbr",
+     *            "time < | > | >= | <=  yyyy-mm-dd hh:mm:ss | dd-mm-yyyy hh:mm:ss" ,
+     *            "comment starts | ends | contains string", first | last
+     * 
      * @return
+     * 
      * @throws DevFailed
      */
     public String[] getSnapIDs(final String contextID, final String searchFilter) throws DevFailed {
-	return getSnapID.execute(String[].class, contextID, searchFilter);
+        return getSnapID.execute(String[].class, contextID, searchFilter);
     }
 
 }
