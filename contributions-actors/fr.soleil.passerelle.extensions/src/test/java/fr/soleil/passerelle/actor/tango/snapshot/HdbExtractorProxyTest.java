0a4095b30611b95f7a8cb5b52c21a58e239cc850
vgramer <vgramer@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-03-22 17:05:08 +0000

    add files

---
+++ contributions-actors/fr.soleil.passerelle.extensions/src/test/java/fr/soleil/passerelle/actor/tango/snapshot/HdbExtractorProxyTest.java
@@ -0,0 +1,84 @@
+package fr.soleil.passerelle.actor.tango.snapshot;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.fail;
+import static org.fest.assertions.api.Assertions.failBecauseExceptionWasNotThrown;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.testng.PowerMockTestCase;
+import org.tango.utils.DevFailedUtils;
+import org.testng.IObjectFactory;
+import org.testng.annotations.ObjectFactory;
+import org.testng.annotations.Test;
+
+import fr.esrf.Tango.DevFailed;
+import fr.soleil.tango.clientapi.TangoCommand;
+import fr.soleil.tango.clientapi.command.MockCommand;
+
+@PrepareForTest(TangoCommand.class)
+public class HdbExtractorProxyTest extends PowerMockTestCase {
+
+    @Test
+    public void _when_extractor_return_a_wrong_format_to_getLastScalarAttrValue_then_throw_devFailed() {
+        HdbExtractorProxy proxy = new HdbExtractorProxy();
+        try {
+            proxy.setGetNewestValueCommad(new TangoCommand(new MockCommand("GetNewestValue",
+                    "tango/tangotes/titan/double_scalar", "invalid_String_Format")));
+        }
+        catch (DevFailed devFailed) {
+            fail("can not init tango Command", devFailed);
+        }
+
+        try {
+            proxy.getLastScalarAttrValue("tango/tangotest/titan", "double_scalar");
+            failBecauseExceptionWasNotThrown(DevFailed.class);
+        }
+        catch (DevFailed e) {
+            assertThat(DevFailedUtils.toString(e)).contains(HdbExtractorProxy.WRONG_FORMAT);
+        }
+    }
+
+    // @Test
+    // public void
+    // _when_extractor_return_a_correct_format_to_getLastScalarAttrValue_then_last_value() throws
+    // DevFailed {
+    // String expectedValue = " -10.686";
+    // HdbExtractorProxy proxy = new HdbExtractorProxy();
+    //
+    // ITangoCommand command = mock(ITangoCommand.class);
+    // when(command.executeExtract(eq("tango/tangotest/titan/double_scalar"))).thenReturn("123;" +
+    // expectedValue);
+    //
+    // proxy.setGetNewestValueCommad(new TangoCommand(command));
+    //
+    // String value = proxy.getLastScalarAttrValue("tango/tangotest/titan", "double_scalar");
+    //
+    // assertThat(value).isEqualTo(expectedValue.trim());
+    //
+    // }
+
+    @ObjectFactory
+    public IObjectFactory getObjectFactory() {
+        return new org.powermock.modules.testng.PowerMockObjectFactory();
+    }
+
+    @Test
+    public void _when_extractor_return_a_correct_format_to_getLastScalarAttrValue_then_last_value()
+            throws DevFailed {
+        String expectedValue = " -10.686";
+        HdbExtractorProxy proxy = new HdbExtractorProxy();
+
+        TangoCommand command = mock(TangoCommand.class);
+        when(command.execute(eq(String.class), eq("tango/tangotest/titan/double_scalar")))
+                .thenReturn("123;" + expectedValue);
+
+        proxy.setGetNewestValueCommad(command);
+        String value = proxy.getLastScalarAttrValue("tango/tangotest/titan", "double_scalar");
+
+        assertThat(value).isEqualTo(expectedValue.trim());
+
+    }
+}
