e823ce4c59b75bf7a8c6ebe73865b3ec136789d8
erwindl0 <erwindl0@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-04-11 12:44:27 +0000

    Add outputNoSplit for a Splitter.
    When it did not find something to split on, the originally received msg is just forwarded via a separate output port.
    (and should not pass via the consecutive Join then)

--- passerelle-core/com.isencia.passerelle.process.actor.test/src/main/java/com/isencia/passerelle/process/actor/forkjoin/Splitter.java
+++ passerelle-core/com.isencia.passerelle.process.actor.test/src/main/java/com/isencia/passerelle/process/actor/forkjoin/Splitter.java
@@ -28,10 +28,9 @@ import com.isencia.passerelle.process.model.Context;
  * multiple individual values, with a delimiter as configured in <code>splitDelimiterParameter</code>. <br/>
  * For each individual value, a separate message will be sent out in sequence, with this particalur value for the named item.
  * </p>
- * 
- * TODO split value is now set as a <code>Context</code> entry. I.e. it is not effectively replacing the original item which could be in any <code>ResultBlock</code>,
- * <code>Request</code> attribute etc. Check if this is OK in all required use cases.
- * (i.e. in rules modules, facts are typically constructed based on result blocks etc, and the context entries are not necessarily taken into account I think!?
+ * TODO split value is now set as a <code>Context</code> entry. I.e. it is not effectively replacing the original item which could be in any
+ * <code>ResultBlock</code>, <code>Request</code> attribute etc. Check if this is OK in all required use cases. (i.e. in rules modules, facts are typically
+ * constructed based on result blocks etc, and the context entries are not necessarily taken into account I think!?
  * 
  * @author erwin
  */
@@ -41,6 +40,10 @@ public class Splitter extends AbstractMessageSequenceGenerator {
 
   public Port input;
   public Port output;
+  /**
+   *  extra output where the received msg will be sent out, when nothing was found to split on
+   */
+  public Port outputNoSplit;
 
   /**
    * Parameter to specify the name of the name/value item in the <code>Context</code>, that must be used as split criterium.
@@ -59,6 +62,7 @@ public class Splitter extends AbstractMessageSequenceGenerator {
     super(container, name, true);
     input = PortFactory.getInstance().createInputPort(this, null);
     output = PortFactory.getInstance().createOutputPort(this);
+    outputNoSplit = PortFactory.getInstance().createOutputPort(this, "outputNoSplit");
 
     splitSrcItemParameter = new StringParameter(this, "Split src item");
     splitOutItemParameter = new StringParameter(this, "Split output item");
@@ -81,33 +85,46 @@ public class Splitter extends AbstractMessageSequenceGenerator {
 
         String splitSrcItemName = ((StringToken) splitSrcItemParameter.getToken()).stringValue();
         String splitOutItemName = ((StringToken) splitOutItemParameter.getToken()).stringValue();
-        if(StringUtils.isEmpty(splitOutItemName)) {
+        if (StringUtils.isEmpty(splitOutItemName)) {
           splitOutItemName = splitSrcItemName;
         }
-        String splitDelimiter = ((StringToken) splitDelimiterParameter.getToken()).stringValue();
-
         String splitValue = processContext.lookupValue(splitSrcItemName);
-
-        try {
-          if (getAuditLogger().isInfoEnabled()) {
-            getAuditLogger().info("Splitting msg {} on {}:{}", new String[] {getAuditTrailMessage(message, input),splitSrcItemName,splitDelimiter} );
+        boolean doingSplit = false;
+        if (StringUtils.isNotEmpty(splitValue)) {
+          String splitDelimiter = ((StringToken) splitDelimiterParameter.getToken()).stringValue();
+          String[] valueParts = splitValue.split(splitDelimiter);
+          if (valueParts.length > 0) {
+            try {
+              if (getAuditLogger().isInfoEnabled()) {
+                getAuditLogger().info("Splitting msg {} on {}:{}", new String[] { getAuditTrailMessage(message, input), splitSrcItemName, splitDelimiter });
+              }
+            } catch (Exception e) {
+              getLogger().error("Error logging audit trail", e);
+            }
+            for (int i = 0; i < valueParts.length; ++i) {
+              Context newOne = processContext.fork();
+              newOne.putEntry(splitOutItemName, valueParts[i]);
+              MessageContainer outputMsg = (MessageContainer) MessageFactory.getInstance().createMessageCloneInSequence(message,
+                  processContext.getRequest().getId(), // sequence ID
+                  new Long(i), // sequence position
+                  (i == (valueParts.length - 1))); // end of sequence?
+              // enforce single Splitter name (so use setHeader i.o. addHeader)
+              outputMsg.setHeader(HEADER_SEQ_SRC, getName());
+              outputMsg.setBodyContent(newOne, ManagedMessage.objectContentType);
+              procResponse.addOutputMessage(output, outputMsg);
+            }
+            doingSplit = true;
           }
-        } catch (Exception e) {
-          getLogger().error("Error logging audit trail", e);
         }
-
-        String[] valueParts = splitValue.split(splitDelimiter);
-        for (int i = 0; i < valueParts.length; ++i) {
-          Context newOne = processContext.fork();
-          newOne.putEntry(splitOutItemName, valueParts[i]);
-          MessageContainer outputMsg = (MessageContainer) MessageFactory.getInstance().createMessageCloneInSequence(message,
-              processContext.getRequest().getId(), // sequence ID
-              new Long(i), // sequence position
-              (i == (valueParts.length - 1))); // end of sequence?
-          // enforce single Splitter name (so use setHeader i.o. addHeader)
-          outputMsg.setHeader(HEADER_SEQ_SRC, getName());
-          outputMsg.setBodyContent(newOne, ManagedMessage.objectContentType);
-          procResponse.addOutputMessage(output, outputMsg);
+        if (!doingSplit) {
+          try {
+            if (getAuditLogger().isInfoEnabled()) {
+              getAuditLogger().info("Forwarding {} via outputNoSplit ; nothing found to split on {}", getAuditTrailMessage(message, input), splitSrcItemName);
+            }
+          } catch (Exception e) {
+            getLogger().error("Error logging audit trail", e);
+          }
+          procResponse.addOutputMessage(outputNoSplit, message);
         }
       } catch (Exception e) {
         throw new ProcessingException(ErrorCode.ACTOR_EXECUTION_ERROR, "Error generating forked messages", this, message, e);
