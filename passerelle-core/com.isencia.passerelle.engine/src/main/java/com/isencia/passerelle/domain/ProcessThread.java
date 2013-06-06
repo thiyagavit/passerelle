f1e8a8c054a15dd679631c5d9b932a0f75abf0c7
erwindl0 <erwindl0@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-06-06 17:41:49 +0000

    ProcessThread change (cfr Ptolemy chg between ProcessThread v7 and now) : - change order of calling director.removeThread() and actor.wrapup() -> this resolves a situation where daemon actors like ErrorHandlers were shut down too rapidly.

--- passerelle-core/com.isencia.passerelle.engine/src/main/java/com/isencia/passerelle/domain/ProcessThread.java
+++ passerelle-core/com.isencia.passerelle.engine/src/main/java/com/isencia/passerelle/domain/ProcessThread.java
@@ -142,13 +142,10 @@ public class ProcessThread extends ptolemy.actor.process.ProcessThread {
       logger.debug(getActor().getFullName() + " - Error in ProcessThread " + t);
       //t.printStackTrace();
     } finally {
-      // Let the director know that this thread stopped.
       // This is synchronized to prevent a race condition
       // where the director might conclude before the
       // call to wrapup() below.
       synchronized (_director) {
-        _director.removeThread(this);
-
         try {
           // NOTE: Deadlock risk here if wrapup is done inside
           // a block synchronized on the _director, as it used to be.
@@ -164,7 +161,14 @@ public class ProcessThread extends ptolemy.actor.process.ProcessThread {
         } catch (IllegalActionException e) {
           thrownWhenWrapup = e;
         } finally {
-          _debug("-- Thread stopped.");
+          // Let the director know that this thread stopped.
+          // This must occur after the call to wrapup above.
+          synchronized (_director) {
+              _director.removeThread(this);
+          }
+          if (_debugging) {
+            _debug("-- Thread stopped.");
+        }
 
           boolean rethrow = false;
 
