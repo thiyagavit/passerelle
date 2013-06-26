7e9482f8e96d3f1f2d2b600bb6c3a35ade33ce22
erwindl0 <erwindl0@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-06-26 08:12:41 +0000

    cdma tests : add test to dump values for selected slice/item

--- contributions-actors/fr.soleil.passerelle.cdma.actor.test/src/test/java/fr/soleil/passerelle/cdma/actor/test/CDMAActorItemRetrievalTest.java
+++ contributions-actors/fr.soleil.passerelle.cdma.actor.test/src/test/java/fr/soleil/passerelle/cdma/actor/test/CDMAActorItemRetrievalTest.java
@@ -26,8 +26,10 @@ import com.isencia.passerelle.model.Flow;
 import com.isencia.passerelle.model.FlowManager;
 import com.isencia.passerelle.testsupport.FlowStatisticsAssertion;
 import com.isencia.passerelle.testsupport.actor.DevNullActor;
+import fr.soleil.passerelle.cdma.actor.CDMAArrayValueDumper;
 import fr.soleil.passerelle.cdma.actor.CDMADataItemSelector;
 import fr.soleil.passerelle.cdma.actor.CDMADataSetReader;
+import fr.soleil.passerelle.cdma.actor.CDMADataSetSlicer;
 
 /**
  * Basic tests for actors that access CDMA files and items.
@@ -40,6 +42,7 @@ public class CDMAActorItemRetrievalTest extends TestCase {
   private static final String ERROR_SINK = "errorSink";
   private static final String SINK = "sink";
   private static final String NXS_READER = "NxsReader";
+  private static final String SLICER = "slicer";
 
   private static final String ERROR_SINK_INPUT = ERROR_SINK+".input";
   private static final String SINK_INPUT = SINK+".input";
@@ -83,6 +86,34 @@ public class CDMAActorItemRetrievalTest extends TestCase {
     return flow;
   }
   
+  protected Flow _buildAndRunBasicItemRetrievalAndDumpFlow(String testName, String itemName, boolean useLogicalLookup) throws Exception {
+    Flow flow = new Flow(testName,null);
+    FlowManager flowMgr = new FlowManager();
+    flow.setDirector(new ETDirector(flow,"director"));
+    
+    CDMADataSetReader source = new CDMADataSetReader(flow,NXS_READER);
+    CDMADataItemSelector itemSelector = new CDMADataItemSelector(flow, "ItemSelector");
+    CDMADataSetSlicer slicer = new CDMADataSetSlicer(flow, SLICER);
+    CDMAArrayValueDumper arrayValueDumper = new CDMAArrayValueDumper(flow, "ArrayDumper");
+    DevNullActor sink = new DevNullActor(flow, SINK);
+    DevNullActor errorSink = new DevNullActor(flow, ERROR_SINK);
+    
+    flow.connect(source, itemSelector);
+    flow.connect(itemSelector, slicer);
+    flow.connect(slicer, arrayValueDumper);
+    flow.connect(arrayValueDumper.output, slicer.nextPort);
+    flow.connect(slicer.endPort, sink.input);
+    flow.connect(itemSelector.errorPort, errorSink.input);
+    
+    Map<String, String> props = new HashMap<String, String>();
+    props.put("NxsReader.DataSet URI", "C:/data/workspaces/CDMA-plain/fr.soleil.passerelle.cdma.actor.test/CDMA_samples/FlyscanSwing_2011-04-11_15-44-11.nxs");
+    props.put("ItemSelector.Item name",itemName);
+    props.put("ItemSelector.Logical selection mode",Boolean.toString(useLogicalLookup));
+    flowMgr.executeBlockingLocally(flow,props);
+    
+    return flow;
+  }
+  
   protected Flow _buildAndRunBasicItemRetrievalFlowOlof(String testName, String itemName, boolean useLogicalLookup) throws Exception {
     Flow flow = new Flow(testName,null);
     FlowManager flowMgr = new FlowManager();
@@ -106,17 +137,17 @@ public class CDMAActorItemRetrievalTest extends TestCase {
     return flow;
   }
   
-  public void testPhysicalPathToItemOlof() throws Exception {
-    new FlowStatisticsAssertion()
-    .expectMsgSentCount(NXS_READER_OUTPUT, 1L)
-    .expectMsgReceiptCount(SINK_INPUT, 1L)
-    .expectMsgReceiptCount(ERROR_SINK_INPUT, 0L)
-    .assertFlow(
-    _buildAndRunBasicItemRetrievalFlowOlof("testPhysicalPathToItemOlof","Result/Raw data/Raw data",false)
-    );
-  }
-
-  
+//  public void testPhysicalPathToItemOlof() throws Exception {
+//    new FlowStatisticsAssertion()
+//    .expectMsgSentCount(NXS_READER_OUTPUT, 1L)
+//    .expectMsgReceiptCount(SINK_INPUT, 1L)
+//    .expectMsgReceiptCount(ERROR_SINK_INPUT, 0L)
+//    .assertFlow(
+//    _buildAndRunBasicItemRetrievalFlowOlof("testPhysicalPathToItemOlof","Result/Raw data/Raw data",false)
+//    );
+//  }
+//
+//  
 //  public void testPhysicalPathToItem() throws Exception {
 //    new FlowStatisticsAssertion()
 //    .expectMsgSentCount(NXS_READER_OUTPUT, 1L)
@@ -136,7 +167,17 @@ public class CDMAActorItemRetrievalTest extends TestCase {
 //    _buildAndRunBasicItemRetrievalFlow("testLogicalPathToItem","scan:data:images",true)
 //    );
 //  }
-//
+
+  public void testLogicalPathToItemAndDump() throws Exception {
+    new FlowStatisticsAssertion()
+    .expectMsgSentCount(NXS_READER_OUTPUT, 1L)
+    .expectMsgReceiptCount(SINK_INPUT, 1L)
+    .expectMsgReceiptCount(ERROR_SINK_INPUT,0L)
+    .assertFlow(
+    _buildAndRunBasicItemRetrievalAndDumpFlow("testLogicalPathToItem","scan:data:images",true)
+    );
+  }
+
 //  public void testPhysicalPathToItemNotFound() throws Exception {
 //    new FlowStatisticsAssertion()
 //    .expectMsgSentCount(NXS_READER_OUTPUT, 1L)
