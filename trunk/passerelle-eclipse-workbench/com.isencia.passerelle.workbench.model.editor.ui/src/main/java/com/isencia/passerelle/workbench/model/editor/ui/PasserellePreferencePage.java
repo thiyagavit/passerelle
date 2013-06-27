7edf7b53fd6603d4a54173d912e81707a502d489
erwindl0 <erwindl0@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-06-27 15:14:56 +0000

    RCP Workbench Workflow Preferences page : Add check on change of Submodel Root folder

--- trunk/passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/PasserellePreferencePage.java
+++ trunk/passerelle-eclipse-workbench/com.isencia.passerelle.workbench.model.editor.ui/src/main/java/com/isencia/passerelle/workbench/model/editor/ui/PasserellePreferencePage.java
@@ -1,25 +1,27 @@
 package com.isencia.passerelle.workbench.model.editor.ui;
 
 import java.io.File;
-
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.DirectoryFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
-import org.eclipse.jface.preference.FileFieldEditor;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
-
 import com.isencia.passerelle.project.repository.api.RepositoryService;
+import com.isencia.passerelle.workbench.util.DialogUtils;
 
 public class PasserellePreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
   public static final String SUBMODEL_DRILLDOWN = "com.isencia.passerelle.submodel.drilldown";
+  private String submodelPath;
+  private DirectoryFieldEditor subModelRoot;
+
   public PasserellePreferencePage() {
     super();
     setPreferenceStore(Activator.getDefault().getPreferenceStore());
     setDescription("Preferences for the workflow editor and for running the workflow.");
-
   }
 
   @Override
@@ -27,23 +29,50 @@ public class PasserellePreferencePage extends FieldEditorPreferencePage implemen
     final BooleanFieldEditor expert = new BooleanFieldEditor(PreferenceConstants.EXPERT, "Expert mode", getFieldEditorParent());
     addField(expert);
 
-    final DirectoryFieldEditor subModelRoot = new DirectoryFieldEditor(RepositoryService.SUBMODEL_ROOT, "Submodel Root", getFieldEditorParent());
+    subModelRoot = new DirectoryFieldEditor(RepositoryService.SUBMODEL_ROOT, "Submodel Root", getFieldEditorParent());
     addField(subModelRoot);
     final BooleanFieldEditor submodelDrillDown = new BooleanFieldEditor(SUBMODEL_DRILLDOWN, "Open submodel in separate editor", getFieldEditorParent());
     addField(submodelDrillDown);
-
   }
 
   public void init(IWorkbench workbench) {
     IPreferenceStore store = Activator.getDefault().getPreferenceStore();
-    String submodelPath = store.getString(RepositoryService.SUBMODEL_ROOT);
+    submodelPath = store.getString(RepositoryService.SUBMODEL_ROOT);
     if (submodelPath == null || submodelPath.trim().equals("")) {
       File userHome = new File(System.getProperty("user.home"));
       File defaultSubmodelPath = new File(userHome, ".passerelle/submodel-repository");
       submodelPath = System.getProperty(RepositoryService.SUBMODEL_ROOT, defaultSubmodelPath.getAbsolutePath());
       store.setValue(RepositoryService.SUBMODEL_ROOT, submodelPath);
-
     }
   }
 
+  @Override
+  public boolean performOk() {
+    boolean result = super.performOk();
+    if (result) {
+      String newSubmodelRoot = Activator.getDefault().getPreferenceStore().getString(RepositoryService.SUBMODEL_ROOT);
+      if (submodelPath != null && !submodelPath.equalsIgnoreCase(newSubmodelRoot)) {
+        MessageBox dialog = new MessageBox(getFieldEditorParent().getShell(), SWT.ICON_WARNING | SWT.OK);
+        dialog.setText("Submodel configuration change");
+        dialog.setMessage("You must restart the workbench after changing the submodel root!");
+        DialogUtils.centerDialog(getShell(), dialog.getParent());
+        dialog.open();
+      }
+    }
+    return result;
+  }
+  
+  @Override
+  public void setValid(boolean b) {
+    super.setValid(b);
+    if(!b) {
+      if (StringUtils.isNotEmpty(subModelRoot.getErrorMessage())) {
+        MessageBox dialog = new MessageBox(getFieldEditorParent().getShell(), SWT.ICON_ERROR | SWT.OK);
+        dialog.setText("Submodel configuration error");
+        dialog.setMessage("Submodel Root " + subModelRoot.getErrorMessage());
+        DialogUtils.centerDialog(getShell(), dialog.getParent());
+        dialog.open();
+      }       
+    }
+  }
 }
