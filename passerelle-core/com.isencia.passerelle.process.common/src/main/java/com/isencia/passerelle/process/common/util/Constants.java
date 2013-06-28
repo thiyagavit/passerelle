40e08bc8f8cb6672ba81d352f5307b85c42cf030
erwindl0 <erwindl0@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-06-28 08:50:42 +0000

    A branch combining Passerelle core with the workbench bundles, for usage by DAWN workflow development.
    The Passerelle core is a prod-ready intermediate stage on the way to the final 8.4.0, as also packaged and deployed in EDM v1.1.

--- passerelle-core/com.isencia.passerelle.process.common/src/main/java/com/isencia/passerelle/process/common/util/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.isencia.passerelle.process.common.util;
-
-public class Constants {
-  public static final String REQUEST_TYPE_ATTRIBUTE = "com.isencia.passerelle.process.request.type";
-}
--- ptolemy/ptolemy.gui/src/main/java/ptolemy/vergil/actor/README.txt
+++
@@ -1,2 +0,0 @@
-$Id: README.txt,v 1.1 2005/04/12 01:22:05 cxh Exp $
-See package.html
