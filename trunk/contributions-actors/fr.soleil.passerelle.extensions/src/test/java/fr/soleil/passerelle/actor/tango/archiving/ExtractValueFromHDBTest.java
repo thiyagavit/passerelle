6931647c40ecbb92121288849c966c1db86248ad
vgramer <vgramer@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-07-04 12:22:16 +0000

    add tests

--- trunk/contributions-actors/fr.soleil.passerelle.extensions/src/test/java/fr/soleil/passerelle/actor/tango/archiving/ExtractValueFromHDBTest.java
+++ trunk/contributions-actors/fr.soleil.passerelle.extensions/src/test/java/fr/soleil/passerelle/actor/tango/archiving/ExtractValueFromHDBTest.java
@@ -1,11 +1,16 @@
 package fr.soleil.passerelle.actor.tango.archiving;
 
 import static fr.soleil.passerelle.actor.tango.ATangoActorV5.OUTPUT_PORT_NAME;
+import static fr.soleil.passerelle.actor.tango.archiving.ExtractValueFromHDB.COMPLETE_ATTR_NAME;
+import static fr.soleil.passerelle.actor.tango.archiving.ExtractValueFromHDB.DATE_PARAM_NAME;
+import static fr.soleil.passerelle.actor.tango.archiving.ExtractValueFromHDB.EXTRACTION_TYPE;
+import static fr.soleil.passerelle.actor.tango.archiving.ExtractValueFromHDB.THROW_EXCEPTION_ON_ERROR;
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -28,12 +33,13 @@ import fr.soleil.passerelle.testUtils.MomlRule;
 public class ExtractValueFromHDBTest {
 
     private static final String ACTOR_NAME = "ExtractValueFromHDB";
-    private static final String EXTRACTION_TYPE_PARAM = ACTOR_NAME + "."
-            + ExtractValueFromHDB.EXTRACTION_TYPE;
-    private static final String COMPLETE_ATTR_NAME_PARAM = ACTOR_NAME + "."
-            + ExtractValueFromHDB.COMPLETE_ATTR_NAME;
+    private static final String EXTRACTION_TYPE_PARAM = ACTOR_NAME + "." + EXTRACTION_TYPE;
+    private static final String COMPLETE_ATTR_NAME_PARAM = ACTOR_NAME + "." + COMPLETE_ATTR_NAME;
     private static final String THROW_EXCEPTION_ON_ERROR_PARAM = ACTOR_NAME + "."
-            + ExtractValueFromHDB.THROW_EXCEPTION_ON_ERROR;
+            + THROW_EXCEPTION_ON_ERROR;
+
+    private static final String DATE_PARAM = ACTOR_NAME + "." + DATE_PARAM_NAME;
+
     private final MomlRule moml = new MomlRule(Constants.SEQUENCES_PATH
             + "ExtractValueFromHDB.moml");
     private ExtractValueFromHDB actor;
@@ -42,7 +48,6 @@ public class ExtractValueFromHDBTest {
     public void setUp() throws Throwable {
         moml.before();
         actor = (ExtractValueFromHDB) moml.getEntity(ACTOR_NAME);
-
     }
 
     @AfterMethod
@@ -108,12 +113,10 @@ public class ExtractValueFromHDBTest {
     public void testExtractLastValue() throws DevFailed, FlowAlreadyExecutingException,
             PasserelleException {
         HdbExtractorProxy mockedProxy = mock(HdbExtractorProxy.class);
-        String deviceName = "domain/family/member";
-        String attrName = "attr";
+        String completeAttrName = "domain/family/member/attr";
         String expectedValue = "123.5";
 
-        when(mockedProxy.getLastScalarAttrValue(eq(deviceName), eq(attrName))).thenReturn(
-                expectedValue);
+        when(mockedProxy.getLastScalarAttrValue(eq(completeAttrName))).thenReturn(expectedValue);
 
         actor.setExtractorProxy(mockedProxy);
 
@@ -121,7 +124,7 @@ public class ExtractValueFromHDBTest {
         moml.addMessageReceiver(ACTOR_NAME, OUTPUT_PORT_NAME, outportMsg);
 
         Map<String, String> props = new HashMap<String, String>();
-        props.put(COMPLETE_ATTR_NAME_PARAM, deviceName + "/" + attrName);
+        props.put(COMPLETE_ATTR_NAME_PARAM, completeAttrName);
         props.put(EXTRACTION_TYPE_PARAM, ExtractValueFromHDB.ExtractionType.LASTED.getDescription());
         props.put(THROW_EXCEPTION_ON_ERROR_PARAM, "true");
 
@@ -131,11 +134,11 @@ public class ExtractValueFromHDBTest {
     }
 
     @Test(expectedExceptions = ProcessingException.class, expectedExceptionsMessageRegExp = "(?s).*domain/family/member/attr is not in Hdb or can not be read: .*")
-    public void when_attr_is_not_in_db_and_throwExceptionOnError_param_is_true_then_throw_Exception()
+    public void when_attr_is_not_in_db_and_throwExceptionOnError_param_is_true_then_lastValue_throws_Exception()
             throws FlowAlreadyExecutingException, PasserelleException, DevFailed {
         HdbExtractorProxy mockedProxy = mock(HdbExtractorProxy.class);
 
-        when(mockedProxy.getLastScalarAttrValue(eq("domain/family/member"), eq("attr")))
+        when(mockedProxy.getLastScalarAttrValue(eq("domain/family/member/attr")))
                 .thenThrow(
                         DevFailedUtils
                                 .newDevFailed("domain/family/member/attr is not in Hdb or can not be read: "));
@@ -154,11 +157,11 @@ public class ExtractValueFromHDBTest {
     }
 
     @Test
-    public void when_attr_is_not_in_db_and_throwExceptionOnError_param_is_false_then_return_empty_msg()
+    public void when_attr_is_not_in_db_and_throwExceptionOnError_param_is_false_then_lastValue_return_empty_msg()
             throws DevFailed, FlowAlreadyExecutingException, PasserelleException {
         HdbExtractorProxy mockedProxy = mock(HdbExtractorProxy.class);
 
-        when(mockedProxy.getLastScalarAttrValue(eq("domain/family/member"), eq("attr")))
+        when(mockedProxy.getLastScalarAttrValue(eq("domain/family/member/attr")))
                 .thenThrow(
                         DevFailedUtils
                                 .newDevFailed("domain/family/member/attr is not in Hdb or can not be read: "));
@@ -175,6 +178,92 @@ public class ExtractValueFromHDBTest {
 
         moml.executeBlockingErrorLocally(props);
 
+        assertThat(outportMsg).hasSize(1);
         assertThat(outportMsg.poll()).isEmpty();
+    }
+
+    @Test(expectedExceptions = ProcessingException.class, expectedExceptionsMessageRegExp = "(?s).*domain/family/member/attr is not in Hdb or can not be read: .*")
+    public void when_attr_is_not_in_db_and_throwExceptionOnError_param_is_true_then_nearestValue_throws_Exception()
+            throws FlowAlreadyExecutingException, PasserelleException, DevFailed {
+        HdbExtractorProxy mockedProxy = mock(HdbExtractorProxy.class);
+
+        Date date = new Date();
+        String dateParam = ExtractValueFromHDB.DATE_PARAMETER_FORMAT.format(date);
+        String dateProxy = HdbExtractorProxy.DATE_FORMAT.format(date);
+
+        when(mockedProxy.getNearestScalarAttrValue(eq("domain/family/member/attr"), eq(dateProxy)))
+                .thenThrow(
+                        DevFailedUtils
+                                .newDevFailed("domain/family/member/attr is not in Hdb or can not be read: "));
+
+        actor.setExtractorProxy(mockedProxy);
+
+        ArrayBlockingQueue<String> outportMsg = new ArrayBlockingQueue<String>(1);
+        moml.addMessageReceiver(ACTOR_NAME, OUTPUT_PORT_NAME, outportMsg);
+
+        Map<String, String> props = new HashMap<String, String>();
+        props.put(COMPLETE_ATTR_NAME_PARAM, "domain/family/member/attr");
+        props.put(EXTRACTION_TYPE_PARAM,
+                ExtractValueFromHDB.ExtractionType.NEAREST.getDescription());
+        props.put(THROW_EXCEPTION_ON_ERROR_PARAM, "true");
+        props.put(DATE_PARAM, dateParam);
+
+        moml.executeBlockingErrorLocally(props);
+    }
+
+    @Test
+    public void when_attr_is_not_in_db_and_throwExceptionOnError_param_is_false_then_nearestValue_return_empty_msg()
+            throws DevFailed, FlowAlreadyExecutingException, PasserelleException {
+        HdbExtractorProxy mockedProxy = mock(HdbExtractorProxy.class);
+
+        Date date = new Date();
+        String dateParam = ExtractValueFromHDB.DATE_PARAMETER_FORMAT.format(date);
+        String dateProxy = HdbExtractorProxy.DATE_FORMAT.format(date);
+
+        when(mockedProxy.getNearestScalarAttrValue(eq("domain/family/member/attr"), eq(dateProxy)))
+                .thenThrow(
+                        DevFailedUtils
+                                .newDevFailed("domain/family/member/attr is not in Hdb or can not be read: "));
+
+        actor.setExtractorProxy(mockedProxy);
+
+        ArrayBlockingQueue<String> outportMsg = new ArrayBlockingQueue<String>(1);
+        moml.addMessageReceiver(ACTOR_NAME, OUTPUT_PORT_NAME, outportMsg);
+
+        Map<String, String> props = new HashMap<String, String>();
+        props.put(COMPLETE_ATTR_NAME_PARAM, "domain/family/member/attr");
+        props.put(EXTRACTION_TYPE_PARAM,
+                ExtractValueFromHDB.ExtractionType.NEAREST.getDescription());
+        props.put(THROW_EXCEPTION_ON_ERROR_PARAM, "false");
+        props.put(DATE_PARAM, dateParam);
+
+        moml.executeBlockingErrorLocally(props);
+
+        assertThat(outportMsg).hasSize(1);
+        assertThat(outportMsg.poll()).isEmpty();
+    }
+
+    @Test
+    public void testExtractNearestValue() throws DevFailed, FlowAlreadyExecutingException,
+            PasserelleException {
+        HdbExtractorProxy mockedProxy = mock(HdbExtractorProxy.class);
+        String completeAttrName = "domain/family/member/attr";
+        String expectedValue = "123.5";
+
+        when(mockedProxy.getLastScalarAttrValue(eq(completeAttrName))).thenReturn(expectedValue);
+
+        actor.setExtractorProxy(mockedProxy);
+
+        ArrayBlockingQueue<String> outportMsg = new ArrayBlockingQueue<String>(1);
+        moml.addMessageReceiver(ACTOR_NAME, OUTPUT_PORT_NAME, outportMsg);
+
+        Map<String, String> props = new HashMap<String, String>();
+        props.put(COMPLETE_ATTR_NAME_PARAM, completeAttrName);
+        props.put(EXTRACTION_TYPE_PARAM, ExtractValueFromHDB.ExtractionType.LASTED.getDescription());
+        props.put(THROW_EXCEPTION_ON_ERROR_PARAM, "true");
+
+        moml.executeBlockingErrorLocally(props);
+
+        assertThat(outportMsg.poll()).isEqualTo(expectedValue);
     }
 }
