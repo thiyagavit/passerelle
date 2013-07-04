03e21d4c0eaa90437191d344b00b50a1ba116e7d
vgramer <vgramer@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-07-04 12:22:03 +0000

    add tests

--- trunk/contributions-actors/fr.soleil.passerelle.extensions/src/test/java/fr/soleil/passerelle/actor/tango/archiving/HdbExtractorProxyTest.java
+++ trunk/contributions-actors/fr.soleil.passerelle.extensions/src/test/java/fr/soleil/passerelle/actor/tango/archiving/HdbExtractorProxyTest.java
@@ -1,7 +1,7 @@
 package fr.soleil.passerelle.actor.tango.archiving;
 
+import static fr.soleil.passerelle.actor.tango.archiving.HdbExtractorProxy.WRONG_FORMAT;
 import static org.fest.assertions.api.Assertions.assertThat;
-import static org.fest.assertions.api.Assertions.fail;
 import static org.fest.assertions.api.Assertions.failBecauseExceptionWasNotThrown;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
@@ -16,31 +16,9 @@ import org.testng.annotations.Test;
 
 import fr.esrf.Tango.DevFailed;
 import fr.soleil.tango.clientapi.TangoCommand;
-import fr.soleil.tango.clientapi.command.MockCommand;
 
 @PrepareForTest(TangoCommand.class)
 public class HdbExtractorProxyTest extends PowerMockTestCase {
-
-    @Test
-    public void should_throw_devFailed_when_extractor_return_a_wrong_format_to_getLastScalarAttrValue()
-            throws DevFailed {
-        HdbExtractorProxy proxy = new HdbExtractorProxy(false);
-        try {
-            proxy.setGetNewestValueCommad(new TangoCommand(new MockCommand("GetNewestValue",
-                    "tango/tangotes/titan/double_scalar", "invalid_String_Format")));
-        }
-        catch (DevFailed devFailed) {
-            fail("can not init tango Command", devFailed);
-        }
-
-        try {
-            proxy.getLastScalarAttrValue("tango/tangotest/titan", "double_scalar");
-            failBecauseExceptionWasNotThrown(DevFailed.class);
-        }
-        catch (DevFailed e) {
-            assertThat(DevFailedUtils.toString(e)).contains(HdbExtractorProxy.WRONG_FORMAT);
-        }
-    }
 
     @ObjectFactory
     public IObjectFactory getObjectFactory() {
@@ -48,18 +26,82 @@ public class HdbExtractorProxyTest extends PowerMockTestCase {
     }
 
     @Test
+    public void should_throw_devFailed_when_extractor_return_a_wrong_format_to_getLastScalarAttrValue()
+            throws DevFailed {
+        HdbExtractorProxy proxy = new HdbExtractorProxy(false);
+        String completeAttrName = "tango/tangotes/titan/double_scalar";
+
+        TangoCommand command = mock(TangoCommand.class);
+        when(command.execute(eq(String.class), eq(completeAttrName))).thenReturn("invaid format");
+
+        proxy.setNewestValueCommad(command);
+
+        try {
+            proxy.getLastScalarAttrValue(completeAttrName);
+            failBecauseExceptionWasNotThrown(DevFailed.class);
+        }
+        catch (DevFailed e) {
+            assertThat(DevFailedUtils.toString(e)).contains(WRONG_FORMAT);
+        }
+    }
+
+    @Test
     public void test_extractorpProxy_return_a_correct_format_to_getLastScalarAttrValue()
             throws DevFailed {
+        String completeAttributeName = "tango/tangotest/titan/double_scalar";
         String expectedValue = " -10.686";
+
         HdbExtractorProxy proxy = new HdbExtractorProxy(false);
 
         TangoCommand command = mock(TangoCommand.class);
-        when(command.execute(eq(String.class), eq("tango/tangotest/titan/double_scalar")))
-                .thenReturn("123;" + expectedValue);
 
-        proxy.setGetNewestValueCommad(command);
-        String value = proxy.getLastScalarAttrValue("tango/tangotest/titan", "double_scalar");
+        when(command.execute(eq(String.class), eq(completeAttributeName))).thenReturn(
+                "123;" + expectedValue);
+
+        proxy.setNewestValueCommad(command);
+        String value = proxy.getLastScalarAttrValue(completeAttributeName);
 
         assertThat(value).isEqualTo(expectedValue.trim());
+    }
+
+    @Test
+    public void test_extractorpProxy_return_a_correct_format_to_getNearestScalarAttrValue()
+            throws DevFailed {
+        String completeAttributeName = "test/motor/1-1/position";
+        String date = "03-07-2013 16:39:00";
+        String expectedValue = " -10.686";
+
+        HdbExtractorProxy proxy = new HdbExtractorProxy(false);
+
+        TangoCommand command = mock(TangoCommand.class);
+        when(command.execute(eq(String.class), eq(completeAttributeName), eq(date))).thenReturn(
+                "123;" + expectedValue);
+
+        proxy.setNearestValueCommand(command);
+
+        String value = proxy.getNearestScalarAttrValue(completeAttributeName, date);
+        assertThat(value).isEqualTo(expectedValue.trim());
+    }
+
+    @Test
+    public void should_throw_devFailed_when_extractor_return_a_wrong_format_to_getNearestScalarAttrValue()
+            throws DevFailed {
+        HdbExtractorProxy proxy = new HdbExtractorProxy(false);
+        String completeAttrName = "tango/tangotes/titan/double_scalar";
+        String date = "03-07-2013 16:39:00";
+
+        TangoCommand mockedCommand = mock(TangoCommand.class);
+        when(mockedCommand.execute(eq(String.class), eq(completeAttrName), eq(date))).thenReturn(
+                "invalid format");
+
+        proxy.setNearestValueCommand(mockedCommand);
+
+        try {
+            proxy.getNearestScalarAttrValue(completeAttrName, date);
+            failBecauseExceptionWasNotThrown(DevFailed.class);
+        }
+        catch (DevFailed e) {
+            assertThat(DevFailedUtils.toString(e)).contains(WRONG_FORMAT);
+        }
     }
 }
