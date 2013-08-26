676b20e35602583202cdbcd8b3d99133935e93ba
sandra.pierrejoseph <sandra.pierrejoseph@synchrotron-soleil.fr>
2013-08-26 16:21:43 +0000

    soleil-extensions:cf bug 25591

--- contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/actor/tango/acquisition/Scan.java
+++ contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/actor/tango/acquisition/Scan.java
@@ -1,24 +1,24 @@
-/*	Synchrotron Soleil
- *
- *   File          :  ScanFromFile.java
- *
- *   Project       :  passerelle-soleil
- *
- *   Description   :
- *
- *   Author        :  ABEILLE
- *
- *   Original      :  17 mai 2005
- *
- *   Revision:  					Author:
- *   Date: 							State:
- *
- *   Log: ScanFromFile.java,v
- *
+/*
+ * Synchrotron Soleil
+ * 
+ * File : ScanFromFile.java
+ * 
+ * Project : passerelle-soleil
+ * 
+ * Description :
+ * 
+ * Author : ABEILLE
+ * 
+ * Original : 17 mai 2005
+ * 
+ * Revision: Author:
+ * Date: State:
+ * 
+ * Log: ScanFromFile.java,v
  */
 /*
  * Created on 17 mai 2005
- *
+ * 
  * To change the template for this generated file go to
  * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
@@ -48,6 +48,9 @@ import com.isencia.passerelle.core.PasserelleException.Severity;
 import com.isencia.passerelle.doc.generator.ParameterName;
 import com.isencia.passerelle.util.ExecutionTracerService;
 
+import fr.esrf.Tango.DevFailed;
+import fr.esrf.TangoApi.DeviceProxyFactory;
+import fr.soleil.comete.tango.data.service.helper.TangoDeviceHelper;
 import fr.soleil.passerelle.actor.IActorFinalizer;
 import fr.soleil.passerelle.actor.TransformerV5;
 import fr.soleil.passerelle.actor.tango.acquisition.scan.ScanUtil;
@@ -63,232 +66,220 @@ import fr.soleil.salsa.exception.ScanNotFoundException;
 
 /**
  * Do scans using Salsa config V3
- *
+ * 
  * @author GRAMER
  */
 @SuppressWarnings("serial")
-public class Scan extends TransformerV5 implements IActorFinalizer{
+public class Scan extends TransformerV5 implements IActorFinalizer {
 
-	private static final String SCAN_CONFIG = "Scan Config";
-	private final static Logger logger = LoggerFactory.getLogger(Scan.class);
+    private static final String SCAN_CONFIG = "Scan Config";
+    private final static Logger logger = LoggerFactory.getLogger(Scan.class);
 
-	/**
-	 * The name of the Salsa config V3. must be the entire path to the config
-	 * exemple : root/config1 or root/folder1/config2
-	 */
-	@ParameterName(name = SCAN_CONFIG)
-	public Parameter scanConfigParam;
-	private String confName = "root/";
+    /**
+     * The name of the Salsa config V3. must be the entire path to the config
+     * exemple : root/config1 or root/folder1/config2
+     */
+    @ParameterName(name = SCAN_CONFIG)
+    public Parameter scanConfigParam;
+    private String confName = "root/";
 
-	protected IConfig<?> conf;
-	protected ScanTask scanTask;
+    protected IConfig<?> conf;
+    protected ScanTask scanTask;
 
-	/**
-	 * Create a Scan V3 actor
-	 *
-	 * @param container
-	 * @param name
-	 * @throws IllegalActionException
-	 * @throws NameDuplicationException
-	 */
-	public Scan(final CompositeEntity container, final String name)
-			throws IllegalActionException, NameDuplicationException {
-		super(container, name);
-		scanConfigParam = new StringParameter(this, SCAN_CONFIG);
-		scanConfigParam.setExpression(confName);
+    /**
+     * Create a Scan V3 actor
+     * 
+     * @param container
+     * @param name
+     * @throws IllegalActionException
+     * @throws NameDuplicationException
+     */
+    public Scan(final CompositeEntity container, final String name) throws IllegalActionException,
+            NameDuplicationException {
+        super(container, name);
+        scanConfigParam = new StringParameter(this, SCAN_CONFIG);
+        scanConfigParam.setExpression(confName);
 
-		input.setName("Trigger");
-		output.setName("TriggerOut");
+        input.setName("Trigger");
+        output.setName("TriggerOut");
 
-		final URL url = this.getClass().getResource(
-				"/fr/soleil/salsa/salsa.png");
+        final URL url = this.getClass().getResource("/fr/soleil/salsa/salsa.png");
 
-		_attachText(
-				"_iconDescription",
-				"<svg>\n"
-						+ " <image x=\"0\" y=\"0\" width =\"75\" height=\"51\" xlink:href=\""
-						+ url + "\"/>\n" + "</svg>\n");
+        _attachText("_iconDescription", "<svg>\n" + " <image x=\"0\" y=\"0\" width =\"75\" height=\"51\" xlink:href=\""
+                + url + "\"/>\n" + "</svg>\n");
 
-	}
+    }
 
-	/**
-	 * Initialize actor
-	 */
-	@Override
-	public void validateInitialization() throws ValidationException {
-		super.validateInitialization();
-		if (logger.isTraceEnabled()) {
-			logger.trace(getInfo() + " validateInitialization() - entry");
-		}
+    /**
+     * Initialize actor
+     */
+    @Override
+    public void validateInitialization() throws ValidationException {
+        super.validateInitialization();
+        if (logger.isTraceEnabled()) {
+            logger.trace(getInfo() + " validateInitialization() - entry");
+        }
 
+        if (!isMockMode()) {
+            final Director dir = getDirector();
+            if (dir instanceof BasicDirector) {
+                ((BasicDirector) dir).registerFinalizer(this);
+            }
 
-		if (!isMockMode()) {
-			final Director dir = getDirector();
-			if (dir instanceof BasicDirector) {
-				((BasicDirector) dir).registerFinalizer(this);
-			}
+            if (logger.isTraceEnabled()) {
+                logger.trace(getInfo() + "load salsa config");
+            }
 
-			if (logger.isTraceEnabled()) {
-				logger.trace(getInfo() + "load salsa config");
-			}
+            try {
+                conf = (ConfigImpl<?>) ScanUtil.getCurrentSalsaApi().getConfigByPath(confName);
 
-			try {
-				conf = (ConfigImpl<?>) ScanUtil.getCurrentSalsaApi()
-						.getConfigByPath(confName);
+            } catch (final ScanNotFoundException e) {
+                ExecutionTracerService.trace(this, "Error: Unknown scan configuration " + confName);
+                throw new ValidationException("Unknown scan configuration ", confName, e);
+            }
 
-			} catch (final ScanNotFoundException e) {
-				ExecutionTracerService.trace(this,
-						"Error: Unknown scan configuration " + confName);
-				throw new ValidationException(
-						"Unknown scan configuration ", confName, e);
-			}
+            try {
+                configureRecordingSession();
+            } catch (SalsaDeviceException e) {
+                ExecutionTracerService.trace(this, "Error: Recording session configuration error");
+                throw new ValidationException("Error: Recording session configuration error", confName, e);
+            }
 
-			try {
-				configureRecordingSession();
-			} catch (SalsaDeviceException e) {
-				ExecutionTracerService.trace(this,
-						"Error: Recording session configuration error");
-				throw new ValidationException("Error: Recording session configuration error",confName, e);
-			}
+        }
+    }
 
-		}
-	}
+    /**
+     * Actor action
+     */
+    @Override
+    protected void process(final ActorContext ctxt, final ProcessRequest request, final ProcessResponse response)
+            throws ProcessingException {
 
-	/**
-	 * Actor action
-	 */
-	@Override
-	protected void process(final ActorContext ctxt,
-			final ProcessRequest request, final ProcessResponse response)
-			throws ProcessingException {
+        if (logger.isTraceEnabled()) {
+            logger.trace(getInfo() + " process() - entry");
+        }
+        if (isMockMode()) {
+            ExecutionTracerService.trace(this, "MOCK - Scan started with config: " + confName);
+            ExecutionTracerService.trace(this, "MOCK - Scan finished");
+        } else {
+            ExecutionTracerService.trace(this, "Scan started with config: " + confName);
 
-		if (logger.isTraceEnabled()) {
-			logger.trace(getInfo() + " process() - entry");
-		}
-		if (isMockMode()) {
-			ExecutionTracerService.trace(this,
-					"MOCK - Scan started with config: " + confName);
-			ExecutionTracerService.trace(this, "MOCK - Scan finished");
-		} else {
-			ExecutionTracerService.trace(this, "Scan started with config: "
-					+ confName);
+            scanTask = new ScanTask(conf, 1000);
+            scanTask.run();
+            if (scanTask.hasFailed()) {
+                throw new ProcessingExceptionWithLog(this, Severity.FATAL, scanTask.getError().getMessage(), this,
+                        scanTask.getError());
+            } else {
+                ExecutionTracerService.trace(this, "Scan finished");
+            }
 
-			scanTask = new ScanTask(conf, 1000);
-			scanTask.run();
-			if (scanTask.hasFailed()) {
-				throw new ProcessingExceptionWithLog(this, Severity.FATAL,
-						scanTask.getError().getMessage(), this, scanTask
-								.getError());
-			} else {
-				ExecutionTracerService.trace(this, "Scan finished");
-			}
+        }
 
-		}
+        sendOutputMsg(output, PasserelleUtil.createTriggerMessage());
 
-		sendOutputMsg(output, PasserelleUtil.createTriggerMessage());
+        if (logger.isTraceEnabled()) {
+            logger.trace(getInfo() + " doFire() - exit");
+        }
+    }
 
-		if (logger.isTraceEnabled()) {
-			logger.trace(getInfo() + " doFire() - exit");
-		}
-	}
+    private void configureRecordingSession() throws SalsaDeviceException {
+        // FIXME : http://controle.synchrotron-soleil.fr/mantis/view.php?id=25591
+        DeviceProxyFactory.remove(ScanUtil.getCurrentSalsaApi().getDevicePreferences().getScanServer().toLowerCase());
 
-	private void configureRecordingSession() throws SalsaDeviceException {
-		if (DataRecorder.getInstance().isSaveActive(this)) {
-			ScanUtil.getCurrentSalsaApi().setDataRecorderPartialMode(true);
-			//System.out.println("setDataRecorderPartialMode(true)");
-		} else {
-			ScanUtil.getCurrentSalsaApi().setDataRecorderPartialMode(false);
-			//System.out.println("setDataRecorderPartialMode(false)");
-		}
-	}
+        if (DataRecorder.getInstance().isSaveActive(this)) {
+            ScanUtil.getCurrentSalsaApi().setDataRecorderPartialMode(true);
+            // System.out.println("setDataRecorderPartialMode(true)");
+        } else {
+            
+            ScanUtil.getCurrentSalsaApi().setDataRecorderPartialMode(false);
+            // System.out.println("setDataRecorderPartialMode(false)");
+        }
+    }
 
-	@Override
-	protected void doStop() {
-		super.doStop();
-		stopScan();
-	}
+    @Override
+    protected void doStop() {
+        super.doStop();
+        stopScan();
+    }
 
-	@Override
-	public void doFinalAction() {
-		//System.out.println("SCAN : dofinal action is requiered");
-	//	stopScan();
-	}
+    @Override
+    public void doFinalAction() {
+        // System.out.println("SCAN : dofinal action is requiered");
+        // stopScan();
+    }
 
-	@Override
-	protected void doPauseFire() {
-		super.doPauseFire();
-		if (!isMockMode()) {
-			try {
-				if (scanTask != null) {
-					if (scanTask.isRunning()) {
-						ExecutionTracerService.trace(this, "Scan is paused");
-						ScanUtil.getCurrentSalsaApi().pauseScan();
-					}
-				}
-			} catch (final Exception e) {
-				e.printStackTrace();
-				// ignore error since it is impossible to throw it
-			}
-		}
-	}
+    @Override
+    protected void doPauseFire() {
+        super.doPauseFire();
+        if (!isMockMode()) {
+            try {
+                if (scanTask != null) {
+                    if (scanTask.isRunning()) {
+                        ExecutionTracerService.trace(this, "Scan is paused");
+                        ScanUtil.getCurrentSalsaApi().pauseScan();
+                    }
+                }
+            } catch (final Exception e) {
+                e.printStackTrace();
+                // ignore error since it is impossible to throw it
+            }
+        }
+    }
 
-	@Override
-	protected void doResumeFire() {
-		super.doResumeFire();
-		if (!isMockMode()) {
-			try {
-				if (scanTask != null) {
-					if (scanTask.isRunning()) {
-						ScanUtil.getCurrentSalsaApi().resumeScan();
-						ExecutionTracerService.trace(this, "Scan is resumed");
-					}
-				}
-			} catch (final Exception e) {
-				e.printStackTrace();
-				// ignore error since it is impossible to throw it
-			}
-		}
-	}
+    @Override
+    protected void doResumeFire() {
+        super.doResumeFire();
+        if (!isMockMode()) {
+            try {
+                if (scanTask != null) {
+                    if (scanTask.isRunning()) {
+                        ScanUtil.getCurrentSalsaApi().resumeScan();
+                        ExecutionTracerService.trace(this, "Scan is resumed");
+                    }
+                }
+            } catch (final Exception e) {
+                e.printStackTrace();
+                // ignore error since it is impossible to throw it
+            }
+        }
+    }
 
-	private void stopScan() {
-		if (!isMockMode()) {
-			try {
-				if (scanTask != null) {
-					if (scanTask.isRunning()) {
-						ScanUtil.getCurrentSalsaApi().stopScan(conf);
-						ExecutionTracerService.trace(this, "Scan aborted");
-					}
-				}
-			} catch (final Exception e) {
-				e.printStackTrace();
-				// ignore error since it is impossible to throw it
-			}
-		}
-	}
+    private void stopScan() {
+        if (!isMockMode()) {
+            try {
+                if (scanTask != null) {
+                    if (scanTask.isRunning()) {
+                        ScanUtil.getCurrentSalsaApi().stopScan(conf);
+                        ExecutionTracerService.trace(this, "Scan aborted");
+                    }
+                }
+            } catch (final Exception e) {
+                e.printStackTrace();
+                // ignore error since it is impossible to throw it
+            }
+        }
+    }
 
-	@Override
-	public void attributeChanged(final Attribute arg0)
-			throws IllegalActionException {
-		if (arg0 == scanConfigParam) {
-			confName = PasserelleUtil.getParameterValue(scanConfigParam);
-		} else {
-			super.attributeChanged(arg0);
-		}
-	}
+    @Override
+    public void attributeChanged(final Attribute arg0) throws IllegalActionException {
+        if (arg0 == scanConfigParam) {
+            confName = PasserelleUtil.getParameterValue(scanConfigParam);
+        } else {
+            super.attributeChanged(arg0);
+        }
+    }
 
-	@Override
-	protected String getExtendedInfo() {
-		return this.getName();
-	}
+    @Override
+    protected String getExtendedInfo() {
+        return this.getName();
+    }
 
-	@Override
-	public Object clone(final Workspace workspace)
-			throws CloneNotSupportedException {
-		final Scan copy = (Scan) super.clone(workspace);
-		// TODO clone Scan V3
-		// copy.scanApi = new ScanApi(copy);
-		return copy;
-	}
-
+    @Override
+    public Object clone(final Workspace workspace) throws CloneNotSupportedException {
+        final Scan copy = (Scan) super.clone(workspace);
+        // TODO clone Scan V3
+        // copy.scanApi = new ScanApi(copy);
+        return copy;
+    }
 
 }
