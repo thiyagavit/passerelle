7f5f32f68f2043933ff79130a762e422579b1f3f
erwindl0 <erwindl0@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-09-13 13:33:52 +0000

    add CDMA Sql Query actor

--- contributions-actors/fr.soleil.passerelle.cdma.actor/src/main/java/fr/soleil/passerelle/cdma/actor/CDMAArrayFileWriter.java
+++ contributions-actors/fr.soleil.passerelle.cdma.actor/src/main/java/fr/soleil/passerelle/cdma/actor/CDMAArrayFileWriter.java
@@ -11,22 +11,27 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-*/
+ */
 package fr.soleil.passerelle.cdma.actor;
 
 import java.io.File;
-import org.cdma.engine.nexus.array.NexusArray;
-import org.cdma.engine.nexus.navigation.NexusDataItem;
-import org.cdma.engine.nexus.navigation.NexusDataset;
 import org.cdma.interfaces.IArray;
-import org.cdma.plugin.soleil.array.NxsArray;
-import org.cdma.plugin.soleil.internal.NexusDatasetImpl;
+import org.cdma.plugin.soleil.nexus.array.NxsArray;
+import org.cdma.plugin.soleil.nexus.navigation.NxsDataItem;
+import org.cdma.plugin.soleil.nexus.navigation.NxsDataset;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ptolemy.actor.gui.style.CheckBoxStyle;
+import ptolemy.data.BooleanToken;
+import ptolemy.data.expr.FileParameter;
+import ptolemy.data.expr.Parameter;
+import ptolemy.data.expr.StringParameter;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
+import com.isencia.passerelle.actor.InitializationException;
 import com.isencia.passerelle.actor.ProcessingException;
+import com.isencia.passerelle.actor.ValidationException;
 import com.isencia.passerelle.actor.v5.Actor;
 import com.isencia.passerelle.actor.v5.ActorContext;
 import com.isencia.passerelle.actor.v5.ProcessRequest;
@@ -35,27 +40,39 @@ import com.isencia.passerelle.core.ErrorCode;
 import com.isencia.passerelle.core.Port;
 import com.isencia.passerelle.core.PortFactory;
 import com.isencia.passerelle.message.ManagedMessage;
-import fr.soleil.nexus.DataItem;
-import fr.soleil.nexus.PathData;
-
 
 /**
- * Writes an IArray to a destination nexus file.
- * 
- * REMARK : This actor doesn't function yet!!!
+ * Writes the received IArrays to a destination nexus file. In append mode, each received array is stored in a data item with the iteration counter as appended
+ * index in its name.
  * 
  * @author delerw
- *
  */
 public class CDMAArrayFileWriter extends Actor {
   private static final long serialVersionUID = 1L;
   private final static Logger LOGGER = LoggerFactory.getLogger(CDMAArrayFileWriter.class);
-
   public Port input;
+  public FileParameter skeletonFileParameter;
+  public StringParameter outputFileParameter;
+  public StringParameter itemNameParameter;
+  public Parameter appendModeParameter;
+  private File outputFile;
+  private File skeletonFile;
 
   public CDMAArrayFileWriter(CompositeEntity container, String name) throws IllegalActionException, NameDuplicationException {
     super(container, name);
     input = PortFactory.getInstance().createInputPort(this, IArray.class);
+    skeletonFileParameter = new FileParameter(this, "CDMA write skeleton file");
+    skeletonFileParameter.setExpression("$HOME/cdma_trials/testSkeleton.nxs");
+    outputFileParameter = new StringParameter(this, "Output file");
+    outputFileParameter.setExpression("C:/Users/delerw/cdma_trials/testOutput.nxs");
+    itemNameParameter = new StringParameter(this, "Result item name");
+    itemNameParameter.setExpression("myItem");
+    appendModeParameter = new Parameter(this, "Append mode", BooleanToken.TRUE);
+    new CheckBoxStyle(appendModeParameter, "checkbox");
+    registerConfigurableParameter(outputFileParameter);
+    registerConfigurableParameter(itemNameParameter);
+    registerConfigurableParameter(appendModeParameter);
+    registerExpertParameter(skeletonFileParameter);
   }
 
   public Logger getLogger() {
@@ -63,38 +80,58 @@ public class CDMAArrayFileWriter extends Actor {
   }
 
   @Override
+  protected void validateInitialization() throws ValidationException {
+    super.validateInitialization();
+    try {
+      if (!skeletonFileParameter.asFile().exists()) {
+        throw new ValidationException(ErrorCode.ACTOR_INITIALISATION_ERROR, "Invalid skeleton file " + skeletonFileParameter.stringValue(), this, null);
+      }
+    } catch (Exception e) {
+      throw new ValidationException(ErrorCode.ACTOR_INITIALISATION_ERROR, "Invalid skeleton file " + skeletonFileParameter, this, e);
+    }
+  }
+
+  @Override
+  protected void doInitialize() throws InitializationException {
+    super.doInitialize();
+    outputFile = null;
+    skeletonFile = null;
+  }
+
+  @Override
   public void process(ActorContext ctxt, ProcessRequest request, ProcessResponse response) throws ProcessingException {
     ManagedMessage msg = request.getMessage(input);
-
     try {
-      NxsArray array = (NxsArray) msg.getBodyContent();
-      IArray[] parts = array.getArrayParts();
-      NexusArray nxsArr = (NexusArray) parts[0];
-////      DataItem dataItem = nxsArr.getDataItem();
-//      AcquisitionData writer = new AcquisitionData();
-////      PathGroup pathGroup = new PathGroup(PathNexus.splitStringPath("my/attr"));
-//      writer.setFile("C:/temp/test.nxs");
-//      writer.writeAttr("hello", array.getFactoryName(), PathNexus.ROOT_PATH);
-//      writer.writeData(dataItem, PathData.Convert(dataItem.getPath()));
-//      writer.finalize();
-      
-      //NxsDataset nxsDataset = NxsDataset.instanciate(new URI("C:/temp/test.nxs"));
-      
-     // IFactory nxsFactory = Factory.getFactory("SoleilNeXus");
-      
-      NexusDataset nxsDataset = new NexusDatasetImpl(new File("C:/temp/test.nxs"),false);
-      DataItem item = new DataItem(nxsArr.getStorage());
-      item.setPath(PathData.ROOT_PATH);
-      NexusDataItem dataItem = new NexusDataItem(array.getFactoryName(),item,nxsDataset.getRootGroup(),nxsDataset);
-      dataItem.setParent(nxsDataset.getRootGroup());
-      dataItem.setName("mydataitem");
-//      NexusAttribute attr = new NexusAttribute(array.getFactoryName(), "hello", "world");
+      boolean appendMode = ((BooleanToken) appendModeParameter.getToken()).booleanValue();
+      boolean doAppend = false;
+      String itemName = itemNameParameter.stringValue();
+      if (skeletonFile == null) {
+        skeletonFile = skeletonFileParameter.asFile();
+      }
+      if (appendMode) {
+        itemName += request.getIterationCount();
+        if (outputFile != null) {
+          // need to pick this one as skeleton now to continue appending to it
+          skeletonFile = outputFile;
+          doAppend = true;
+        }
+      }
+      if (outputFile == null) {
+        outputFile = new File(outputFileParameter.stringValue());
+      }
+      IArray rcvdArray = (IArray) msg.getBodyContent();
+      NxsDataset nxsDataset = NxsDataset.instanciate(skeletonFile.toURI());
+      NxsDataItem dataItem = new NxsDataItem();
+      dataItem.setName(itemName);
+      NxsArray array = new NxsArray((NxsArray) rcvdArray);
+      dataItem.setCachedData(array, false);
       nxsDataset.getRootGroup().addDataItem(dataItem);
-      
-      
       nxsDataset.getRootGroup().addStringAttribute("hello", "world");
-      
-      nxsDataset.save();
+//      if (doAppend) {
+//        nxsDataset.save();
+//      } else {
+        nxsDataset.saveTo(outputFile.getAbsolutePath());
+//      }
     } catch (Throwable e) {
       throw new ProcessingException(ErrorCode.ACTOR_EXECUTION_ERROR, "", this, e);
     }
