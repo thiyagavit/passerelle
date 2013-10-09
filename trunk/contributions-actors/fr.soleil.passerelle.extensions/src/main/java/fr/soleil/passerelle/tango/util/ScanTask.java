b561c5f9ee6a1b9c875d8daed897c2cb0d58af54
sandra.pierrejoseph@synchrotron-soleil.fr <sandra.pierrejoseph@synchrotron-soleil.fr@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2013-10-09 10:48:46 +0000

    soleil-extensions:cf 25506 : log addition

--- trunk/contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/tango/util/ScanTask.java
+++ trunk/contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/tango/util/ScanTask.java
@@ -11,89 +11,103 @@ import fr.soleil.salsa.api.SalsaAPI;
 import fr.soleil.salsa.entity.IConfig;
 import fr.soleil.salsa.entity.IScanStatus;
 import fr.soleil.salsa.entity.ScanState;
+import fr.soleil.salsa.exception.SalsaDeviceException;
 import fr.soleil.salsa.exception.SalsaException;
 import fr.soleil.salsa.exception.SalsaLoggingException;
+import fr.soleil.salsa.exception.SalsaScanConfigurationException;
 
 public class ScanTask implements Runnable {
 
-	private final static Logger logger = LoggerFactory
-			.getLogger(ScanTask.class);
-	private volatile boolean cancelled = false;
-	private volatile Exception exception;
-	private volatile boolean hasFailed;
-	private volatile boolean isRunning;
+    private final static Logger logger = LoggerFactory.getLogger(ScanTask.class);
+    private volatile boolean cancelled = false;
+    private volatile Exception exception;
+    private volatile boolean hasFailed;
+    private volatile boolean isRunning;
 
-	private final IConfig<?> conf;
-	private final int pollingPeriod;
+    private final IConfig<?> conf;
+    private final int pollingPeriod;
 
-	public ScanTask(final IConfig<?> config, final int poll) {
+    public ScanTask(final IConfig<?> config, final int poll) {
 
-		conf = config;
-		pollingPeriod = poll;
-	}
+        conf = config;
+        pollingPeriod = poll;
+    }
 
-	public void run() {
-		final SalsaAPI currentSalsaApi = ScanUtil.getCurrentSalsaApi();
-		isRunning = true;
-		hasFailed = false;
+    public void run() {
+        final SalsaAPI currentSalsaApi = ScanUtil.getCurrentSalsaApi();
+        isRunning = true;
+        hasFailed = false;
 
-		try {
-			// long start = System.currentTimeMillis();
-			try {
-				currentSalsaApi.startScan(conf);
-			} catch (final SalsaLoggingException e) {
-				e.printStackTrace();
-			} catch (final Exception e) {
-				throw e;
-			}
-			// long end = System.currentTimeMillis();
-			// System.out.println("Duration = " + (end - start));
+        try {
+            // long start = System.currentTimeMillis();
+            try {
+                currentSalsaApi.startScan(conf);
+                logger.info("Scan is running");
+            } catch (final SalsaLoggingException e) {
+                e.printStackTrace();
+            } catch (final SalsaDeviceException e) {
+                logger.error("SalsaDeviceException : Scan not started, because {} ", e.getMessage());
+                throw e;
+            } catch (final SalsaScanConfigurationException e) {
+                logger.error("SalsaScanConfigurationException : Scan not started, because {} ", e.getMessage());
+                throw e;
+            }
+        } catch (final SalsaException e) {
+            hasFailed = true;
+            exception = e;
+        }
+        if (!hasFailed) {
+            // long end = System.currentTimeMillis();
+            // System.out.println("Duration = " + (end - start));
+            try {
 
-			ScanState currentState = currentSalsaApi.getScanState();
-			while ((currentState == ScanState.RUNNING || currentState == ScanState.PAUSED)
-					&& !cancelled) {
+                ScanState currentState = currentSalsaApi.getScanState();
+                while ((currentState == ScanState.RUNNING || currentState == ScanState.PAUSED) && !cancelled) {
 
-				try {
-					Thread.sleep(pollingPeriod);
-				} catch (final InterruptedException e) {
-					e.printStackTrace();
-				}
-				currentState = currentSalsaApi.getScanState();
-				// System.out.println("CurrentState = " + currentState);
-			}
+                    try {
+                        Thread.sleep(pollingPeriod);
+                    } catch (final InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    currentState = currentSalsaApi.getScanState();
+                    // System.out.println("CurrentState = " + currentState);
+                }
 
-			if (currentSalsaApi.getScanState() == ScanState.ABORT) {
-				hasFailed = true;
-				final IScanStatus status = currentSalsaApi.getStatus();
-				exception = new PasserelleException(Severity.FATAL,
-						"The Scan has been interrupted with status: " + status.getStatus(), null, null);
-			}
+                if (currentSalsaApi.getScanState() == ScanState.ABORT) {
+                    hasFailed = true;
+                    final IScanStatus status = currentSalsaApi.getStatus();
+                    exception = new PasserelleException(Severity.FATAL, "The Scan has been interrupted with status: "
+                            + status.getStatus(), null, null);
+                }
 
-		} catch (final SalsaException e) {
-			hasFailed = true;
-			exception = e;
-		} catch (final Exception e) {
-			hasFailed = true;
-			exception = e;
-		}
+            } catch (final SalsaDeviceException e) {
+                logger.error("SalsaDeviceException : Scan has been interrupted on getState, because {} ", e.getMessage());
+                hasFailed = true;
+                exception = e;
 
-		isRunning = false;
-	}
+            } catch (final Exception e) {
+                hasFailed = true;
+                exception = e;
+            }
+        }
 
-	public void cancel() {
-		cancelled = true;
-		logger.debug("Scan task cancelled " + cancelled);
-	}
+        isRunning = false;
+    }
 
-	public boolean hasFailed() {
-		return hasFailed;
-	}
+    public void cancel() {
+        cancelled = true;
+        logger.debug("Scan task cancelled " + cancelled);
+    }
 
-	public Exception getError() {
-		return exception;
-	}
+    public boolean hasFailed() {
+        return hasFailed;
+    }
 
-	public boolean isRunning() {
-		return isRunning;
-	}
+    public Exception getError() {
+        return exception;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
 }
