392dcda7cd1df8a2781d8c2007610b9edb489eaf
erwindl0 <erwindl0@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2014-03-20 14:45:04 +0000

    correct copyright

--- contributions-actors/uk.ac.diamond.python.service/src/main/java/uk/ac/diamond/python/service/PythonService.java
+++ contributions-actors/uk.ac.diamond.python.service/src/main/java/uk/ac/diamond/python/service/PythonService.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright 2014 Diamond Light Source Ltd. & iSencia Belgium NV
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.diamond.python.service;
 
 import java.io.File;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -13,8 +27,10 @@ import uk.ac.diamond.scisoft.analysis.rpc.AnalysisRpcRemoteException;
 import com.isencia.util.commandline.ManagedCommandline;
 
 /**
- * This class encapsulates a system command to python used with the RPC service and drives python using the diamond RPC link. It is a subset of the RPC
- * interface available by diamond pertinent to running python commands in the way jep does.
+ * This class encapsulates a system command to python used with the RPC service.
+ * Minimized version of the org.dawnsci.python.rpc.PythonService.
+ * 
+ * @author erwindl
  */
 public class PythonService {
 
