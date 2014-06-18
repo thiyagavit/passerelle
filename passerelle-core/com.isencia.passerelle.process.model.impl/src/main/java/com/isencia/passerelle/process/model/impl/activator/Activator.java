89db41f5e607b677cb12efe6f533e91efbbd3f46
jan.vermeulen.es <jan.vermeulen.es@gmail.com>
2014-06-18 08:04:27 +0000

    use ProcessFactory, ProcessManager and ProcessPersistenceService

--- passerelle-core/com.isencia.passerelle.process.model.impl/src/main/java/com/isencia/passerelle/process/model/impl/activator/Activator.java
+++ passerelle-core/com.isencia.passerelle.process.model.impl/src/main/java/com/isencia/passerelle/process/model/impl/activator/Activator.java
@@ -8,11 +8,8 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 import com.isencia.passerelle.process.model.ResultItemFromRawBuilderRegistry;
-import com.isencia.passerelle.process.model.factory.ProcessFactory;
 import com.isencia.passerelle.process.model.impl.ResultItemFromRawBuilderNOP;
 import com.isencia.passerelle.process.model.impl.ResultItemFromRawBuilderRegistryImpl;
-import com.isencia.passerelle.process.model.impl.factory.ProcessFactoryImpl;
-import com.isencia.passerelle.process.service.ServiceRegistry;
 
 /**
  * @author "puidir"
@@ -20,25 +17,15 @@ import com.isencia.passerelle.process.service.ServiceRegistry;
  */
 public class Activator implements BundleActivator {
 
-  private ServiceRegistration<ProcessFactory> factoryServiceRegistration;
   private ServiceRegistration<ResultItemFromRawBuilderRegistry> registryServiceRegistration;
 
   public void start(BundleContext bundleContext) throws Exception {
     ResultItemFromRawBuilderRegistryImpl rawBuilderRegistryImpl = new ResultItemFromRawBuilderRegistryImpl();
     rawBuilderRegistryImpl.registerBuilder(new ResultItemFromRawBuilderNOP());
     registryServiceRegistration = bundleContext.registerService(ResultItemFromRawBuilderRegistry.class, rawBuilderRegistryImpl, null);
-    if (ServiceRegistry.getInstance().getProcessFactory() == null) {
-      ProcessFactoryImpl entityFactory = new ProcessFactoryImpl();
-      factoryServiceRegistration = bundleContext.registerService(ProcessFactory.class, entityFactory, null);
-      ServiceRegistry.getInstance().setProcessFactory(entityFactory);
-    }
   }
 
   public void stop(BundleContext bundleContext) throws Exception {
-    if (factoryServiceRegistration != null) {
-      ServiceRegistry.getInstance().setProcessFactory(null);
-      factoryServiceRegistration.unregister();
-    }
     if (registryServiceRegistration != null) {
       registryServiceRegistration.unregister();
     }
