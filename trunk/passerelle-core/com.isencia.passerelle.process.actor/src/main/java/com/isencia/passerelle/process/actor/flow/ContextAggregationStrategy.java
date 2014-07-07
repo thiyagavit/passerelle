f8d7ee452c31c412f1a30f8407a7957219026d41
kheuninck@gmail.com <kheuninck@gmail.com@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2014-07-07 14:30:45 +0000

    some initial changes to make Fork/Join work

--- trunk/passerelle-core/com.isencia.passerelle.process.actor/src/main/java/com/isencia/passerelle/process/actor/flow/ContextAggregationStrategy.java
+++ trunk/passerelle-core/com.isencia.passerelle.process.actor/src/main/java/com/isencia/passerelle/process/actor/flow/ContextAggregationStrategy.java
@@ -11,35 +11,40 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-*/
+ */
 package com.isencia.passerelle.process.actor.flow;
 
 import com.isencia.passerelle.message.ManagedMessage;
 import com.isencia.passerelle.message.MessageException;
 import com.isencia.passerelle.message.internal.MessageContainer;
 import com.isencia.passerelle.process.model.Context;
+import com.isencia.passerelle.process.service.ProcessManager;
 
 /**
- * A <code>AggregationStrategy</code> that looks for processing <code>Context</code>s in the sequenced messages,
- * and aggregates their tasks and results.
+ * A <code>AggregationStrategy</code> that looks for processing <code>Context</code>s in the sequenced messages, and
+ * aggregates their tasks and results.
  * 
  * @author erwin
- *
+ * 
  */
 public class ContextAggregationStrategy implements AggregationStrategy {
 
-  public ManagedMessage aggregateMessages(ManagedMessage initialMsg, ManagedMessage... otherMessages) throws MessageException {
+  public ManagedMessage aggregateMessages(ProcessManager processManager, ManagedMessage initialMsg, ManagedMessage... otherMessages) throws MessageException {
     MessageContainer scopeMsg = (MessageContainer) initialMsg;
     ManagedMessage msg = scopeMsg.copy();
-    Context mergedCtxt = (Context) scopeMsg.getBodyContent();
+    Context mergedCtxt = processManager.getRequest().getProcessingContext();
     Context[] branches = new Context[otherMessages.length];
     for (int i = 0; i < otherMessages.length; i++) {
-      Context branchedCtx = (Context)otherMessages[i].getBodyContent();
       msg.addCauseID(otherMessages[i].getID());
-      branches[i] = branchedCtx;
+      Object bodyContent = otherMessages[i].getBodyContent();
+      if (bodyContent instanceof Context) {
+        Context branchedCtx = (Context) bodyContent;
+        branches[i] = branchedCtx;
+      }else{
+        branches[i] = mergedCtxt;
+      }
     }
     mergedCtxt.join(branches);
-    msg.setBodyContent(mergedCtxt, ManagedMessage.objectContentType);
     return msg;
   }
 }
