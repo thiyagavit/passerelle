c9f0908e48d62cd27d0d091d2c4b4983460ef45f
chingaching@yahoo.fr <chingaching@yahoo.fr@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2014-07-08 16:13:59 +0000

    Add Nb actuators parameter in PreConfiguredScan that create dynamically input ports see JIRA PASSERELLE-88 Modifcation of ScanUtil using interface classes instead of implementation. Fix bug sending trajectory on enable actuator

--- trunk/contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/actor/tango/acquisition/scan/ScanUtil.java
+++ trunk/contributions-actors/fr.soleil.passerelle.extensions/src/main/java/fr/soleil/passerelle/actor/tango/acquisition/scan/ScanUtil.java
@@ -1,144 +1,203 @@
 package fr.soleil.passerelle.actor.tango.acquisition.scan;
 
+import java.util.List;
+
+import com.isencia.passerelle.core.ErrorCode;
 import com.isencia.passerelle.core.PasserelleException;
+
 import fr.soleil.salsa.api.SalsaAPI;
+import fr.soleil.salsa.entity.IActuator;
+import fr.soleil.salsa.entity.IConfig;
+import fr.soleil.salsa.entity.IDevice;
+import fr.soleil.salsa.entity.IDimension;
+import fr.soleil.salsa.entity.IRange;
+import fr.soleil.salsa.entity.IRangeIntegrated;
 import fr.soleil.salsa.entity.ITrajectory;
-import fr.soleil.salsa.entity.impl.scan1d.Config1DImpl;
 import fr.soleil.salsa.entity.impl.scan2d.Config2DImpl;
 import fr.soleil.salsa.entity.impl.scan2d.Dimension2DXImpl;
 import fr.soleil.salsa.entity.impl.scan2d.Dimension2DYImpl;
-import fr.soleil.salsa.entity.scan1d.IDimension1D;
-import fr.soleil.salsa.entity.scan1d.IRange1D;
 import fr.soleil.salsa.entity.scan2D.IRange2DX;
 import fr.soleil.salsa.entity.scan2D.IRange2DY;
 
 public final class ScanUtil {
 
-	// The context (ScanServer Name) is managed by the Api itself and the -DSalsaPreferences
-	private static SalsaAPI m_salsaApi = null;
+    // The context (ScanServer Name) is managed by the Api itself and the -DSalsaPreferences
+    private static SalsaAPI m_salsaApi = null;
 
-	public static SalsaAPI getCurrentSalsaApi() {
+    public static SalsaAPI getCurrentSalsaApi() {
 
-		if (m_salsaApi == null) {
-			m_salsaApi = new SalsaAPI();
-		}
-		return m_salsaApi;
-	}
+        if (m_salsaApi == null) {
+            m_salsaApi = new SalsaAPI();
+        }
+        return m_salsaApi;
+    }
 
-	private static double calculDelta(double endPos, double beginPos, int steps){
-		return Math.abs((endPos - beginPos)/ steps);
-	}
-	private static double calculSpeed(double delta, double integrationTime){
-		return delta / integrationTime;
-	}
-	public static void setTrajectory1D(final Config1DImpl conf,
-			final ScanRangeX scanRangeX) throws PasserelleException {
+    private static double calculDelta(double endPos, double beginPos, int steps) {
+        return Math.abs((endPos - beginPos) / steps);
+    }
 
-		final IDimension1D dim = conf.getDimensionX();
-		if (dim.getRangesXList().size() == 0) {
-			throw new PasserelleException("Range List must not be empty.",
-					null, null);
-		}
+    private static double calculSpeed(double delta, double integrationTime) {
+        return delta / integrationTime;
+    }
 
-		final IRange1D xRange = dim.getRangesXList().get(0);
+    private static void setTrajectory(final IConfig<?> conf, final IRangeIntegrated sourceRange, boolean relative, int dimensionIndex)  throws PasserelleException{
+        if (conf == null) {
+            throw new PasserelleException(ErrorCode.ERROR, "Configuration is not defined.", null);
+        }
 
-		if (xRange.getTrajectoriesList().size() == 0) {
-			throw new PasserelleException(
-					"X Actuators (trajectory) must not be empty.", null, null);
-		}
-		// Range Update
-		xRange.setIntegrationTime(scanRangeX.getIntegrationTime());
-		xRange.setStepsNumber(scanRangeX.getNumberOfSteps());
+        List<IDimension> dimensionList = conf.getDimensionList();
+        if (dimensionList == null || dimensionList.isEmpty() || dimensionIndex >= dimensionList.size()) {
+            throw new PasserelleException(ErrorCode.ERROR, "No dimension found.", null);
+        }
 
-		// Only one actuator
-		final ITrajectory xtrajectory = xRange.getTrajectoriesList().get(0);
-		xtrajectory.setBeginPosition(scanRangeX.getFrom());
-		xtrajectory.setEndPosition(scanRangeX.getTo());
-		xtrajectory.setRelative(scanRangeX.isRelative());
+        IDimension dim = dimensionList.get(dimensionIndex);
+        List<IActuator> actuatorsList = dim.getActuatorsList();
+        int nbEnable = getNbEnableDevice(actuatorsList);
 
-		double delta = calculDelta(xtrajectory.getEndPosition(),xtrajectory
-				.getBeginPosition(), xRange.getStepsNumber());
-		xtrajectory.setDelta(delta);
-		xtrajectory.setSpeed(calculSpeed(delta,xRange.getIntegrationTime()));
+        if (nbEnable == 0) {
+            throw new PasserelleException(ErrorCode.ERROR, "No activated actuator is defined.", null);
+        }
 
-	}
+        if (dim == null || dim.getRangeList() == null || dim.getRangeList().isEmpty()) {
+            throw new PasserelleException(ErrorCode.ERROR, "Range List must not be empty.", null);
+        }
 
-	/*
-	 * public static void setTrajectory2D(final Config2DImpl conf, final
-	 * List<ScanRangeX> scanRangeXList, final List<ScanRangeY> scanRangeYList) {
-	 *
-	 * }
-	 */
-	public static void setTrajectory2D(final Config2DImpl conf,
-			final ScanRangeX scanRangeX, final ScanRangeY scanRangeY)
-			throws PasserelleException {
-		setTrajectory2D(conf, scanRangeX);
-		setTrajectory2D(conf, scanRangeY);
-	}
+        List<? extends IRange> rangeList = dim.getRangeList();
+        if (rangeList.size() > 1) {
+            throw new PasserelleException(ErrorCode.ERROR, "There must be only one range", null);
+        }
 
-	private static void setTrajectory2D(final Config2DImpl conf,
-			final ScanRangeX scanRangeX) throws PasserelleException {
+        final IRange destRange = rangeList.get(0);
+        List<ITrajectory> sourceTrajectoriesList = sourceRange.getTrajectoriesList();
+        List<ITrajectory> destTrajectoriesList = destRange.getTrajectoriesList();
 
-		final Dimension2DXImpl xDim = (Dimension2DXImpl) conf.getDimensionX();
+        if (sourceTrajectoriesList == null || sourceTrajectoriesList.isEmpty() || destTrajectoriesList == null
+                || destTrajectoriesList.isEmpty()) {
+            throw new PasserelleException(ErrorCode.ERROR, "Actuators (trajectory) must not be empty.", null);
+        }
 
-		if (xDim.getRangesList().size() == 0) {
-			throw new PasserelleException("Range List must not be empty.",
-					null, null);
-		}
+        if (nbEnable > sourceTrajectoriesList.size()) {
+            throw new PasserelleException(ErrorCode.ERROR,
+                    "Trajectory list size must be equals to activated actuator list size", null);
+        }
 
-		final IRange2DX xRange = xDim.getRangesList().get(0);
-		// modify one
-		if (xRange.getTrajectoriesList().size() == 0) {
-			throw new PasserelleException("X Trajectory must not be empty.",
-					null, null);
-		}
+        if (destTrajectoriesList.size() < sourceTrajectoriesList.size()) {
+            throw new PasserelleException(ErrorCode.ERROR,
+                    "Trajectory list size must be at least smaller than the trajectory list of the configuration", null);
+        }
 
-		final ITrajectory xTrajectory = xRange.getTrajectoriesList().get(0);
-		// Range Update
-		xRange.setIntegrationTime(scanRangeX.getIntegrationTime());
-		xRange.setStepsNumber(scanRangeX.getNumberOfSteps());
-		// Only one actuator X
-		xTrajectory.setBeginPosition(scanRangeX.getFrom());
-		xTrajectory.setEndPosition(scanRangeX.getTo());
-		xTrajectory.setRelative(scanRangeX.isRelative());
+        // Range Update
+        if (destRange instanceof IRangeIntegrated) {
+            ((IRangeIntegrated) destRange).setIntegrationTime(sourceRange.getIntegrationTime());
+        }
+        destRange.setStepsNumber(sourceRange.getStepsNumber());
 
-		double delta = calculDelta(xTrajectory.getEndPosition(),xTrajectory
-				.getBeginPosition(), xRange.getStepsNumber());
-		xTrajectory.setDelta(delta);
-		xTrajectory.setSpeed(calculSpeed(delta,xRange.getIntegrationTime()));
+        IActuator actuator = null;
+        int nbOfActuator = sourceTrajectoriesList.size();
+        int nbOfActivatedActuator = 0;
+        ITrajectory sourceTrajectory = null;
+        ITrajectory destTrajectory = null;
+        for (int actuatorIndex = 0; actuatorIndex < actuatorsList.size(); actuatorIndex++) {
+            actuator = actuatorsList.get(actuatorIndex);
+            if (actuator.isEnabled() && nbOfActivatedActuator < nbOfActuator) {
+                sourceTrajectory = sourceTrajectoriesList.get(nbOfActivatedActuator);
+                destTrajectory = destTrajectoriesList.get(actuatorIndex);
+                destTrajectory.setBeginPosition(sourceTrajectory.getBeginPosition());
+                destTrajectory.setEndPosition(sourceTrajectory.getEndPosition());
+                destTrajectory.setRelative(relative);
+                nbOfActivatedActuator++;
+            }
+        }
+    }
+    
+    public static void setTrajectory1D(final IConfig<?> conf, final IRangeIntegrated sourceRange, boolean relative)
+            throws PasserelleException {
+        setTrajectory(conf, sourceRange,relative,0);
+    }
 
-	}
+    private static int getNbEnableDevice(List<? extends IDevice> deviceList) {
+        int nb = 0;
+        if (deviceList != null && !deviceList.isEmpty()) {
+            for (IDevice device : deviceList) {
+                if (device.isEnabled()) {
+                    nb++;
+                }
+            }
+        }
+        return nb;
+    }
 
-	private static void setTrajectory2D(final Config2DImpl conf,
-			final ScanRangeY scanRangeY) throws PasserelleException {
+    /*
+     * public static void setTrajectory2D(final Config2DImpl conf, final
+     * List<ScanRangeX> scanRangeXList, final List<ScanRangeY> scanRangeYList) {
+     *
+     * }
+     */
+    public static void setTrajectory2D(final Config2DImpl conf, final ScanRangeX scanRangeX, final ScanRangeY scanRangeY)
+            throws PasserelleException {
+        setTrajectory2D(conf, scanRangeX);
+        setTrajectory2D(conf, scanRangeY);
+    }
 
-		final Dimension2DYImpl yDim = (Dimension2DYImpl) conf.getDimensionY();
+    private static void setTrajectory2D(final Config2DImpl conf, final ScanRangeX scanRangeX)
+            throws PasserelleException {
 
-		if (yDim.getRangesList().size() == 0) {
-			throw new PasserelleException("Range List must not be empty.",
-					null, null);
-		}
+        final Dimension2DXImpl xDim = (Dimension2DXImpl) conf.getDimensionX();
 
-		final IRange2DY yRange = yDim.getRangesList().get(0);
-		if (yRange.getTrajectoriesList().size() == 0) {
-			throw new PasserelleException("Y Trajectory must not be empty.",
-					null, null);
-		}
+        if (xDim.getRangesList().size() == 0) {
+            throw new PasserelleException(ErrorCode.ERROR, "Range List must not be empty.", null);
+        }
 
-		final ITrajectory yTrajectory = yRange.getTrajectoriesList().get(0);
+        final IRange2DX xRange = xDim.getRangesList().get(0);
+        // modify one
+        if (xRange.getTrajectoriesList().size() == 0) {
+            throw new PasserelleException(ErrorCode.ERROR, "X Trajectory must not be empty.", null);
+        }
 
-		// Range Update
-		yRange.setStepsNumber(scanRangeY.getNumberOfSteps());
+        final ITrajectory xTrajectory = xRange.getTrajectoriesList().get(0);
+        // Range Update
+        xRange.setIntegrationTime(scanRangeX.getIntegrationTime());
+        xRange.setStepsNumber(scanRangeX.getNumberOfSteps());
+        // Only one actuator X
+        xTrajectory.setBeginPosition(scanRangeX.getFrom());
+        xTrajectory.setEndPosition(scanRangeX.getTo());
+        xTrajectory.setRelative(scanRangeX.isRelative());
 
-		// Only one actuator Y
-		yTrajectory.setBeginPosition(scanRangeY.getFrom());
-		yTrajectory.setEndPosition(scanRangeY.getTo());
-		yTrajectory.setRelative(scanRangeY.isRelative());
+        double delta = calculDelta(xTrajectory.getEndPosition(), xTrajectory.getBeginPosition(),
+                xRange.getStepsNumber());
+        xTrajectory.setDelta(delta);
+        xTrajectory.setSpeed(calculSpeed(delta, xRange.getIntegrationTime()));
 
-		double delta = calculDelta(yTrajectory.getEndPosition(),yTrajectory
-				.getBeginPosition(), yRange.getStepsNumber());
-		yTrajectory.setDelta(delta);
-		yTrajectory.setSpeed(calculSpeed(delta,yRange.getStepsNumber()));
+    }
 
-	}
+    private static void setTrajectory2D(final Config2DImpl conf, final ScanRangeY scanRangeY)
+            throws PasserelleException {
+
+        final Dimension2DYImpl yDim = (Dimension2DYImpl) conf.getDimensionY();
+
+        if (yDim.getRangesList().size() == 0) {
+            throw new PasserelleException(ErrorCode.ERROR, "Range List must not be empty.", null);
+        }
+
+        final IRange2DY yRange = yDim.getRangesList().get(0);
+        if (yRange.getTrajectoriesList().size() == 0) {
+            throw new PasserelleException(ErrorCode.ERROR, "Y Trajectory must not be empty.", null);
+        }
+
+        final ITrajectory yTrajectory = yRange.getTrajectoriesList().get(0);
+
+        // Range Update
+        yRange.setStepsNumber(scanRangeY.getNumberOfSteps());
+
+        // Only one actuator Y
+        yTrajectory.setBeginPosition(scanRangeY.getFrom());
+        yTrajectory.setEndPosition(scanRangeY.getTo());
+        yTrajectory.setRelative(scanRangeY.isRelative());
+
+        double delta = calculDelta(yTrajectory.getEndPosition(), yTrajectory.getBeginPosition(),
+                yRange.getStepsNumber());
+        yTrajectory.setDelta(delta);
+        yTrajectory.setSpeed(calculSpeed(delta, yRange.getStepsNumber()));
+
+    }
 }
