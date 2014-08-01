943a300f873ad6e76acf00eee53efef620ad4cae
jurgenderoeck <jurgenderoeck@gmail.com>
2014-08-01 10:36:18 +0000

    datetimeparameter on branch 8.7

--- passerelle-core/com.isencia.passerelle.engine/src/main/java/com/isencia/passerelle/util/ptolemy/DateTimeParameter.java
+++ passerelle-core/com.isencia.passerelle.engine/src/main/java/com/isencia/passerelle/util/ptolemy/DateTimeParameter.java
@@ -11,7 +11,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-*/
+ */
 package com.isencia.passerelle.util.ptolemy;
 
 import java.text.DateFormat;
@@ -35,41 +35,83 @@ import ptolemy.kernel.util.NamedObj;
  * @author erwin
  */
 public class DateTimeParameter extends StringParameter {
-	private final static Logger logger = LoggerFactory.getLogger(DateTimeParameter.class);
-	private final static DateFormat format1 = new SimpleDateFormat("dd MMM yyyy HH:mm:ss");
-	private final static DateFormat format2 = new SimpleDateFormat("dd MMM yyyy");
-	
-	/**
-	 * @param container
-	 * @param name
-	 * @throws IllegalActionException
-	 * @throws NameDuplicationException
-	 */
-	public DateTimeParameter(NamedObj container, String name) throws IllegalActionException, NameDuplicationException {
-		super(container, name);
-	}
 
-	public void setDateValue(Date value) {
-		if(value!=null)
-			setExpression(format1.format(value));
-		else
-			setExpression("");
-	}
-	public Date getDateValue() {
-		try {
-			return format1.parse(stringValue());
-		} catch (IllegalActionException e) {
-			logger.error("",e);
-			return null;
-		} catch (ParseException e) {
-			try {
-				return format2.parse(stringValue());
-			} catch (IllegalActionException ex) {
-				logger.error("",ex);
-				return null;
-			} catch (ParseException ex) {
-				return null;
-			}
-		}
-	}
+	private static final long serialVersionUID = -7023867564820251370L;
+
+  private final static Logger logger = LoggerFactory.getLogger(DateTimeParameter.class);
+  private final static DateFormat format1 = new SimpleDateFormat("dd MMM yyyy HH:mm:ss");
+  private final static DateFormat format2 = new SimpleDateFormat("dd MMM yyyy");
+
+  private DateFormat format;
+
+  /**
+   * 
+   * @param container
+   * @param name
+   * @param format
+   * @throws IllegalActionException
+   * @throws NameDuplicationException
+   */
+  public DateTimeParameter(NamedObj container, String name, DateFormat format) throws IllegalActionException, NameDuplicationException {
+    this(container, name);
+    this.format = format;
+  }
+  /**
+   * @param container
+   * @param name
+   * @throws IllegalActionException
+   * @throws NameDuplicationException
+   */
+  public DateTimeParameter(NamedObj container, String name) throws IllegalActionException, NameDuplicationException {
+    super(container, name);
+  }
+
+  public void setDateValue(Date value) {
+    if(value!=null) {
+      if (format != null) {
+        setExpression(format.format(value));
+      } else {
+        setExpression(format1.format(value));
+      }
+    } else {
+      setExpression("");
+    }
+  }
+  public Date getDateValue() {
+
+    if (format != null) {
+      try {
+        return format.parse(stringValue());
+      } catch (IllegalActionException e) {
+        logger.error("", e);
+        return null;
+      } catch (ParseException e) {
+        return getDefaultDateValue();
+      }
+    }
+
+    return getDefaultDateValue();
+  }
+
+  /**
+   * 
+   * @return Date based on one of the default DateFormats defined here(format1, format2..)
+   */
+  private Date getDefaultDateValue() {
+    try {
+      return format1.parse(stringValue());
+    } catch (IllegalActionException e) {
+      logger.error("",e);
+      return null;
+    } catch (ParseException e) {
+      try {
+        return format2.parse(stringValue());
+      } catch (IllegalActionException ex) {
+        logger.error("",ex);
+        return null;
+      } catch (ParseException ex) {
+        return null;
+      }
+    }
+  }
 }
