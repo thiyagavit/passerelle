ab46fbf99fce97c5a4986185d84c3180f73672ac
erwindl0 <erwindl0@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2015-02-11 10:23:20 +0000

    synchronize Netutils' free port searching

--- contributions-actors/uk.ac.diamond.python.service/src/main/java/uk/ac/diamond/python/service/util/NetUtils.java
+++ contributions-actors/uk.ac.diamond.python.service/src/main/java/uk/ac/diamond/python/service/util/NetUtils.java
@@ -6,7 +6,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- */ 
+ */
 package uk.ac.diamond.python.service.util;
 
 import java.io.IOException;
@@ -15,53 +15,47 @@ import java.net.ServerSocket;
 
 public class NetUtils {
 
-	/**
-	 * Attempts to get a free port starting at the passed in port and
-	 * working up.
-	 * 
-	 * @param startPort
-	 * @return
-	 */
-	public static int getFreePort(final int startPort) {
-		
-	    int port = startPort;
-	    while(!NetUtils.isPortFree(port)) port++;
-	    	
-	    return port;
-	}
+  /**
+   * Attempts to get a free port starting at the passed in port and working up.
+   * 
+   * @param startPort
+   * @return
+   */
+  public static synchronized int getFreePort(final int startPort) {
+    int port = startPort;
+    while (!NetUtils.isPortFree(port))
+      port++;
+    return port;
+  }
 
-
-	/**
-	 * Checks if a port is free.
-	 * @param port
-	 * @return
-	 */
-	public static boolean isPortFree(int port) {
-
-	    ServerSocket ss = null;
-	    DatagramSocket ds = null;
-	    try {
-	        ss = new ServerSocket(port);
-	        ss.setReuseAddress(true);
-	        ds = new DatagramSocket(port);
-	        ds.setReuseAddress(true);
-	        return true;
-	    } catch (IOException e) {
-	    } finally {
-	        if (ds != null) {
-	            ds.close();
-	        }
-
-	        if (ss != null) {
-	            try {
-	                ss.close();
-	            } catch (IOException e) {
-	                /* should not be thrown */
-	            }
-	        }
-	    }
-
-	    return false;
-	}
-
+  /**
+   * Checks if a port is free.
+   * 
+   * @param port
+   * @return
+   */
+  public static synchronized boolean isPortFree(int port) {
+    ServerSocket ss = null;
+    DatagramSocket ds = null;
+    try {
+      ss = new ServerSocket(port);
+      ss.setReuseAddress(true);
+      ds = new DatagramSocket(port);
+      ds.setReuseAddress(true);
+      return true;
+    } catch (IOException e) {
+    } finally {
+      if (ds != null) {
+        ds.close();
+      }
+      if (ss != null) {
+        try {
+          ss.close();
+        } catch (IOException e) {
+          /* should not be thrown */
+        }
+      }
+    }
+    return false;
+  }
 }
