08c6670d97b0ec374eea3799f104dccb4c2c58bb
dimitri.devos@gmail.com <dimitri.devos@gmail.com@62d17b0d-fca0-a5f6-7928-6b95978538f4>
2015-04-30 09:50:30 +0000

    return more specific task class at creation

--- trunk/passerelle-core/com.isencia.passerelle.process.model/src/main/java/com/isencia/passerelle/process/model/factory/ProcessFactory.java
+++ trunk/passerelle-core/com.isencia.passerelle.process.model/src/main/java/com/isencia/passerelle/process/model/factory/ProcessFactory.java
@@ -28,56 +28,61 @@ import com.isencia.passerelle.process.model.Task;
  */
 public interface ProcessFactory {
 
-	Attribute createAttribute(AttributeHolder holder, String name, String value);
+  Attribute createAttribute(AttributeHolder holder, String name, String value);
 
-	/**
-	 * Create a new Case.
-	 * 
-	 * @param externalReference
-	 *            Can be used to link the Case to external system, e.g. order
-	 *            identifiers, client numbers, etc
-	 */
-	Case createCase(String externalReference);
+  /**
+   * Create a new Case.
+   * 
+   * @param externalReference
+   *          Can be used to link the Case to external system, e.g. order identifiers, client numbers, etc
+   */
+  Case createCase(String externalReference);
 
-	ContextErrorEvent createContextErrorEvent(Context context, ErrorItem errorItem);
+  ContextErrorEvent createContextErrorEvent(Context context, ErrorItem errorItem);
 
-	ContextErrorEvent createContextErrorEvent(Context context, Severity severity, ErrorCategory category, String code, String shortDescription, String description, Set<String> relatedDataTypes);
+  ContextErrorEvent createContextErrorEvent(Context context, Severity severity, ErrorCategory category, String code, String shortDescription, String description,
+      Set<String> relatedDataTypes);
 
-	ContextErrorEvent createContextErrorEvent(Context context, Severity severity, ErrorCategory category, String code, String shortDescription, Throwable cause, Set<String> relatedDataTypes);
+  ContextErrorEvent createContextErrorEvent(Context context, Severity severity, ErrorCategory category, String code, String shortDescription, Throwable cause,
+      Set<String> relatedDataTypes);
 
-	ContextEvent createContextEvent(Context context, String topic, String message);
+  ContextEvent createContextEvent(Context context, String topic, String message);
 
-	ErrorItem createErrorItem(Severity severity, ErrorCategory category, String code, String shortDescription, String description, List<String> detailedDescriptions, Set<String> relatedDataTypes);
+  ErrorItem createErrorItem(Severity severity, ErrorCategory category, String code, String shortDescription, String description, List<String> detailedDescriptions,
+      Set<String> relatedDataTypes);
 
-	ErrorItem createErrorItem(Severity severity, ErrorCategory category, String code, String shortDescription, Throwable cause, Set<String> relatedDataTypes);
+  ErrorItem createErrorItem(Severity severity, ErrorCategory category, String code, String shortDescription, Throwable cause, Set<String> relatedDataTypes);
 
-	RawResultBlock createRawResultBlock(Task task, String type);
+  RawResultBlock createRawResultBlock(Task task, String type);
 
-	/**
-	 * Create a Request.
-	 * 
-	 * @param requestCase
-	 *            Requests are always linked to a Case
-	 * @param type
-	 * @param correlationId
-	 */
-	Request createRequest(Case requestCase, String initiator, String category, String type, String correlationId);
+  /**
+   * Create a Request.
+   * 
+   * @param requestCase
+   *          Requests are always linked to a Case
+   * @param type
+   * @param correlationId
+   */
+  Request createRequest(Case requestCase, String initiator, String category, String type, String correlationId);
 
-	Request createRequest(Case requestCase, String initiator, String executor, String category, String type, String correlationId);
+  Request createRequest(Case requestCase, String initiator, String executor, String category, String type, String correlationId);
 
-	ResultBlock createResultBlock(Task task, String type);
+  ResultBlock createResultBlock(Task task, String type);
 
-	ResultBlock createResultBlock(Task task, String type, Date date);
+  ResultBlock createResultBlock(Task task, String type, Date date);
 
-	ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit);
+  ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit);
 
-	ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit, Date date);
+  ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit, Date date);
 
-	ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit, Integer level);
+  ResultItem<String> createResultItem(ResultBlock resultBlock, String name, String value, String unit, Integer level);
 
-	Task createTask(Class<? extends Task> taskClass, Request request, String initiator, String type) throws Exception;
-	Task createTask(Request request, String initiator, String type);
-	// these are temporarily back to support frok/join scoped context handling
-  Task createTask(Class<? extends Task> taskClass, Context context, String initiator, String type) throws Exception;
+  <T extends Task> T createTask(Class<T> taskClass, Request request, String initiator, String type) throws Exception;
+
+  Task createTask(Request request, String initiator, String type);
+
+  // these are temporarily back to support frok/join scoped context handling
+  <T extends Task> T createTask(Class<T> taskClass, Context context, String initiator, String type) throws Exception;
+
   Task createTask(Context context, String initiator, String type);
 }
